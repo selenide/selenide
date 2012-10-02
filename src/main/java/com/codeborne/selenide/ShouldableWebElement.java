e4a4cbb85c1391f9d5800a2974149a65e3a1c336
Andrei Solntsev <drusha49@hotmail.com>
2012-10-02 18:24:05 +0300

    ShouldableWebElement.toString method displays WebElement in human-readable format. Useful for logging.

--- src/main/java/com/codeborne/selenide/ShouldableWebElement.java
+++ src/main/java/com/codeborne/selenide/ShouldableWebElement.java
@@ -6,4 +6,10 @@ public interface ShouldableWebElement extends WebElement {
   ShouldableWebElement should(Condition... condition);
   ShouldableWebElement shouldHave(Condition... condition);
   ShouldableWebElement shouldBe(Condition... condition);
+
+  /**
+   * Displays WebElement in human-readable format
+   * @return e.g. <strong id=orderConfirmedStatus class=>Order has been confirmed</strong>
+   */
+  @Override String toString();
 }
