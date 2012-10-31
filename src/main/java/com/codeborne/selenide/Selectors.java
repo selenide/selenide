2d732e11dcbeb0d6cabffb8463a5f157fe875d98
Andrei Solntsev <drusha49@hotmail.com>
2012-11-01 00:09:20 +0200

    NB! Changed Selectors.byText() behaviour - now it matches THE WHOLE TEXT, not a substring. A new method withText() has been added that matches substring.

--- src/main/java/com/codeborne/selenide/Selectors.java
+++ src/main/java/com/codeborne/selenide/Selectors.java
@@ -3,12 +3,31 @@ package com.codeborne.selenide;
 import org.openqa.selenium.By;
 
 public class Selectors {
-  public static By byText(String elementText) {
+  /**
+   * Find element CONTAINING given text (as a substring)
+   * @param elementText
+   * @return
+   */
+  public static By withText(String elementText) {
     if (elementText.contains("'") && elementText.contains("\"")) {
       throw new UnsupportedOperationException("Text with both apostrophes and quotes is not supported");
     }
     return elementText.contains("'") ?
         By.xpath("//*[contains(text(), \"" + elementText + "\")]") :
         By.xpath("//*[contains(text(), '" + elementText + "')]");
+  }
+
+  /**
+   * Find element HAVING given text (exactly this text)
+   * @param elementText
+   * @return
+   */
+  public static By byText(String elementText) {
+    if (elementText.contains("'") && elementText.contains("\"")) {
+      throw new UnsupportedOperationException("Text with both apostrophes and quotes is not supported");
+    }
+    return elementText.contains("'") ?
+        By.xpath("//*[text() = \"" + elementText + "\"]") :
+        By.xpath("//*[text() = '" + elementText + "']");
   }
 }
