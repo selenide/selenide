cd4d3b8c149ca2dcc6d9b32524a91ca619dfa104
Andrei Solntsev <drusha49@hotmail.com>
2012-12-16 13:48:34 +0200

    Shorten tweet to take less space

--- javascripts/selenide-web.js
+++ javascripts/selenide-web.js
@@ -1,3 +1,7 @@
+function shortTweet(tweetText) {
+  return tweetText.length < 80 ? tweetText : tweetText.substring(0, 80) + "...";
+}
+
 $(function() {
   $.getJSON("https://api.twitter.com/1/statuses/user_timeline.json"
       + "?screen_name=jselenide&count=5&callback=?", function(tweets) {
@@ -7,7 +11,9 @@ $(function() {
       var h = $("<tr><td class=title><span class='date'></span> <a></a></td></tr>");
       h.find(".date").html(date.getDate() + "." + date.getMonth() + "." + (1900+date.getYear()));
       h.find(".title a")
-          .attr("href", "http://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str).text(tweet.text);
+          .attr("href", "http://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str)
+          .attr('title', tweet.text)
+          .text(shortTweet(tweet.text));
 
       b.append(h);
     });
