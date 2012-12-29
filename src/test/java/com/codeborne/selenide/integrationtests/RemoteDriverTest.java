631395adddbb9107be3a3fd284eab86ca019f606
Andrei Solntsev <drusha49@hotmail.com>
2012-12-30 00:41:55 +0200

    Removed dependency on an external resource

--- src/test/java/com/codeborne/selenide/integrationtests/RemoteDriverTest.java
+++ src/test/java/com/codeborne/selenide/integrationtests/RemoteDriverTest.java
@@ -4,9 +4,11 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.DOM.$;
 import static com.codeborne.selenide.Navigation.navigateToAbsoluteUrl;
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
+import static java.lang.Thread.currentThread;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
@@ -29,8 +31,8 @@ public class RemoteDriverTest {
 
   @Test
   public void testRemoteDriver() {
-    navigateToAbsoluteUrl("http://seleniumhq.org/");
-    assertThat($("#mainContent > h2").getText(), equalTo("What is Selenium?"));
+    navigateToAbsoluteUrl(currentThread().getContextClassLoader().getResource("page_with_selects_without_jquery.html"));
+    assertThat($("body > h1").getText(), equalTo("Page without JQuery")); // long style
+    $("body > h1").shouldHave(text("Page without JQuery")); // short style
   }
-
 }
