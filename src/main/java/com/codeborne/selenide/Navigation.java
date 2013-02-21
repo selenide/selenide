a16ef39d9df6be28c323556d424d221f5a2d1aa5
Andrei Solntsev <drusha49@hotmail.com>
2013-02-21 23:26:45 +0200

    Fixed bug in open method: file:/ url can start with single slash

--- src/main/java/com/codeborne/selenide/Navigation.java
+++ src/main/java/com/codeborne/selenide/Navigation.java
@@ -23,9 +23,9 @@ public class Navigation {
    * @param relativeOrAbsoluteUrl If starting with "http://" or "https://" or "file://", it's considered to be relative URL. In this case, it's prepended by baseUrl
    */
   public static void open(String relativeOrAbsoluteUrl) {
-    if (relativeOrAbsoluteUrl.startsWith("http://") ||
-        relativeOrAbsoluteUrl.startsWith("https://") ||
-        relativeOrAbsoluteUrl.startsWith("file://")) {
+    if (relativeOrAbsoluteUrl.startsWith("http:") ||
+        relativeOrAbsoluteUrl.startsWith("https:") ||
+        relativeOrAbsoluteUrl.startsWith("file:")) {
       navigateToAbsoluteUrl(relativeOrAbsoluteUrl);
     } else {
       navigateToAbsoluteUrl(absoluteUrl(relativeOrAbsoluteUrl));
