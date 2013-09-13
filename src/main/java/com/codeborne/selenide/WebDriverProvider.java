83a545185358f3d03284cada5868419e1a53307c
Andrei Solntsev <andrei.solntsev@gmail.com>
2013-09-13 23:08:05 +0300

    WebDriverProvider is deprecated

--- src/main/java/com/codeborne/selenide/WebDriverProvider.java
+++ src/main/java/com/codeborne/selenide/WebDriverProvider.java
@@ -2,6 +2,16 @@ package com.codeborne.selenide;
 
 import org.openqa.selenium.WebDriver;
 
+/**
+ * We found that this interface is useless.
+ * It's easier to call method com.codeborne.selenide.WebDriverRunner#setWebDriver(org.openqa.selenium.WebDriver).
+ *
+ * Another alternative is Overriding com.codeborne.selenide.impl.WebDriverThreadLocalContainer
+ *
+ * @deprecated
+ */
+@Deprecated
 public interface WebDriverProvider {
+  @Deprecated
   WebDriver createDriver();
 }
