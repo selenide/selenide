ffe04cc696514c067571bd2cf1011d12e454416d
Andrei Solntsev <andrei.solntsev@gmail.com>
2013-11-10 22:36:43 +0200

    29 Created annotations for TestNG that automatically create browser per test/per class

---
+++ src/main/java/com/codeborne/selenide/testng/BrowserPerTest.java
@@ -0,0 +1,36 @@
+package com.codeborne.selenide.testng;
+
+import org.testng.ITestResult;
+import org.testng.reporters.ExitCodeListener;
+
+import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
+
+/**
+ * Annotate your test class with <code>@Listeners({ BrowserPerTest.class})</code>
+ */
+public class BrowserPerTest extends ExitCodeListener {
+
+  @Override
+  public void onTestFailure(ITestResult result) {
+    super.onTestFailure(result);
+    closeWebDriver();
+  }
+
+  @Override
+  public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+    super.onTestFailedButWithinSuccessPercentage(result);
+    closeWebDriver();
+  }
+
+  @Override
+  public void onTestSuccess(ITestResult result) {
+    super.onTestSuccess(result);
+    closeWebDriver();
+  }
+
+  @Override
+  public void onConfigurationFailure(ITestResult result) {
+    super.onConfigurationFailure(result);
+    closeWebDriver();
+  }
+}
