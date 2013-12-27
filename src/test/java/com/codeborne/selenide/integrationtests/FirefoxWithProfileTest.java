601489e2fc3f893362924d85b6932eb054775db0
Andrei Solntsev <andrei.solntsev@gmail.com>
2013-12-28 00:31:51 +0200

    Removed dependency on Google search to avoid randomly failing test

--- src/test/java/com/codeborne/selenide/integrationtests/FirefoxWithProfileTest.java
+++ src/test/java/com/codeborne/selenide/integrationtests/FirefoxWithProfileTest.java
@@ -1,5 +1,6 @@
 package com.codeborne.selenide.integrationtests;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -10,15 +11,17 @@ import org.openqa.selenium.firefox.FirefoxProfile;
 import java.io.File;
 import java.io.IOException;
 
-import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.enabled;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class FirefoxWithProfileTest {
   @Test
   public void createFirefoxWithCustomProfile() {
+    if (!WebDriverRunner.FIREFOX.equalsIgnoreCase(Configuration.browser)) {
+      return;
+    }
+
     FirefoxProfile profile = new FirefoxProfile();
     try {
       profile.addExtension(new File(Thread.currentThread().getContextClassLoader().getResource("firebug-1.11.4.xpi").getPath()));
@@ -37,6 +40,5 @@ public class FirefoxWithProfileTest {
 
     open("http://google.com");
     $(By.name("q")).shouldBe(enabled).val("selenide").pressEnter();
-    $$("#ires li.g").shouldHave(size(10));
   }
 }
