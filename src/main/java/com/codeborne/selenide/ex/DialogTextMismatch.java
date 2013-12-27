5766a9ce2c1e740df3f24385f103703384f4bda5
Andrei Solntsev <andrei.solntsev@gmail.com>
2013-12-28 00:16:41 +0200

    takes screenshots also in case of dialog text mismatch

--- src/main/java/com/codeborne/selenide/ex/DialogTextMismatch.java
+++ src/main/java/com/codeborne/selenide/ex/DialogTextMismatch.java
@@ -1,9 +1,12 @@
 package com.codeborne.selenide.ex;
 
+import static com.codeborne.selenide.ex.ErrorMessages.screenshot;
+
 public class DialogTextMismatch extends AssertionError {
   public DialogTextMismatch(String actualText, String expectedText) {
     super("\nActual: " + actualText +
-        "\nExpected: " + expectedText);
+        "\nExpected: " + expectedText +
+        screenshot());
   }
 
   @Override
