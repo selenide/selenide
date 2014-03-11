c938d5a03c685b8ea78f9717f222da87c8f67ae9
Andrei Solntsev <andrei.solntsev@gmail.com>
2014-03-11 13:38:36 +0200

    added package javadoc for "com.codeborne.selenide.impl" package

---
+++ src/main/java/com/codeborne/selenide/impl/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * Implementation details of Selenide library.
+ *
+ * Selenide is designed so that you can you override any single piece of its logic.
+ * For example, to override the logic of taking screenshots one can override class:
+ *
+ * <pre>
+ * Screenshots.screenshots = new ScreenShotLaboratory() {
+ *   @Override
+ *   protected File savePageImageToFile(String fileName, WebDriver webdriver) {
+ *     // your custom logic
+ *   }
+ * };
+ * </pre>
+ *
+ * NB! But "com.codeborne.selenide.impl" package is subject to change.
+ * It's up to you to maintain your overridden logic.
+ */
+package com.codeborne.selenide.impl;
