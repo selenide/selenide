caddf27ddb120c4a1cbc6ab516590051979fe2dc
Andrei Solntsev <andrei.solntsev@gmail.com>
2014-04-07 22:56:51 +0300

    take screenshot on Selenide failed tests

--- src/test/java/integration/IntegrationTest.java
+++ src/test/java/integration/IntegrationTest.java
@@ -1,11 +1,16 @@
 package integration;
 
+import com.codeborne.selenide.junit.ScreenShooter;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.openqa.selenium.net.PortProber.findFreePort;
 
 public abstract class IntegrationTest {
+  @Rule
+  public ScreenShooter img = ScreenShooter.failedTests() ;
+
   private static int port;
   private static LocalHttpServer server;
 
