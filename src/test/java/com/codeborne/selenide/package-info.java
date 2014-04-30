500b39cbdbd0651362844b513161f491964a3ed3
Andrei Solntsev <andrei.solntsev@gmail.com>
2014-05-01 00:03:23 +0300

    Added package javadoc

---
+++ src/test/java/com/codeborne/selenide/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Unit-tests for Selenide.
+ * 
+ * These tests do not require browser and run very quickly. 
+ */
+package com.codeborne.selenide;
---
+++ src/test/java/integration/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * Integration tests for Selenide.
+ * 
+ * These tests require a real browser and run quite slowly.
+ * We typically run them in all basic browsers (firefox, chrome, htmlunit, phantomjs).
+ * 
+ * They can execute from command line: "./gradle allTests"
+ * 
+ * They can also be executed for concrete browsers:
+ * "./gradle test"  (htmlunit)
+ * "./gradle firefox"
+ * "./gradle chrome"
+ * "./gradle phantomjs"
+ * "./gradle ie"
+ * "./gradle safari"
+ * "./gradle opera"
+ */
+package integration;
