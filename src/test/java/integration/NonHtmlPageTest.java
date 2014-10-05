4304956eec2cfa17ecbf94fa49f951abe33d9712
Andrei Solntsev <andrei.solntsev@gmail.com>
2014-10-05 22:42:27 +0300

    Fixes #136 Can open non-html pages (e.g. plain text)

--- src/test/java/integration/NonHtmlPageTest.java
+++ src/test/java/integration/NonHtmlPageTest.java
@@ -3,12 +3,13 @@ package integration;
 import org.junit.Test;
 
 import static com.codeborne.selenide.WebDriverRunner.source;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class NonHtmlPageTest extends IntegrationTest {
   @Test
   public void canOpenNonHtmlPage() {
     openFile("hello_world.txt");
-    assertEquals("Hello, WinRar!", source());
+    String source = source();
+    assertTrue("Actual source: " + source, source.contains("Hello, WinRar!"));
   }
 }
