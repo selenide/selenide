b8adf8fbb3db81513f9aadc4c58e23ac256eb6f5
Ariel-isaacm <ariel.isaacm@gmail.com>
2015-10-07 12:45:10 -0700

    TODO done

--- src/main/java/com/codeborne/selenide/Selenide.java
+++ src/main/java/com/codeborne/selenide/Selenide.java
@@ -396,7 +396,7 @@ public class Selenide {
 
   private static void checkDialogText(String expectedDialogText, String actualDialogText) {
     if (expectedDialogText != null && !expectedDialogText.equals(actualDialogText)) {
-      // TODO Take screenshot
+      Screenshots.takeScreenShot(Selenide.class.getName(),Thread.currentThread().getName());
       throw new DialogTextMismatch(actualDialogText, expectedDialogText);
     }
   }
