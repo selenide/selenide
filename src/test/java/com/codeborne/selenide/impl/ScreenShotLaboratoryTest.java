91aa0018ab476fc4b715e423a71f1ecb53f1b918
Andrei Solntsev <andrei.solntsev@gmail.com>
2015-11-25 00:58:00 +0200

    #242 #226 remove unused import

--- src/test/java/com/codeborne/selenide/impl/ScreenShotLaboratoryTest.java
+++ src/test/java/com/codeborne/selenide/impl/ScreenShotLaboratoryTest.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.util.List;
 
 import static java.io.File.separatorChar;
-import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
