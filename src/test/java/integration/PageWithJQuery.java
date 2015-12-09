3b0b7aba4262a34e3185227a470a4ef445c245b8
Andrei Solntsev <andrei.solntsev@gmail.com>
2015-12-10 00:36:29 +0200

    Remove deprecated class JQuery

--- src/test/java/integration/PageWithJQuery.java
+++ src/test/java/integration/PageWithJQuery.java
@@ -1,25 +1,16 @@
 package integration;
 
-import com.codeborne.selenide.JQuery;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
-import static org.junit.Assert.assertTrue;
 
 public class PageWithJQuery extends IntegrationTest {
-  JQuery jQuery = new JQuery();
-
   @Before
   public void openTestPageWithJQuery() {
     openFile("page_with_jquery.html");
-  }
-
-  @Test
-  public void worksForPagesWithJQuery() {
-    assertTrue(jQuery.isJQueryAvailable());
   }
 
   @Test
--- src/test/java/integration/PageWithoutJQuery.java
+++ src/test/java/integration/PageWithoutJQuery.java
@@ -1,24 +1,15 @@
 package integration;
 
-import com.codeborne.selenide.JQuery;
 import org.junit.Before;
 import org.junit.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
-import static org.junit.Assert.assertFalse;
 
 public class PageWithoutJQuery extends IntegrationTest {
-  JQuery jQuery = new JQuery();
-
   @Before
   public void openTestPageWithJQuery() {
     openFile("page_with_selects_without_jquery.html");
-  }
-
-  @Test
-  public void worksForPagesWithoutJQuery() {
-    assertFalse(jQuery.isJQueryAvailable());
   }
 
   @Test
