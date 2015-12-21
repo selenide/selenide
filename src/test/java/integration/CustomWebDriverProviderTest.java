28c6340c0a15cec3569a06f0816d747cbe5e196d
Andrei Solntsev <andrei.solntsev@gmail.com>
2015-12-22 01:37:21 +0200

    #228 add integration test for WebdriverProvider

---
+++ src/test/java/integration/CustomWebDriverProviderTest.java
@@ -0,0 +1,49 @@
+package integration;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverProvider;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.close;
+import static org.junit.Assert.assertTrue;
+
+public class CustomWebDriverProviderTest extends IntegrationTest {
+  private String originalWebdriver;
+  
+  @Before
+  public void setUp() {
+    close();
+    originalWebdriver = Configuration.browser;
+  }
+
+  @After
+  public void tearDown() {
+    close();
+    Configuration.browser = originalWebdriver;
+  }
+
+  @Test
+  public void userCanImplementAnyCustomWebdriverProvider() {
+    Configuration.browser = CustomWebDriverProvider.class.getName();
+    
+    openFile("autocomplete.html");
+    
+    assertTrue(WebDriverRunner.getWebDriver() instanceof CustomFirefoxDriver);
+  }
+
+  private static class CustomFirefoxDriver extends FirefoxDriver {
+  }
+  
+  public static class CustomWebDriverProvider implements WebDriverProvider {
+    @Override
+    public WebDriver createDriver(DesiredCapabilities desiredCapabilities) {
+      return new CustomFirefoxDriver();
+    }
+  }
+}
