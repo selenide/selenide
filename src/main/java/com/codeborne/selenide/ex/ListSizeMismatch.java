68f0d818c19b9712f3cca8532722bdfa2076df80
Andrei Solntsev <andrei.solntsev@gmail.com>
2016-03-27 22:44:24 +0300

    #274 fix code formatting

--- src/main/java/com/codeborne/selenide/ex/ListSizeMismatch.java
+++ src/main/java/com/codeborne/selenide/ex/ListSizeMismatch.java
@@ -8,8 +8,8 @@ import java.util.List;
 import static com.codeborne.selenide.ElementsCollection.elementsToString;
 
 public class ListSizeMismatch extends UIAssertionError {
-  public ListSizeMismatch(String operator, int expectedSize, WebElementsCollection collection, List<WebElement> actualElements,
-                          Exception lastError, long timeoutMs) {
+  public ListSizeMismatch(String operator, int expectedSize, WebElementsCollection collection, 
+                          List<WebElement> actualElements, Exception lastError, long timeoutMs) {
     super(": expected: " + operator + " " + expectedSize +
         ", actual: " + (actualElements == null ? 0 : actualElements.size()) +
         ", collection: " + collection.description() +
