48144be5c08d15bfda62fb168035b57e91eaf6e3
Andrei Solntsev <andrei.solntsev@gmail.com>
2016-05-02 08:21:18 +0300

    #320 fix checkstyle issue

--- src/test/java/integration/BasicAuthTest.java
+++ src/test/java/integration/BasicAuthTest.java
@@ -19,80 +19,84 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertThat;
 import static org.openqa.selenium.support.ui.ExpectedConditions.alertIsPresent;
 
-public class BasicAuthTest extends IntegrationTest  {
+public class BasicAuthTest extends IntegrationTest {
 
-    private String browserOriginalValue;
+  private String browserOriginalValue;
 
-    @Before
-    public void setUp() {
-        browserOriginalValue = Configuration.browser;
+  @Before
+  public void setUp() {
+    browserOriginalValue = Configuration.browser;
+  }
+
+  @After
+  public void tearDown() {
+    WebDriverRunner.closeWebDriver();
+    Configuration.browser = browserOriginalValue;
+  }
+
+  @Test
+  public void canPassBasicAuthInFirefox() {
+    Configuration.browser = "firefox";
+    Selenide.open("http://httpbin.org/basic-auth/user/passwd",
+        "",
+        "user",
+        "passwd");
+    $(By.tagName("pre")).waitUntil(visible, 10000);
+    assertThat(source(), containsString("\"authenticated\": true,"));
+  }
+
+  @Test
+  public void canPassBasicAuthInHtmlUnit() {
+    Configuration.browser = "htmlunit";
+    Selenide.open("http://httpbin.org/basic-auth/user/passwd",
+        "",
+        "user",
+        "passwd");
+    assertThat(source(), containsString("\"authenticated\": true,"));
+  }
+
+  @Test
+  @Ignore
+  public void canPassBasicAuthInPhantom() {
+    Configuration.browser = "phantomjs";
+    Selenide.open("http://httpbin.org/basic-auth/user/passwd",
+        "",
+        "user",
+        "passwd");
+    $(By.tagName("pre")).waitUntil(visible, 10000);
+    assertThat(source(), containsString("\"authenticated\": true,"));
+  }
+
+  @Test
+  @Ignore
+  public void canPassBasicAuthInChrome() {
+    Configuration.browser = "chrome";
+    Selenide.open("http://httpbin.org/basic-auth/user/passwd",
+        "",
+        "user",
+        "passwd");
+    $(By.tagName("pre")).waitUntil(visible, 10000);
+    assertThat(source(), containsString("\"authenticated\": true,"));
+  }
+
+  @Test
+  @Ignore
+  public void canPassBasicAuthInIe() {
+    Configuration.browser = "ie";
+    Selenide.open("http://httpbin.org/basic-auth/user/passwd",
+        "",
+        "user",
+        "passwd");
+    assertThat(source(), containsString("WebDriver"));
+  }
+
+  public boolean isAlertPresent() {
+    try {
+      Wait().until(alertIsPresent());
+      return true;
     }
-
-    @After
-    public void tearDown() {
-        WebDriverRunner.closeWebDriver();
-        Configuration.browser = browserOriginalValue;
+    catch (TimeoutException | NoAlertPresentException ex) {
+      return false;
     }
-
-    @Test
-    public void canPassBasicAuthInFirefox() {
-        Configuration.browser = "firefox";
-        Selenide.open("http://httpbin.org/basic-auth/user/passwd",
-                "",
-                "user",
-                "passwd");
-        $(By.tagName("pre")).waitUntil(visible, 10000);
-        assertThat(source(), containsString("\"authenticated\": true,"));
-    }
-
-    @Test
-    public void canPassBasicAuthInHtmlUnit() {
-        Configuration.browser = "htmlunit";
-        Selenide.open("http://httpbin.org/basic-auth/user/passwd",
-                "",
-                "user",
-                "passwd");
-        assertThat(source(), containsString("\"authenticated\": true,"));
-    }
-
-    @Test @Ignore
-    public void canPassBasicAuthInPhantom() {
-        Configuration.browser = "phantomjs";
-        Selenide.open("http://httpbin.org/basic-auth/user/passwd",
-                "",
-                "user",
-                "passwd");
-        $(By.tagName("pre")).waitUntil(visible, 10000);
-        assertThat(source(), containsString("\"authenticated\": true,"));
-    }
-
-    @Test @Ignore
-    public void canPassBasicAuthInChrome() {
-        Configuration.browser = "chrome";
-        Selenide.open("http://httpbin.org/basic-auth/user/passwd",
-                "",
-                "user",
-                "passwd");
-        $(By.tagName("pre")).waitUntil(visible, 10000);
-        assertThat(source(), containsString("\"authenticated\": true,"));
-    }
-
-    @Test @Ignore
-    public void canPassBasicAuthInIe() {
-        Configuration.browser = "ie";
-        Selenide.open("http://httpbin.org/basic-auth/user/passwd",
-                "",
-                "user",
-                "passwd");
-        assertThat(source(), containsString("WebDriver"));
-    }
-
-    public boolean isAlertPresent() {
-        try {
-            Wait().until(alertIsPresent());
-            return true;
-        } catch (TimeoutException | NoAlertPresentException ex) {
-            return false;
-        }
-    }
+  }
 }
