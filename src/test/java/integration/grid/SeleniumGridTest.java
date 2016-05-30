55988b71c24faaf2e2db9495e16c42f14051d9e7
Andrei Solntsev <andrei.solntsev@gmail.com>
2016-05-31 00:45:17 +0300

    ignore SeleniumGrid test for PhantomJS (temporarily)

--- src/test/java/integration/grid/SeleniumGridTest.java
+++ src/test/java/integration/grid/SeleniumGridTest.java
@@ -1,6 +1,7 @@
 package integration.grid;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
 import integration.IntegrationTest;
 import org.junit.After;
 import org.junit.Before;
@@ -10,11 +11,14 @@ import org.openqa.grid.selenium.GridLauncher;
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.close;
+import static org.junit.Assume.assumeFalse;
 import static org.openqa.selenium.net.PortProber.findFreePort;
 
 public class SeleniumGridTest extends IntegrationTest {
   @Before
   public void setUp() throws Exception {
+    assumeFalse(WebDriverRunner.isPhantomjs()); // for unknown reason, node registration doesn't work with PhantomJS yet.
+    
     close();
     
     int hubPort = findFreePort();
