0fba466253bf7f2aff3880fccabfa26bac61b233
Andrei Solntsev <andrei.solntsev@gmail.com>
2016-08-03 22:06:00 +0300

    use rule MockWebdriverContainer to safely mock/reset static variable WebDriverRunner.webdriverContainer in tests

--- src/test/java/com/codeborne/selenide/SelenideTest.java
+++ src/test/java/com/codeborne/selenide/SelenideTest.java
@@ -1,19 +1,16 @@
 package com.codeborne.selenide;
 
-import com.codeborne.selenide.impl.WebDriverThreadLocalContainer;
-import org.junit.Before;
+import com.codeborne.selenide.rules.MockWebdriverContainer;
+import org.junit.Rule;
 import org.junit.Test;
 
 import static com.codeborne.selenide.WebDriverRunner.webdriverContainer;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
 
 public class SelenideTest {
-  @Before
-  public void setUp() {
-    webdriverContainer = mock(WebDriverThreadLocalContainer.class);
-  }
+  @Rule
+  public MockWebdriverContainer mockWebdriverContainer = new MockWebdriverContainer();
 
   @Test
   public void getJavascriptErrors_returnsEmptyListIfWebdriverIsNotStarted() {
