40a39a5ea229806e5102b87bc68d0529d0ce3723
Alexei Vinogradov <alexei@vinogradoff.de>
2016-08-07 15:36:30 +0200

    Added Status-Quo Tests for Invisible and NotExistingElements

--- src/test/java/integration/InvisibleElementTest.java
+++ src/test/java/integration/InvisibleElementTest.java
@@ -1,48 +1,35 @@
 package integration;
 
 import org.junit.*;
-import org.openqa.selenium.*;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
-import static org.junit.Assert.*;
 
 public class InvisibleElementTest extends IntegrationTest {
 	@Before
-	public void openTestPageWithJQuery() {
-		openFile("page_with_selects_without_jquery.html");
+	public void clickHidesElement() {
+		openFile("elements_disappear_on_click.html");
+		$("#hide").click();
 	}
 
 	@Test
-	public void userCanSelectCheckbox() {
-		$(By.name("rememberMe")).shouldNotBe(selected);
-
-		$(By.name("rememberMe")).click();
-
-		$(By.name("rememberMe")).shouldBe(selected);
-		assertEquals("<input name=\"rememberMe\" type=\"checkbox\" value=\"on\" selected:true></input>",
-						$(By.name("rememberMe")).toString());
+	public void shouldBeHidden() {
+		$("#hide").shouldBe(hidden);
 	}
 
 	@Test
-	public void userCanCheckCheckbox() {
-		$(By.name("rememberMe")).setSelected(true);
-		$(By.name("rememberMe")).shouldBe(selected);
-
-		$(By.name("rememberMe")).setSelected(true);
-		$(By.name("rememberMe")).shouldBe(selected);
+	public void shouldNotBeVisible() {
+		$("#hide").shouldNotBe(visible);
 	}
 
 	@Test
-	public void userCanUnCheckCheckbox() {
-		$(By.name("rememberMe")).setSelected(true);
-		$(By.name("rememberMe")).shouldBe(selected);
+	public void shouldHaveTextHide() {
+		$("#hide").shouldHave(text("Hide me"));
+	}
 
-		$(By.name("rememberMe")).setSelected(false);
-		$(By.name("rememberMe")).shouldNotBe(selected);
-
-		$(By.name("rememberMe")).setSelected(false);
-		$(By.name("rememberMe")).shouldNotBe(selected);
+	@Test
+	public void shouldNotHaveTextRemove() {
+		$("#hide").shouldNotHave(text("Remove me"));
 	}
 
 }
---
+++ src/test/java/integration/NotExistingElementTest.java
@@ -0,0 +1,39 @@
+package integration;
+
+import org.junit.*;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class NotExistingElementTest extends IntegrationTest {
+	@Before
+	public void openPage() {
+		openFile("elements_disappear_on_click.html");
+	}
+
+	@Test
+	public void shouldNotExist() {
+		$("#not_exist").shouldNot(exist);
+	}
+
+	@Test
+	public void shouldBeHidden() {
+		$("#not_exist").shouldBe(hidden);
+	}
+
+	@Test
+	public void shouldNotBeVisible() {
+		$("#not_exist").shouldNotBe(visible);
+	}
+
+	@Test
+	public void shouldNotHaveTextRemove() {
+		$("#not_exist").shouldNotHave(text("Remove me"));
+	}
+
+	@Test
+	public void shouldNotHaveAttributeAbc() {
+		$("#not_exist").shouldNotHave(attribute("abc"));
+	}
+
+}
