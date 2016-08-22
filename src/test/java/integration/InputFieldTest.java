27c1b9ef58b053ab943b9112da7c8eda309b4d57
Boris Osipov <boris_osipov@epam.com>
2016-08-22 18:37:03 +0300

    Bug http://bit.ly/1JKT4AE has been fixed. Turn on related tests.

--- src/test/java/integration/InputFieldTest.java
+++ src/test/java/integration/InputFieldTest.java
@@ -4,14 +4,9 @@ import com.codeborne.selenide.*;
 import org.junit.*;
 
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.WebDriverRunner.*;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
-import static org.junit.Assume.*;
 
-/**
- * Created by vinogradov on 11.01.16.
- */
 public class InputFieldTest extends IntegrationTest {
 
   @Before
@@ -21,8 +16,6 @@ public class InputFieldTest extends IntegrationTest {
 
   @Test
   public void selenideClearTest() {
-    assumeTrue("Bug in Selenium should be fixed in 2.49+, see http://bit.ly/1JKT4AE", isFirefox());
-
     SelenideElement input = $("#id1");
     assertThat(input.getValue(), is(equalTo("")));
 
