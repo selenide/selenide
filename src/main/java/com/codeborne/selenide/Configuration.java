d84648f5b7f28a650a529a4a2cc39b0d2cabc945
Andrei Solntsev <andrei.solntsev@gmail.com>
2016-09-02 08:52:05 +0300

    Closes #387 User can choose how to download files via Configuration.fileDownload

--- src/main/java/com/codeborne/selenide/Configuration.java
+++ src/main/java/com/codeborne/selenide/Configuration.java
@@ -280,4 +280,23 @@ public class Configuration {
    * @see AssertionMode
    */
   public static AssertionMode assertionMode = STRICT;
+  
+  public enum FileDownloadMode {
+    /**
+     * Download files via direct http request.
+     * Works only for <a href></a> elements. 
+     * Sends GET request to "href" with all cookies from current browser session. 
+     */
+    HTTPGET,
+
+    /**
+     * Download files via selenide embedded proxy server.
+     * Works for any elements (e.g. form submission).
+     * Doesn't work if you are using custom webdriver without selenide proxy server.
+     */
+    PROXY
+  }
+
+  public static FileDownloadMode fileDownload = FileDownloadMode.valueOf(
+      System.getProperty("selenide.fileDownload", "PROXY"));
 }
--- src/main/java/com/codeborne/selenide/commands/DownloadFile.java
+++ src/main/java/com/codeborne/selenide/commands/DownloadFile.java
@@ -1,6 +1,7 @@
 package com.codeborne.selenide.commands;
 
 import com.codeborne.selenide.Command;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.impl.DownloadFileWithHttpRequest;
 import com.codeborne.selenide.impl.DownloadFileWithProxyServer;
@@ -11,6 +12,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.logging.Logger;
 
+import static com.codeborne.selenide.Configuration.FileDownloadMode.HTTPGET;
 import static com.codeborne.selenide.WebDriverRunner.webdriverContainer;
 
 public class DownloadFile implements Command<File> {
@@ -22,7 +24,11 @@ public class DownloadFile implements Command<File> {
   @Override
   public File execute(SelenideElement proxy, WebElementSource linkWithHref, Object[] args) throws IOException {
     WebElement link = linkWithHref.findAndAssertElementIsVisible();
-    if (webdriverContainer.getProxyServer() == null) {
+    if (Configuration.fileDownload == HTTPGET) {
+      LOG.config("selenide.fileDownload = " + System.getProperty("selenide.fileDownload") + " download file via http get");
+      return downloadFileWithHttpRequest.download(link);
+    }
+    else if (webdriverContainer.getProxyServer() == null) {
       LOG.config("Proxy server is not started - download file via http get");
       return downloadFileWithHttpRequest.download(link);
     }
