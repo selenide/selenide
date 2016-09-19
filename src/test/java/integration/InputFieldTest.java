264fe19996cdcfdddc9eefb8680815803227d268
Andrei Solntsev <andrei.solntsev@gmail.com>
2016-09-20 01:01:01 +0300

    #379 ignore test in htmlunit
    It fails with the following error:
    
    java.lang.StringIndexOutOfBoundsException: start > length()
    	at java.lang.AbstractStringBuilder.replace(AbstractStringBuilder.java:826)
    	at java.lang.StringBuilder.replace(StringBuilder.java:262)
    	at com.gargoylesoftware.htmlunit.html.DoTypeProcessor.add(DoTypeProcessor.java:136)
    	at com.gargoylesoftware.htmlunit.html.DoTypeProcessor.doType(DoTypeProcessor.java:121)
    	at com.gargoylesoftware.htmlunit.html.HtmlNumberInput.doType(HtmlNumberInput.java:54)
    	at com.gargoylesoftware.htmlunit.html.HtmlElement.type(HtmlElement.java:547)
    	at com.gargoylesoftware.htmlunit.html.HtmlElement.type(HtmlElement.java:495)
    	at com.gargoylesoftware.htmlunit.html.HtmlElement.type(HtmlElement.java:480)
    	at org.openqa.selenium.htmlunit.HtmlUnitKeyboard.sendKeys(HtmlUnitKeyboard.java:88)
    	at org.openqa.selenium.htmlunit.HtmlUnitWebElement.sendKeys(HtmlUnitWebElement.java:333)
    	at com.codeborne.selenide.commands.SetValue.execute(SetValue.java:42)

--- src/test/java/integration/InputFieldTest.java
+++ src/test/java/integration/InputFieldTest.java
@@ -1,11 +1,16 @@
 package integration;
 
-import com.codeborne.selenide.*;
-import org.junit.*;
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.Before;
+import org.junit.Test;
 
-import static com.codeborne.selenide.Selenide.*;
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assume.assumeFalse;
 
 public class InputFieldTest extends IntegrationTest {
 
@@ -16,6 +21,8 @@ public class InputFieldTest extends IntegrationTest {
 
   @Test
   public void selenideClearTest() {
+    assumeFalse("Fails with StringIndexOutOfBoundsException: start > length()", WebDriverRunner.isHtmlUnit());
+    
     SelenideElement input = $("#id1");
     assertThat(input.getValue(), is(equalTo("")));
 
