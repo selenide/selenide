1fd25266fa796d9ce104364d70b6116596cdbeed
Andrei Solntsev <andrei.solntsev@gmail.com>
2016-09-20 07:52:31 +0300

    #379 ignore test in phantomjs

--- src/test/java/integration/InputFieldTest.java
+++ src/test/java/integration/InputFieldTest.java
@@ -1,12 +1,13 @@
 package integration;
 
 import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.WebDriverRunner;
 import org.junit.Before;
 import org.junit.Test;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.isHtmlUnit;
+import static com.codeborne.selenide.WebDriverRunner.isPhantomjs;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -21,7 +22,8 @@ public class InputFieldTest extends IntegrationTest {
 
   @Test
   public void selenideClearTest() {
-    assumeFalse("Fails with StringIndexOutOfBoundsException: start > length()", WebDriverRunner.isHtmlUnit());
+    assumeFalse("Fails with StringIndexOutOfBoundsException: start > length()", isHtmlUnit());
+    assumeFalse("Fails with Expected: '456', but: was ''", isPhantomjs());
     
     SelenideElement input = $("#id1");
     assertThat(input.getValue(), is(equalTo("")));
