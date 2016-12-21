c51bb433c3d07428e8d4655a77c352d8a500f735
sepi <sepi@ciklum.com>
2016-12-21 16:25:27 +0200

    checkstyle fix for tests

--- src/test/java/integration/SelenidePageFactoryTest.java
+++ src/test/java/integration/SelenidePageFactoryTest.java
@@ -1,7 +1,6 @@
 package integration;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,33 +12,33 @@ import static com.codeborne.selenide.Selenide.page;
 /**
  * Created by sepi on 21.12.16.
  */
-public class SelenidePageFactoryTest extends IntegrationTest{
+public class SelenidePageFactoryTest extends IntegrationTest {
 
-    @Before
-    public void openTestPage() {
-        openFile("start_page.html");
-    }
+  @Before
+  public void openTestPage() {
+    openFile("start_page.html");
+  }
 
-    @Test
-    public void canInitPageWithFindByAnnotations(){
-        StartPageWithAnnotation startPage = page(StartPageWithAnnotation.class);
-        startPage.pageText.shouldHave(Condition.text("Start page"));
-    }
+  @Test
+  public void canInitPageWithFindByAnnotations() {
+    StartPageWithAnnotation startPage = page(StartPageWithAnnotation.class);
+    startPage.pageText.shouldHave(Condition.text("Start page"));
+  }
 
-    @Test
-    public void canInitPageWithoutFindByAnnotations(){
-        StartPageWithoutAnnotation startPage = page(StartPageWithoutAnnotation.class);
-        startPage.pageHeader.shouldHave(Condition.text("Selenide"));
-    }
+  @Test
+  public void canInitPageWithoutFindByAnnotations() {
+    StartPageWithoutAnnotation startPage = page(StartPageWithoutAnnotation.class);
+    startPage.pageHeader.shouldHave(Condition.text("Selenide"));
+  }
 }
 
 class StartPageWithAnnotation {
-    @FindBy(css = "#start-selenide")
-    public SelenideElement pageText;
+  @FindBy(css = "#start-selenide")
+  public SelenideElement pageText;
 }
 
 class StartPageWithoutAnnotation {
-    public SelenideElement pageHeader  = $("h1");
+  public SelenideElement pageHeader = $("h1");
 }
 
 
