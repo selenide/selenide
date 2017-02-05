d93403a4865f6d33e96300a287f0bd18c62a3916
Alexei Vinogradov <alexei@vinogradoff.de>
2017-02-05 13:48:45 +0100

    fixed JavaDoc on $,$$,find methods

--- src/main/java/com/codeborne/selenide/Selenide.java
+++ src/main/java/com/codeborne/selenide/Selenide.java
@@ -231,7 +231,8 @@ public class Selenide {
   }
 
   /**
-   * Find the first element matching given CSS selector
+   * Locates the first element matching given CSS selector
+   * ATTENTION! This method doesn't start any search yet!
    * @param cssSelector any CSS selector like "input[name='first_name']" or "#messages .new_message"
    * @return SelenideElement
    */
@@ -240,7 +241,8 @@ public class Selenide {
   }
 
   /**
-   * Find the first element matching given CSS selector
+   * Locates the first element matching given CSS selector
+   * ATTENTION! This method doesn't start any search yet!
    * @param seleniumSelector any Selenium selector like By.id(), By.name() etc.
    * @return SelenideElement
    */
@@ -260,7 +262,8 @@ public class Selenide {
    * (method will not be removed until 4.x or later)
    * @see  #$(String)
    *
-   * Find the first element matching given CSS selector
+   * Locates the first element matching given CSS selector
+   * ATTENTION! This method doesn't start any search yet!
    * @param parent the WebElement to search elements in
    * @param cssSelector any CSS selector like "input[name='first_name']" or "#messages .new_message"
    * @return SelenideElement
@@ -271,7 +274,8 @@ public class Selenide {
   }
 
   /**
-   * Find the Nth element matching given criteria
+   * Locates the Nth element matching given criteria
+   * ATTENTION! This method doesn't start any search yet!
    * @param cssSelector any CSS selector like "input[name='first_name']" or "#messages .new_message"
    * @param index 0..N
    * @return SelenideElement
@@ -285,7 +289,8 @@ public class Selenide {
    * (method will not be removed until 4.x or later)
    * @see  #$(String, int)
    *
-   * Find the Nth element matching given criteria
+   * Locates the Nth element matching given criteria
+   * ATTENTION! This method doesn't start any search yet!
    * @param parent the WebElement to search elements in
    * @param cssSelector any CSS selector like "input[name='first_name']" or "#messages .new_message"
    * @param index 0..N
@@ -301,7 +306,8 @@ public class Selenide {
    * (method will not be removed until 4.x or later)
    * @see  #$(By)
    *
-   * Find the first element matching given criteria
+   * Locates the first element matching given criteria
+   * ATTENTION! This method doesn't start any search yet!
    * @param parent the WebElement to search elements in
    * @param seleniumSelector any Selenium selector like By.id(), By.name() etc.
    * @return SelenideElement
@@ -316,7 +322,8 @@ public class Selenide {
    * (method will not be removed until 4.x or later)
    * @see  #$(By, int)
    *
-   * Find the Nth element matching given criteria
+   * Locates the Nth element matching given criteria
+   * ATTENTION! This method doesn't start any search yet!
    * @param parent the WebElement to search elements in
    * @param seleniumSelector any Selenium selector like By.id(), By.name() etc.
    * @param index 0..N
@@ -337,7 +344,8 @@ public class Selenide {
   }
 
   /**
-   * Find all elements matching given CSS selector.
+   * Locates all elements matching given CSS selector.
+   * ATTENTION! This method doesn't start any search yet!
    * Methods returns an ElementsCollection which is a list of WebElement objects that can be iterated,
    * and at the same time is implementation of WebElement interface,
    * meaning that you can call methods .sendKeys(), click() etc. on it.
@@ -350,7 +358,8 @@ public class Selenide {
   }
 
   /**
-   * Find all elements matching given CSS selector.
+   * Locates all elements matching given CSS selector.
+   * ATTENTION! This method doesn't start any search yet!
    * Methods returns an ElementsCollection which is a list of WebElement objects that can be iterated,
    * and at the same time is implementation of WebElement interface,
    * meaning that you can call methods .sendKeys(), click() etc. on it.
@@ -367,7 +376,8 @@ public class Selenide {
    * (method will not be removed until 4.x or later)
    * @see  #$$(String)
    *
-   * Find all elements matching given CSS selector inside given parent element
+   * Locates all elements matching given CSS selector inside given parent element
+   * ATTENTION! This method doesn't start any search yet!
    * Methods returns an ElementsCollection which is a list of WebElement objects that can be iterated,
    * and at the same time is implementation of WebElement interface,
    * meaning that you can call methods .sendKeys(), click() etc. on it.
@@ -386,7 +396,8 @@ public class Selenide {
    * (method will not be removed until 4.x or later)
    * @see  #$$(By)
    *
-   * Find all elements matching given criteria inside given parent element
+   * Locates all elements matching given criteria inside given parent element
+   * ATTENTION! This method doesn't start any search yet!
    * @see Selenide#$$(WebElement, String)
    */
   @Deprecated
@@ -395,7 +406,8 @@ public class Selenide {
   }
 
   /**
-   * Find the first element matching given criteria
+   * Locates the first element matching given criteria
+   * ATTENTION! This method doesn't start any search yet!
    * @param criteria instance of By: By.id(), By.className() etc.
    * @return SelenideElement
    */
@@ -404,7 +416,8 @@ public class Selenide {
   }
 
   /**
-   * Find the Nth element matching given criteria
+   * Locates the Nth element matching given criteria
+   * ATTENTION! This method doesn't start any search yet!
    * @param criteria instance of By: By.id(), By.className() etc.
    * @param index 0..N
    * @return SelenideElement
@@ -414,7 +427,8 @@ public class Selenide {
   }
 
   /**
-   * Find all elements matching given CSS selector
+   * Locates all elements matching given CSS selector
+   * ATTENTION! This method doesn't start any search yet!
    * @param criteria instance of By: By.id(), By.className() etc.
    * @return empty list if element was no found
    */
