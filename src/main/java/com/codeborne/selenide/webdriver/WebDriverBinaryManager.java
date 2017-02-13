a3376f55a4aaf2f2666ff65e38e7bf691e5c3bad
SergeyPirogov <Semen4ik20>
2017-02-13 15:03:40 +0200

    isEmptyProperty deleted because it's not required

--- src/main/java/com/codeborne/selenide/webdriver/WebDriverBinaryManager.java
+++ src/main/java/com/codeborne/selenide/webdriver/WebDriverBinaryManager.java
@@ -14,39 +14,23 @@ import static com.codeborne.selenide.WebDriverRunner.*;
 public class WebDriverBinaryManager {
   private static final Logger log = Logger.getLogger(WebDriverFactory.class.getName());
 
-  private static final String EDGE_PROPERTY = "webdriver.edge.driver";
-  private static final String CHROME_PROPERTY = "webdriver.chrome.driver";
-  private static final String IE_PROPERTY = "webdriver.ie.driver";
-  private static final String OPERA_PROPERTY = "webdriver.opera.driver";
-  private static final String PHANTOM_PROPERTY = "phantomjs.binary.path";
-  private static final String MARIONETTE_PROPERTY = "webdriver.gecko.driver";
-
   static void setupBinaryPath() {
     try {
-      if (isChrome() &&
-          isEmptyProperty(CHROME_PROPERTY)) {
+      if (isChrome()) {
         ChromeDriverManager.getInstance().setup();
-      } else if (isEdge() &&
-          isEmptyProperty(EDGE_PROPERTY)) {
+      } else if (isEdge()) {
         EdgeDriverManager.getInstance().setup();
-      } else if (isIE() && isEmptyProperty(IE_PROPERTY)) {
+      } else if (isIE()) {
         InternetExplorerDriverManager.getInstance().setup();
-      } else if (isOpera() &&
-          isEmptyProperty(OPERA_PROPERTY)) {
+      } else if (isOpera()) {
         OperaDriverManager.getInstance().setup();
-      } else if (isPhantomjs() &&
-          isEmptyProperty(PHANTOM_PROPERTY)) {
+      } else if (isPhantomjs()) {
         PhantomJsDriverManager.getInstance().setup();
-      } else if (isMarionette() &&
-          isEmptyProperty(MARIONETTE_PROPERTY)) {
+      } else if (isMarionette()) {
         FirefoxDriverManager.getInstance().setup();
       }
     } catch (final Exception ex) {
       log.warning("Problem to load driver binary for " + Configuration.browser);
     }
-  }
-
-  private static boolean isEmptyProperty(String property) {
-    return StringUtils.isBlank(System.getProperty(property));
   }
 }
