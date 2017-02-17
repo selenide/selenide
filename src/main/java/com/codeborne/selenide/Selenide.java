908d406c53207a41687b6824199fab835ab32c5b
Erik Khalimov <biomaks@gmail.com>
2017-02-16 21:45:44 -0600

    Added clearBrowserCookies() and clearBrowserLocalStorage() methods to Selenide

--- src/main/java/com/codeborne/selenide/Selenide.java
+++ src/main/java/com/codeborne/selenide/Selenide.java
@@ -144,7 +144,7 @@ public class Selenide {
    * Open a web page using Basic Auth credentials and create PageObject for it.
    * @return PageObject of given class
    */
-  public static <PageObjectClass> PageObjectClass open(String relativeOrAbsoluteUrl, 
+  public static <PageObjectClass> PageObjectClass open(String relativeOrAbsoluteUrl,
                                                        String domain, String login, String password,
                                                        Class<PageObjectClass> pageObjectClassClass) {
     open(relativeOrAbsoluteUrl, domain, login, password);
@@ -546,10 +546,10 @@ public class Selenide {
   /**
    * Switch to window/tab/frame/parentFrame/innerFrame/alert.
    * Allows switching to window by title, index, name etc.
-   * 
+   *
    * Similar to org.openqa.selenium.WebDriver#switchTo(), but all methods wait until frame/window/alert
    * appears if it's not visible yet (like other Selenide methods).
-   * 
+   *
    * @return SelenideTargetLocator
    */
   public static SelenideTargetLocator switchTo() {
@@ -589,12 +589,12 @@ public class Selenide {
 
   /**
    * Create a org.openqa.selenium.support.ui.FluentWait instance with Selenide timeout/polling.
-   * 
-   * Sample usage: 
+   *
+   * Sample usage:
    * {@code
    *   Wait().until(invisibilityOfElementLocated(By.id("magic-id")));
    * }
-   * 
+   *
    * @return instance of org.openqa.selenium.support.ui.FluentWait
    */
   public static FluentWait<WebDriver> Wait() {
@@ -655,7 +655,7 @@ public class Selenide {
     } catch (WebDriverException | UnsupportedOperationException cannotExecuteJs) {
       log.severe(cannotExecuteJs.toString());
       return emptyList();
-    } 
+    }
   }
 
   private static List<String> errorsFromList(List<Object> errors) {
@@ -741,6 +741,25 @@ public class Selenide {
    */
   public static List<String> getWebDriverLogs(String logType, Level logLevel) {
     return listToString(getLogEntries(logType, logLevel));
+  }
+
+  /**
+   * Clear browser cookies.
+   *
+   * In case if you are trying to avoid restarting browser
+   *
+   */
+  public static void clearBrowserCookies() {
+    getWebDriver().manage().deleteAllCookies();
+  }
+
+  /**
+   *  Clear browser local storage.
+   *
+   *  In case if you need to be sure that browser's localStorage is empty
+   */
+  public static void clearBrowserLocalStorage() {
+    executeJavaScript("localStorage.clear()");
   }
 
   private static List<LogEntry> getLogEntries(String logType, Level logLevel) {
---
+++ src/test/java/integration/ClearLocalStorageTest.java
@@ -0,0 +1,26 @@
+package integration;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static com.codeborne.selenide.Selenide.clearBrowserLocalStorage;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+import static org.junit.Assert.assertTrue;
+
+public class ClearLocalStorageTest extends IntegrationTest {
+    @Before
+    public void addDataToLocalStorage() {
+        executeJavaScript("localStorage.setItem('item1', 'item1');");
+        executeJavaScript("localStorage.setItem('item2', 'item2');");
+        Long storageLength = executeJavaScript("return localStorage.length;");
+        assertTrue(storageLength > 0);
+    }
+
+    @Test
+    public void clearLocalStorageTest() {
+        clearBrowserLocalStorage();
+        Long storageLength = executeJavaScript("return localStorage.length;");
+        assertTrue(storageLength == 0);
+    }
+
+}
