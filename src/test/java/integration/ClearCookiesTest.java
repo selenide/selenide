1c87b8ac41e270d2c2f33a509364d9fa66c45182
Erik Khalimov <biomaks@gmail.com>
2017-02-18 16:57:36 -0600

    fixed indentation

--- src/test/java/integration/ClearCookiesTest.java
+++ src/test/java/integration/ClearCookiesTest.java
@@ -12,18 +12,18 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeFalse;
 
 public class ClearCookiesTest extends IntegrationTest {
-    @Before
-    public void addCookiesBeforeTest() {
-        getWebDriver().manage().addCookie(new Cookie("username", "John Doe"));
-        Set<Cookie> cookieSet = getWebDriver().manage().getCookies();
-        assumeFalse(cookieSet.isEmpty());
-    }
+  @Before
+  public void addCookiesBeforeTest() {
+    getWebDriver().manage().addCookie(new Cookie("username", "John Doe"));
+    Set<Cookie> cookieSet = getWebDriver().manage().getCookies();
+    assumeFalse(cookieSet.isEmpty());
+  }
 
 
-    @Test
-    public void clearCookieTest() {
-        clearBrowserCookies();
-        Set<Cookie> cookieSet = getWebDriver().manage().getCookies();
-        assertTrue(cookieSet.isEmpty());
-    }
+  @Test
+  public void clearCookieTest() {
+    clearBrowserCookies();
+    Set<Cookie> cookieSet = getWebDriver().manage().getCookies();
+    assertTrue(cookieSet.isEmpty());
+  }
 }
--- src/test/java/integration/ClearLocalStorageTest.java
+++ src/test/java/integration/ClearLocalStorageTest.java
@@ -9,19 +9,19 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeTrue;
 
 public class ClearLocalStorageTest extends IntegrationTest {
-    @Before
-    public void addDataToLocalStorage() {
-        executeJavaScript("localStorage.setItem('item1', 'item1');");
-        executeJavaScript("localStorage.setItem('item2', 'item2');");
-        Long storageLength = executeJavaScript("return localStorage.length;");
-        assumeTrue(storageLength > 0);
-    }
+  @Before
+  public void addDataToLocalStorage() {
+    executeJavaScript("localStorage.setItem('item1', 'item1');");
+    executeJavaScript("localStorage.setItem('item2', 'item2');");
+    Long storageLength = executeJavaScript("return localStorage.length;");
+    assumeTrue(storageLength > 0);
+  }
 
-    @Test
-    public void clearLocalStorageTest() {
-        clearBrowserLocalStorage();
-        Long storageLength = executeJavaScript("return localStorage.length;");
-        assertTrue(storageLength == 0);
-    }
+  @Test
+  public void clearLocalStorageTest() {
+    clearBrowserLocalStorage();
+    Long storageLength = executeJavaScript("return localStorage.length;");
+    assertTrue(storageLength == 0);
+  }
 
 }
