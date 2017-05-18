99b01e07532030ff38d83b0e783a5c5be76483d6
ia.baranov <ia.baranov@jet.msk.su>
2017-05-19 00:27:55 +0300

    Added runtime driver type checks in method WebDriverRunner#isIE for Internet Explorer

--- src/main/java/com/codeborne/selenide/WebDriverRunner.java
+++ src/main/java/com/codeborne/selenide/WebDriverRunner.java
@@ -2,270 +2,285 @@ package com.codeborne.selenide;
 
 import com.codeborne.selenide.impl.WebDriverContainer;
 import com.codeborne.selenide.impl.WebDriverThreadLocalContainer;
+import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.events.WebDriverEventListener;
 
 import static com.codeborne.selenide.Configuration.browser;
 
 public class WebDriverRunner {
-  public static WebDriverContainer webdriverContainer = new WebDriverThreadLocalContainer();
+    public static WebDriverContainer webdriverContainer = new WebDriverThreadLocalContainer();
 
-  public static final String CHROME = "chrome";
-  public static final String INTERNET_EXPLORER = "ie";
-  public static final String EDGE = "edge";
-  public static final String FIREFOX = "firefox";
-  /**
-   * Marinonette Driver for Firefox is the same as Gecko Driver
-   */
-  public static final String MARIONETTE = "marionette";
-  /**
-   * Marinonette Driver for Firefox is the same as Gecko Driver
-   */
-  public static final String GECKO = "gecko";
+    public static final String CHROME = "chrome";
+    public static final String INTERNET_EXPLORER = "ie";
+    public static final String EDGE = "edge";
+    public static final String FIREFOX = "firefox";
+    /**
+     * Marinonette Driver for Firefox is the same as Gecko Driver
+     */
+    public static final String MARIONETTE = "marionette";
+    /**
+     * Marinonette Driver for Firefox is the same as Gecko Driver
+     */
+    public static final String GECKO = "gecko";
 
-  /**
-   * To use Safari webdriver, you need to include extra dependency to your project:
-   * &lt;dependency org="org.seleniumhq.selenium" name="selenium-safari-driver" rev="2.+" conf="test-&gt;default"/&gt;
-   */
-  public static final String SAFARI = "safari";
+    /**
+     * To use Safari webdriver, you need to include extra dependency to your project:
+     * &lt;dependency org="org.seleniumhq.selenium" name="selenium-safari-driver" rev="2.+" conf="test-&gt;default"/&gt;
+     */
+    public static final String SAFARI = "safari";
 
-  /**
-   * To use HtmlUnitDriver, you need to include extra dependency to your project:
-   * <dependency org="org.seleniumhq.selenium" name="selenium-htmlunit-driver" rev="2.+" conf="test->default"/>
-   *
-   * It's also possible to run HtmlUnit driver emulating different browsers:
-   * <p>
-   * java -Dbrowser=htmlunit:firefox
-   * </p>
-   * <p>
-   * java -Dbrowser=htmlunit:chrome
-   * </p>
-   * <p>
-   * java -Dbrowser=htmlunit:internet explorer   (default)
-   * </p>
-   * etc.
-   */
-  public static final String HTMLUNIT = "htmlunit";
+    /**
+     * To use HtmlUnitDriver, you need to include extra dependency to your project:
+     * <dependency org="org.seleniumhq.selenium" name="selenium-htmlunit-driver" rev="2.+" conf="test->default"/>
+     * <p>
+     * It's also possible to run HtmlUnit driver emulating different browsers:
+     * <p>
+     * java -Dbrowser=htmlunit:firefox
+     * </p>
+     * <p>
+     * java -Dbrowser=htmlunit:chrome
+     * </p>
+     * <p>
+     * java -Dbrowser=htmlunit:internet explorer   (default)
+     * </p>
+     * etc.
+     */
+    public static final String HTMLUNIT = "htmlunit";
 
-  /**
-   * To use PhantomJS, you need to set system property: -Dselenide.browser=phantomjs
-   */
-  public static final String PHANTOMJS = "phantomjs";
+    /**
+     * To use PhantomJS, you need to set system property: -Dselenide.browser=phantomjs
+     */
+    public static final String PHANTOMJS = "phantomjs";
 
-  /**
-   * To use OperaDriver, you need to include extra dependency to your project:
-   * &lt;dependency org="com.opera" name="operadriver" rev="1.5" conf="test-&gt;default"/&gt;
-   */
-  public static final String OPERA = "opera";
-  
-  /**
-   * To use JbrowserDriver, you need to include extra dependency to your project:
-   * <dependency org="com.machinepublishers" name="jbrowserdriver" rev="[0.13.0, 2.0)" conf="test-&gt;default"/&gt;
-   * 
-   * Note: You need minimum of Java 8.
-   */
-  public static final String JBROWSER = "jbrowser";
+    /**
+     * To use OperaDriver, you need to include extra dependency to your project:
+     * &lt;dependency org="com.opera" name="operadriver" rev="1.5" conf="test-&gt;default"/&gt;
+     */
+    public static final String OPERA = "opera";
 
-  /**
-   * Use this method BEFORE opening a browser to add custom event listeners to webdriver.
-   * @param listener your listener of webdriver events
-   */
-  public static void addListener(WebDriverEventListener listener) {
-    webdriverContainer.addListener(listener);
-  }
+    /**
+     * To use JbrowserDriver, you need to include extra dependency to your project:
+     * <dependency org="com.machinepublishers" name="jbrowserdriver" rev="[0.13.0, 2.0)" conf="test-&gt;default"/&gt;
+     * <p>
+     * Note: You need minimum of Java 8.
+     */
+    public static final String JBROWSER = "jbrowser";
 
-  /**
-   * Tell Selenide use your provided WebDriver instance.
-   * Use it if you need a custom logic for creating WebDriver.
-   *
-   * It's recommended not to use implicit wait with this driver, because Selenide handles timing issues explicitly.
-   *
-   * <p/>
-   * <p/>
-   *
-   * NB! Be sure to call this method before calling <code>open(url)</code>.
-   * Otherwise Selenide will create its own WebDriver instance and would not close it.
-   *
-   * <p>
-   * NB! When using your custom webdriver, you are responsible for closing it.
-   *     Selenide will not take care of it.
-   * </p>
-   *
-   * <p>
-   * NB! Webdriver instance should be created and used in the same thread.
-   *     A typical error is to create webdriver instance in one thread and use it in another.
-   *     Selenide does not support it.
-   *     If you really need using multiple threads, please use #com.codeborne.selenide.WebDriverProvider
-   * </p>
-   *
-   * <p>
-   * P.S. Alternatively, you can run tests with system property
-   * <pre>  -Dbrowser=com.my.WebDriverFactory</pre>
-   *
-   * which should implement interface #com.codeborne.selenide.WebDriverProvider
-   * </p>
-   */
-  public static void setWebDriver(WebDriver webDriver) {
-    webdriverContainer.setWebDriver(webDriver);
-  }
+    /**
+     * Use this method BEFORE opening a browser to add custom event listeners to webdriver.
+     *
+     * @param listener your listener of webdriver events
+     */
+    public static void addListener(WebDriverEventListener listener) {
+        webdriverContainer.addListener(listener);
+    }
 
-  /**
-   * Get the underlying instance of Selenium WebDriver.
-   * This can be used for any operations directly with WebDriver.
-   */
-  public static WebDriver getWebDriver() {
-    return webdriverContainer.getWebDriver();
-  }
+    /**
+     * Tell Selenide use your provided WebDriver instance.
+     * Use it if you need a custom logic for creating WebDriver.
+     * <p>
+     * It's recommended not to use implicit wait with this driver, because Selenide handles timing issues explicitly.
+     * <p>
+     * <p/>
+     * <p/>
+     * <p>
+     * NB! Be sure to call this method before calling <code>open(url)</code>.
+     * Otherwise Selenide will create its own WebDriver instance and would not close it.
+     * <p>
+     * <p>
+     * NB! When using your custom webdriver, you are responsible for closing it.
+     * Selenide will not take care of it.
+     * </p>
+     * <p>
+     * <p>
+     * NB! Webdriver instance should be created and used in the same thread.
+     * A typical error is to create webdriver instance in one thread and use it in another.
+     * Selenide does not support it.
+     * If you really need using multiple threads, please use #com.codeborne.selenide.WebDriverProvider
+     * </p>
+     * <p>
+     * <p>
+     * P.S. Alternatively, you can run tests with system property
+     * <pre>  -Dbrowser=com.my.WebDriverFactory</pre>
+     *
+     * which should implement interface #com.codeborne.selenide.WebDriverProvider
+     * </p>
+     */
+    public static void setWebDriver(WebDriver webDriver) {
+        webdriverContainer.setWebDriver(webDriver);
+    }
 
-  /**
-   * Sets Selenium Proxy instance
-   *
-   * @param webProxy
-   */
-  public static void setProxy(Proxy webProxy) {
-    webdriverContainer.setProxy(webProxy);
-  }
+    /**
+     * Get the underlying instance of Selenium WebDriver.
+     * This can be used for any operations directly with WebDriver.
+     */
+    public static WebDriver getWebDriver() {
+        return webdriverContainer.getWebDriver();
+    }
 
-  /**
-   * Get the underlying instance of Selenium WebDriver, and assert that it's still alive.
-   * @return new instance of WebDriver if the previous one has been closed meanwhile.
-   */
-  public static WebDriver getAndCheckWebDriver() {
-    return webdriverContainer.getAndCheckWebDriver();
-  }
+    /**
+     * Sets Selenium Proxy instance
+     *
+     * @param webProxy
+     */
+    public static void setProxy(Proxy webProxy) {
+        webdriverContainer.setProxy(webProxy);
+    }
 
-  /**
-   * Close the browser if it's open
-   */
-  public static void closeWebDriver() {
-    webdriverContainer.closeWebDriver();
-  }
+    /**
+     * Get the underlying instance of Selenium WebDriver, and assert that it's still alive.
+     *
+     * @return new instance of WebDriver if the previous one has been closed meanwhile.
+     */
+    public static WebDriver getAndCheckWebDriver() {
+        return webdriverContainer.getAndCheckWebDriver();
+    }
 
-  /**
-   * @return true iff instance of Selenium WebDriver is started in current thread
-   */
-  public static boolean hasWebDriverStarted() {
-    return webdriverContainer.hasWebDriverStarted();
-  }
+    /**
+     * Close the browser if it's open
+     */
+    public static void closeWebDriver() {
+        webdriverContainer.closeWebDriver();
+    }
 
-  /**
-   * Is Selenide configured to use Firefox browser
-   */
-  public static boolean isFirefox() {
-    return FIREFOX.equalsIgnoreCase(browser);
-  }
+    /**
+     * @return true iff instance of Selenium WebDriver is started in current thread
+     */
+    public static boolean hasWebDriverStarted() {
+        return webdriverContainer.hasWebDriverStarted();
+    }
 
-  /**
-   * Is Selenide configured to use Marionette (Gecko) driver
-   */
-  public static boolean isMarionette() {
-    return MARIONETTE.equalsIgnoreCase(browser) || GECKO.equalsIgnoreCase(browser);
-  }
+    /**
+     * Is Selenide configured to use Firefox browser
+     */
+    public static boolean isFirefox() {
+        return FIREFOX.equalsIgnoreCase(browser);
+    }
 
-  /**
-   * Is Selenide configured to use Chrome browser
-   */
-  public static boolean isChrome() {
-    return CHROME.equalsIgnoreCase(browser);
-  }
+    /**
+     * Is Selenide configured to use Marionette (Gecko) driver
+     */
+    public static boolean isMarionette() {
+        return MARIONETTE.equalsIgnoreCase(browser) || GECKO.equalsIgnoreCase(browser);
+    }
 
-  /**
-   * Is Selenide configured to use Internet Explorer browser
-   */
-  public static boolean isIE() {
-    return INTERNET_EXPLORER.equalsIgnoreCase(browser);
-  }
+    /**
+     * Is Selenide configured to use Chrome browser
+     */
+    public static boolean isChrome() {
+        return CHROME.equalsIgnoreCase(browser);
+    }
 
-  /**
-   * Is Selenide configured to use Microsoft EDGE browser
-   */
-  public static boolean isEdge() {
-    return EDGE.equalsIgnoreCase(browser);
-  }
+    /**
+     * Is Selenide configured to use Internet Explorer browser
+     */
+    public static boolean isIE() {
+        return INTERNET_EXPLORER.equalsIgnoreCase(browser) ||
+                (hasWebDriverStarted() &&
+                        (getWebDriver() instanceof InternetExplorerDriver ||
+                                (getWebDriver() instanceof RemoteWebDriver && remoteDriverIsIE())));
+    }
 
-  /**
-   * Is Selenide configured to user Safari browser
-   */
-  public static boolean isSafari() {
-    return SAFARI.equalsIgnoreCase(browser);
-  }
+    private static boolean remoteDriverIsIE() {
+        Capabilities capabilities = ((RemoteWebDriver) getWebDriver()).getCapabilities();
+        String browserName = capabilities == null ? null : capabilities.getBrowserName();
+        return !StringUtils.isEmpty(browserName) && browserName.equals("internet explorer");
+    }
 
-  /**
-   * Is Selenide configured to use headless browser (HtmlUnit or PhantomJS)
-   */
-  public static boolean isHeadless() {
-    return isHtmlUnit() || isPhantomjs();
-  }
+    /**
+     * Is Selenide configured to use Microsoft EDGE browser
+     */
+    public static boolean isEdge() {
+        return EDGE.equalsIgnoreCase(browser);
+    }
 
-  /**
-   * Does this browser support "alert" and "confirm" dialogs.
-   */
-  public static boolean supportsModalDialogs() {
-    return !isHeadless() && !isSafari();
-  }
+    /**
+     * Is Selenide configured to user Safari browser
+     */
+    public static boolean isSafari() {
+        return SAFARI.equalsIgnoreCase(browser);
+    }
 
-  /**
-   * Does this browser support javascript
-   */
-  public static boolean supportsJavascript() {
-    return hasWebDriverStarted() && getWebDriver() instanceof JavascriptExecutor;
-  }
+    /**
+     * Is Selenide configured to use headless browser (HtmlUnit or PhantomJS)
+     */
+    public static boolean isHeadless() {
+        return isHtmlUnit() || isPhantomjs();
+    }
 
-  /**
-   * Is Selenide configured to use HtmlUnit browser
-   */
-  public static boolean isHtmlUnit() {
-    return browser != null && browser.startsWith(HTMLUNIT);
-  }
+    /**
+     * Does this browser support "alert" and "confirm" dialogs.
+     */
+    public static boolean supportsModalDialogs() {
+        return !isHeadless() && !isSafari();
+    }
 
-  /**
-   * Is Selenide configured to use PhantomJS browser
-   */
-  public static boolean isPhantomjs() {
-    return PHANTOMJS.equalsIgnoreCase(browser);
-  }
+    /**
+     * Does this browser support javascript
+     */
+    public static boolean supportsJavascript() {
+        return hasWebDriverStarted() && getWebDriver() instanceof JavascriptExecutor;
+    }
 
-  /**
-   * Is Selenide configured to use Opera browser
-   */
-  public static boolean isOpera() {
-    return OPERA.equalsIgnoreCase(browser);
-  }
-  
-  /**
-   * Is Selenide configured to use JBrowser browser
-   */
-  public static boolean isJBrowser() {
-    return JBROWSER.equalsIgnoreCase(browser);
-  }
+    /**
+     * Is Selenide configured to use HtmlUnit browser
+     */
+    public static boolean isHtmlUnit() {
+        return browser != null && browser.startsWith(HTMLUNIT);
+    }
 
-  /**
-   * Delete all the browser cookies
-   */
-  public static void clearBrowserCache() {
-    webdriverContainer.clearBrowserCache();
-  }
+    /**
+     * Is Selenide configured to use PhantomJS browser
+     */
+    public static boolean isPhantomjs() {
+        return PHANTOMJS.equalsIgnoreCase(browser);
+    }
 
-  /**
-   * @return the source (HTML) of current page
-   */
-  public static String source() {
-    return webdriverContainer.getPageSource();
-  }
+    /**
+     * Is Selenide configured to use Opera browser
+     */
+    public static boolean isOpera() {
+        return OPERA.equalsIgnoreCase(browser);
+    }
 
-  /**
-   * @return the URL of current page
-   */
-  public static String url() {
-    return webdriverContainer.getCurrentUrl();
-  }
+    /**
+     * Is Selenide configured to use JBrowser browser
+     */
+    public static boolean isJBrowser() {
+        return JBROWSER.equalsIgnoreCase(browser);
+    }
 
-  /**
-   * @return the URL of current frame
-   */
-  public static String currentFrameUrl() {
-    return webdriverContainer.getCurrentFrameUrl();
-  }
+    /**
+     * Delete all the browser cookies
+     */
+    public static void clearBrowserCache() {
+        webdriverContainer.clearBrowserCache();
+    }
+
+    /**
+     * @return the source (HTML) of current page
+     */
+    public static String source() {
+        return webdriverContainer.getPageSource();
+    }
+
+    /**
+     * @return the URL of current page
+     */
+    public static String url() {
+        return webdriverContainer.getCurrentUrl();
+    }
+
+    /**
+     * @return the URL of current frame
+     */
+    public static String currentFrameUrl() {
+        return webdriverContainer.getCurrentFrameUrl();
+    }
 }
