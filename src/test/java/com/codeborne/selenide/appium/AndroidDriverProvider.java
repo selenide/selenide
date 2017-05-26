e0bd4209e46d2f921f3c10b43d6433a8759cf6c8
Aliaksandr Rasolka <rosolko@gmail.com>
2017-05-26 09:51:57 +0300

    Update gradle scripts and version to 3.5

--- src/test/java/com/codeborne/selenide/appium/AndroidDriverProvider.java
+++ src/test/java/com/codeborne/selenide/appium/AndroidDriverProvider.java
@@ -17,7 +17,8 @@ public class AndroidDriverProvider implements WebDriverProvider {
     @Override
     public WebDriver createDriver(DesiredCapabilities capabilities) {
         Configuration.startMaximized = false;
-        
+        Configuration.captureJavascriptErrors = false;
+
         capabilities.setCapability(MobileCapabilityType.VERSION, "4.4.2");
         capabilities.setCapability("automationName", "Appium");
         capabilities.setCapability("platformName", "Android");
