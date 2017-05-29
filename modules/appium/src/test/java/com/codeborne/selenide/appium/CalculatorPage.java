42d9375c538a1b588cc27bcc433bc2f029719a90
Aliaksandr Rasolka <rosolko@gmail.com>
2017-05-29 17:36:12 +0300

    Indent code with 2 spaces

--- modules/appium/src/test/java/com/codeborne/selenide/appium/CalculatorPage.java
+++ modules/appium/src/test/java/com/codeborne/selenide/appium/CalculatorPage.java
@@ -7,16 +7,16 @@ import org.openqa.selenium.support.FindBy;
 public class CalculatorPage {
   @FindBy(id = "com.android.calculator2:id/plus")
   SelenideElement plus;
-  
+
   @FindBy(id = "com.android.calculator2:id/equal")
   SelenideElement equal;
-  
+
   @FindBy(id = "com.android.calculator2:id/digit2")
   SelenideElement number2;
-  
+
   @FindBy(id = "com.android.calculator2:id/digit4")
   SelenideElement number4;
-  
+
   @AndroidFindBy(className = "android.widget.EditText")
   SelenideElement result;
 }
