b09277754b0333f4ea6a6eafd2346d6c6a195816
Aliaksandr Rasolka <rosolko@gmail.com>
2017-05-29 17:36:12 +0300

    Indent code with 2 spaces

--- src/test/java/com/codeborne/selenide/appium/AndroidDriverProvider.java
+++ src/test/java/com/codeborne/selenide/appium/AndroidDriverProvider.java
@@ -1,5 +1,8 @@
 package com.codeborne.selenide.appium;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverProvider;
 import io.appium.java_client.android.AndroidDriver;
@@ -7,29 +10,26 @@ import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import static io.appium.java_client.remote.AndroidMobileCapabilityType.APP_ACTIVITY;
 import static io.appium.java_client.remote.AndroidMobileCapabilityType.APP_PACKAGE;
 
 public class AndroidDriverProvider implements WebDriverProvider {
-    @Override
-    public WebDriver createDriver(DesiredCapabilities capabilities) {
-        Configuration.startMaximized = false;
-        Configuration.captureJavascriptErrors = false;
+  @Override
+  public WebDriver createDriver(DesiredCapabilities capabilities) {
+    Configuration.startMaximized = false;
+    Configuration.captureJavascriptErrors = false;
 
-        capabilities.setCapability(MobileCapabilityType.VERSION, "4.4.2");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "0123456789ABCDEF");
-        capabilities.setCapability(APP_PACKAGE, "com.android.calculator2");
-        capabilities.setCapability(APP_ACTIVITY, "com.android.calculator2.Calculator");
+    capabilities.setCapability(MobileCapabilityType.VERSION, "4.4.2");
+    capabilities.setCapability("automationName", "Appium");
+    capabilities.setCapability("platformName", "Android");
+    capabilities.setCapability("deviceName", "0123456789ABCDEF");
+    capabilities.setCapability(APP_PACKAGE, "com.android.calculator2");
+    capabilities.setCapability(APP_ACTIVITY, "com.android.calculator2.Calculator");
 
-        try {
-            return new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
+    try {
+      return new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    } catch (MalformedURLException e) {
+      throw new RuntimeException(e);
     }
+  }
 }
--- src/test/java/com/codeborne/selenide/appium/CalculatorTest.java
+++ src/test/java/com/codeborne/selenide/appium/CalculatorTest.java
@@ -12,33 +12,33 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class CalculatorTest {
 
-    @Before
-    public void setUp() {
-        Configuration.browser = AndroidDriverProvider.class.getName();
-    }
+  @Before
+  public void setUp() {
+    Configuration.browser = AndroidDriverProvider.class.getName();
+  }
 
-    @Test
-    public void plain(){
-        $(By.id("com.android.calculator2:id/digit2")).click();
-        $(By.id("com.android.calculator2:id/plus")).click();
-        $(By.id("com.android.calculator2:id/digit4")).click();
-        $(By.id("com.android.calculator2:id/equal")).click();
+  @Test
+  public void plain() {
+    $(By.id("com.android.calculator2:id/digit2")).click();
+    $(By.id("com.android.calculator2:id/plus")).click();
+    $(By.id("com.android.calculator2:id/digit4")).click();
+    $(By.id("com.android.calculator2:id/equal")).click();
 
-        $(By.className("android.widget.EditText")).shouldHave(text("6"));
-    }
+    $(By.className("android.widget.EditText")).shouldHave(text("6"));
+  }
 
-    @Test
-    public void pageObject(){
-        CalculatorPage page = new CalculatorPage();
-        PageFactory.initElements(new SelenideAppiumFieldDecorator(getWebDriver()),
-            page
-        );
+  @Test
+  public void pageObject() {
+    CalculatorPage page = new CalculatorPage();
+    PageFactory.initElements(new SelenideAppiumFieldDecorator(getWebDriver()),
+      page
+    );
 
-        page.number2.click();
-        page.plus.click();
-        page.number4.click();
-        page.equal.click();
-        
-        page.result.shouldHave(text("6"));
-    }
+    page.number2.click();
+    page.plus.click();
+    page.number4.click();
+    page.equal.click();
+
+    page.result.shouldHave(text("6"));
+  }
 }
