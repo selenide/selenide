bf44dbd8a165e0af75cc1751036793d9ec0dad0c
Andrei Solntsev <andrei.solntsev@gmail.com>
2017-06-17 23:31:26 +0300

    Closes #551 Method `$.setValue()` should not fail if it could not trigger change event (for whatever reason).

--- src/main/java/com/codeborne/selenide/impl/Events.java
+++ src/main/java/com/codeborne/selenide/impl/Events.java
@@ -1,18 +1,33 @@
 package com.codeborne.selenide.impl;
 
+import com.codeborne.selenide.Selenide;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 
 import java.util.logging.Logger;
 
-import static com.codeborne.selenide.Selenide.executeJavaScript;
 import static com.codeborne.selenide.WebDriverRunner.supportsJavascript;
+import static java.util.Arrays.asList;
 
 public class Events {
-  private static final Logger log = Logger.getLogger(Events.class.getName());
-  
   public static Events events = new Events();
-  
+
+  Logger log = Logger.getLogger(Events.class.getName());
+  final String jsCodeToTriggerEvent =
+      "var webElement = arguments[0];\n" +
+          "var eventNames = arguments[1];\n" +
+          "for (var i = 0; i < eventNames.length; i++) {" +
+          "  if (document.createEventObject) {\n" +  // IE
+          "    var evt = document.createEventObject();\n" +
+          "    webElement.fireEvent('on' + eventNames[i], evt);\n" +
+          "  }\n" +
+          "  else {\n" +
+          "    var evt = document.createEvent('HTMLEvents');\n " +
+          "    evt.initEvent(eventNames[i], true, true );\n " +
+          "    webElement.dispatchEvent(evt);\n" +
+          "  }\n" +
+          '}';
+
   public void fireChangeEvent(WebElement element) {
     if (supportsJavascript()) {
       fireEvent(element, "change");
@@ -24,22 +39,16 @@ public class Events {
 
   public void fireEvent(WebElement element, final String... event) {
     try {
-      final String jsCodeToTriggerEvent =
-          "var webElement = arguments[0];\n" +
-              "var eventNames = arguments[1];\n" +
-              "for (var i = 0; i < eventNames.length; i++) {" +
-              "  if (document.createEventObject) {\n" +  // IE
-              "    var evt = document.createEventObject();\n" +
-              "    webElement.fireEvent('on' + eventNames[i], evt);\n" +
-              "  }\n" +
-              "  else {\n" +
-              "    var evt = document.createEvent('HTMLEvents');\n " +
-              "    evt.initEvent(eventNames[i], true, true );\n " +
-              "    webElement.dispatchEvent(evt);\n" +
-              "  }\n" +
-              '}';
-      executeJavaScript(jsCodeToTriggerEvent, element, event);
-    } catch (StaleElementReferenceException ignore) {
+      executeJavaScript(element, event);
     }
+    catch (StaleElementReferenceException ignore) {
+    }
+    catch (Exception e) {
+      log.warning("Failed to trigger events " + asList(event) + ": " + Cleanup.of.webdriverExceptionMessage(e));
+    }
+  }
+
+  void executeJavaScript(WebElement element, String... event) {
+    Selenide.executeJavaScript(jsCodeToTriggerEvent, element, event);
   }
 }
