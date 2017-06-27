4af74cdb79fbf2529ceaa2d27068b2a2ba7eb3da
Aliaksandr Rasolka <rosolko@gmail.com>
2017-06-27 10:14:27 +0300

    Update selenide to 4.5 version. Restart application between tests. Create method to init object pages(initially it's screen).

--- src/test/java/com/codeborne/selenide/appium/CalculatorPage.java
+++ src/test/java/com/codeborne/selenide/appium/CalculatorPage.java
@@ -2,19 +2,18 @@ package com.codeborne.selenide.appium;
 
 import com.codeborne.selenide.SelenideElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
-import org.openqa.selenium.support.FindBy;
 
 class CalculatorPage {
-  @FindBy(id = "op_add")
+  @AndroidFindBy(id = "op_add")
   SelenideElement plus;
 
-  @FindBy(id = "eq")
+  @AndroidFindBy(id = "eq")
   SelenideElement equal;
 
-  @FindBy(id = "digit_2")
+  @AndroidFindBy(id = "digit_2")
   SelenideElement number2;
 
-  @FindBy(id = "digit_4")
+  @AndroidFindBy(id = "digit_4")
   SelenideElement number4;
 
   @AndroidFindBy(id = "formula")
