5adef658d32cc112c4b96a63ea3888599d52e06a
Hyunil Shin <anyone.can.test@gmail.com>
2017-09-10 00:19:58 +0900

    fix checkstyle

--- src/test/java/com/codeborne/selenide/collections/TextsInAnyOrderTest.java
+++ src/test/java/com/codeborne/selenide/collections/TextsInAnyOrderTest.java
@@ -4,11 +4,7 @@ import org.junit.Test;
 import org.openqa.selenium.WebElement;
 
 import static java.util.Arrays.asList;
-import static java.util.Collections.emptyList;
-import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -32,7 +28,7 @@ public class TextsInAnyOrderTest {
     testApplyMethod(false, texts);
   }
   
-   @Test
+  @Test
   public void testApplyWithBiggerSize() {
     TextsInAnyOrder texts = new TextsInAnyOrder(asList("Two", "One", "four", "three"));
     testApplyMethod(false, texts);
@@ -41,7 +37,7 @@ public class TextsInAnyOrderTest {
 
 
   private void testApplyMethod(boolean shouldMatch, TextsInAnyOrder texts) {
-   WebElement mockElement1 = mock(WebElement.class);
+    WebElement mockElement1 = mock(WebElement.class);
     WebElement mockElement2 = mock(WebElement.class);
     WebElement mockElement3 = mock(WebElement.class);
 
@@ -49,8 +45,6 @@ public class TextsInAnyOrderTest {
     when(mockElement2.getText()).thenReturn("Two");
     when(mockElement3.getText()).thenReturn("Three");
     assertEquals(shouldMatch, texts.apply(asList(mockElement1, mockElement2, mockElement3)));
-  
-	  
   }
   
   @Test
