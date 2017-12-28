0c2e62dfaf963f857caef65fdd58e1772813d819
Aliaksandr Rasolka <rosolko@gmail.com>
2017-12-28 17:02:37 +0300

    Try to fix codecov patch by simplify assert.

--- src/test/java/integration/UserAgentTest.java
+++ src/test/java/integration/UserAgentTest.java
@@ -1,51 +1,30 @@
 package integration;
 
 import com.codeborne.selenide.Configuration;
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
 import static com.codeborne.selenide.Selenide.getUserAgent;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.isHtmlUnit;
-import static integration.UserAgentTest.CaseInsensitiveSubstringMatcher.containsIgnoringCase;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.text.IsEmptyString.isEmptyOrNullString;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeFalse;
 
 public class UserAgentTest extends IntegrationTest {
   @Test
   public void currentUserAgentTest() {
+    Configuration.browser = "chrome";
     assumeFalse(isHtmlUnit());
 
     open("/start_page.html");
     String userAgent = getUserAgent();
+    String browser = Configuration.browser;
 
     assertThat(userAgent, not(isEmptyOrNullString()));
-    assertThat(userAgent, containsIgnoringCase(Configuration.browser));
-  }
-
-  public static class CaseInsensitiveSubstringMatcher extends TypeSafeMatcher<String> {
-    private final String subString;
-
-    private CaseInsensitiveSubstringMatcher(final String subString) {
-      this.subString = subString;
-    }
-
-    static Matcher<String> containsIgnoringCase(final String subString) {
-      return new CaseInsensitiveSubstringMatcher(subString);
-    }
-
-    @Override
-    protected boolean matchesSafely(final String actualString) {
-      return actualString.toLowerCase().contains(this.subString.toLowerCase());
-    }
-
-    @Override
-    public void describeTo(final Description description) {
-      description.appendText("containing substring \"" + this.subString + "\"");
-    }
+    assertTrue(String.format("Current user agent [%s] should belong to '%s' browser", userAgent, browser),
+      StringUtils.containsIgnoreCase(userAgent, browser));
   }
 }
