42c1d9214e4eac8699792a187e6afb2d6c325d4f
Teslarasu Bogdan <teslarasu.bogdan@gmail.com>
2018-01-19 00:55:23 +0200

    #675: Chrome browser gets maximized if Configuration.startMaximized is set.

--- src/test/java/com/codeborne/selenide/webdriver/WebDriverFactoryTest.java
+++ src/test/java/com/codeborne/selenide/webdriver/WebDriverFactoryTest.java
@@ -1,18 +1,14 @@
 package com.codeborne.selenide.webdriver;
 
-import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-
 import com.codeborne.selenide.Configuration;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
+
+import static org.mockito.Mockito.*;
 
 public class WebDriverFactoryTest {
 
@@ -54,10 +50,25 @@ public class WebDriverFactoryTest {
   public void canMaximizeBrowserWindow_chrome() {
     Configuration.startMaximized = true;
     Configuration.browser = "chrome";
-    doReturn(new Dimension(1600, 1200)).when(factory).getScreenSize();
 
     factory.adjustBrowserSize(webdriver);
 
+    verify(webdriver.manage().window()).maximize();
+  }
+
+  @Test
+  public void canMaximizeBrowserWindow_chromeException() {
+    Configuration.startMaximized = true;
+    Configuration.browser = "chrome";
+
+    WebDriver.Options options = Mockito.mock(WebDriver.Options.class);
+    WebDriver.Window window = Mockito.mock(WebDriver.Window.class);
+    doReturn(window).when(options).window();
+    doReturn(options).when(webdriver).manage();
+    doThrow(new RuntimeException("Chrome webdriver cannot maximize the window!")).when(window).maximize();
+    doReturn(new Dimension(1600, 1200)).when(factory).getScreenSize();
+
+    factory.adjustBrowserSize(webdriver);
     verify(webdriver.manage().window()).setSize(new Dimension(1600, 1200));
     verify(webdriver.manage().window()).setPosition(new Point(0, 0));
   }
