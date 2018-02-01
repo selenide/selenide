f3f7882fa692288b9c6a1e4c21cd9f569d507fac
Aliaksandr Rasolka <rosolko@gmail.com>
2018-02-01 10:14:42 +0300

    Add ability to set browser window position

---
+++ src/test/java/integration/BrowserPositionTest.java
@@ -0,0 +1,47 @@
+package integration;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Point;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.isHtmlUnit;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assume.assumeFalse;
+
+public class BrowserPositionTest extends IntegrationTest {
+  String position;
+  Point point;
+
+  @Before
+  public void prepareTestData() {
+    position = "30x60";
+    String[] coordinates = position.split("x");
+    int x = Integer.parseInt(coordinates[0]);
+    int y = Integer.parseInt(coordinates[1]);
+    point = new Point(x, y);
+  }
+
+  @Test
+  public void ableToSetBrowserPosition() {
+    assumeFalse(isHtmlUnit());
+
+    Configuration.browserPosition = position;
+
+    open("/start_page.html");
+
+    assertEquals(point, WebDriverRunner.getWebDriver().manage().window().getPosition());
+  }
+
+  @Test
+  public void ableToOpenBrowserWithoutPosition() {
+    assumeFalse(isHtmlUnit());
+
+    open("/start_page.html");
+
+    assertNotEquals(point, WebDriverRunner.getWebDriver().manage().window().getPosition());
+  }
+}
