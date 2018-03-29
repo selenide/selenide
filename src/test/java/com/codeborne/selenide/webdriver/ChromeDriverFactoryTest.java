4fe2f5122b84c1948873945715fbb9224e88f0eb
Dmitry Romashov <framebassman@gmail.com>
2018-03-29 11:34:28 +0300

    Fix checkstyle errors

--- src/test/java/com/codeborne/selenide/webdriver/ChromeDriverFactoryTest.java
+++ src/test/java/com/codeborne/selenide/webdriver/ChromeDriverFactoryTest.java
@@ -1,5 +1,13 @@
 package com.codeborne.selenide.webdriver;
 
+import org.junit.After;
+import org.junit.Test;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+import java.util.Map;
+
 import static com.codeborne.selenide.webdriver.SeleniumCapabilitiesHelper.getBrowserLaunchArgs;
 import static com.codeborne.selenide.webdriver.SeleniumCapabilitiesHelper.getBrowserLaunchPrefs;
 import static org.hamcrest.CoreMatchers.is;
@@ -7,16 +15,6 @@ import static org.hamcrest.collection.IsMapContaining.hasEntry;
 import static org.hamcrest.core.IsCollectionContaining.hasItems;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.List;
-import java.util.Map;
-
-import org.junit.After;
-import org.junit.Test;
-import org.openqa.selenium.Proxy;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 @SuppressWarnings("unchecked")
 public class ChromeDriverFactoryTest {
@@ -32,20 +30,20 @@ public class ChromeDriverFactoryTest {
     System.clearProperty(CHROME_OPTIONS_PREFS);
   }
 
-    @Test
-    public void transferChromeOptionArgumentsFromSystemPropsToDriver() {
-      // abdd, --abcd, xcvcd=123, a="def", b="a\"d", --user-agent="A,B") should work
-      System.setProperty("chromeoptions.args", "abdd,--abcd,xcvcd=123,a=\"def\",b=\"a\\\"d\",--user-agent=\"A,B\"");
-      ChromeOptions chromeOptions = new ChromeDriverFactory().createChromeOptions(proxy);
-      List<String> optionArguments = getBrowserLaunchArgs(ChromeOptions.CAPABILITY, chromeOptions);
+  @Test
+  public void transferChromeOptionArgumentsFromSystemPropsToDriver() {
+    // abdd, --abcd, xcvcd=123, a="def", b="a\"d", --user-agent="A,B") should work
+    System.setProperty("chromeoptions.args", "abdd,--abcd,xcvcd=123,a=\"def\",b=\"a\\\"d\",--user-agent=\"A,B\"");
+    ChromeOptions chromeOptions = new ChromeDriverFactory().createChromeOptions(proxy);
+    List<String> optionArguments = getBrowserLaunchArgs(ChromeOptions.CAPABILITY, chromeOptions);
 
-      assertThat(optionArguments, hasItems("abdd"));
-      assertThat(optionArguments, hasItems("--abcd"));
-      assertThat(optionArguments, hasItems("xcvcd=123"));
-      assertThat(optionArguments, hasItems("a=def"));
-      assertThat(optionArguments, hasItems("b=a\"d"));
-      assertThat(optionArguments, hasItems("--user-agent=A,B"));
-    }
+    assertThat(optionArguments, hasItems("abdd"));
+    assertThat(optionArguments, hasItems("--abcd"));
+    assertThat(optionArguments, hasItems("xcvcd=123"));
+    assertThat(optionArguments, hasItems("a=def"));
+    assertThat(optionArguments, hasItems("b=a\"d"));
+    assertThat(optionArguments, hasItems("--user-agent=A,B"));
+  }
 
   @Test
   public void transferChromeOptionPreferencesFromSystemPropsToDriver() {
