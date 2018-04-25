7715163ba11f4d73ced4073eb5cc7a8a2cd87391
mike.shysh <ukrainian1991@gmail.com>
2018-04-25 09:19:56 +0300

    introduce setValueChangeEvent option (#718)
    * introduce setValueChangeEvent option
    * add tests for setValueChangeEvent option

--- src/test/java/integration/PageWithoutJQuery.java
+++ src/test/java/integration/PageWithoutJQuery.java
@@ -1,19 +1,25 @@
 package integration;
 
 import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
+import com.codeborne.selenide.Configuration;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.WebDriverRunner.isHtmlUnit;
+import static org.junit.Assume.assumeFalse;
 
 public class PageWithoutJQuery extends IntegrationTest {
   @Before
-  public void openTestPageWithJQuery() {
+  public void openTestPageWithOutJQuery() {
     openFile("page_with_selects_without_jquery.html");
   }
 
   @Test
   public void setValueTriggersOnChangeEvent() {
+    assumeFalse(isHtmlUnit());
+
     $("#username").setValue("john");
     $("#username-mirror").shouldHave(text("john"));
 
@@ -21,5 +27,25 @@ public class PageWithoutJQuery extends IntegrationTest {
     $("#username").append("bon-jovi");
 
     $("#username-mirror").shouldHave(text("john bon-jovi"));
+  }
+
+  @Test
+  public void setValueDoesNotTriggerOnChangeEvent() {
+    assumeFalse(isHtmlUnit());
+
+    Configuration.setValueChangeEvent = false;
+
+    $("#username").setValue("john");
+    $("#username-mirror").shouldHave(text("_"));
+
+    $("#username").append(" ");
+    $("#username").append("bon-jovi");
+
+    $("#username-mirror").shouldHave(text("_"));
+  }
+
+  @After
+  public void tearDown() {
+    Configuration.setValueChangeEvent = true;
   }
 }
