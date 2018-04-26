c9db710fc4e323020ee1e84c114cbe3c9954a08c
Aliaksandr Rasolka <rosolko@gmail.com>
2018-04-26 20:06:37 +0300

    Migrate from old webdrivermanager api to new one.

--- src/main/java/com/codeborne/selenide/webdriver/WebDriverBinaryManager.java
+++ src/main/java/com/codeborne/selenide/webdriver/WebDriverBinaryManager.java
@@ -1,11 +1,6 @@
 package com.codeborne.selenide.webdriver;
 
-import io.github.bonigarcia.wdm.ChromeDriverManager;
-import io.github.bonigarcia.wdm.EdgeDriverManager;
-import io.github.bonigarcia.wdm.FirefoxDriverManager;
-import io.github.bonigarcia.wdm.InternetExplorerDriverManager;
-import io.github.bonigarcia.wdm.OperaDriverManager;
-import io.github.bonigarcia.wdm.PhantomJsDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import static com.codeborne.selenide.WebDriverRunner.isChrome;
 import static com.codeborne.selenide.WebDriverRunner.isEdge;
@@ -18,17 +13,17 @@ import static org.apache.commons.lang3.StringUtils.isNotBlank;
 public class WebDriverBinaryManager {
   public void setupBinaryPath() {
     if (isChrome() && !isSystemPropertySet("webdriver.chrome.driver")) {
-      ChromeDriverManager.getInstance().setup();
+      WebDriverManager.chromedriver().setup();
     } else if (isEdge() && !isSystemPropertySet("webdriver.edge.driver")) {
-      EdgeDriverManager.getInstance().setup();
+      WebDriverManager.edgedriver().setup();
     } else if (isIE() && !isSystemPropertySet("webdriver.ie.driver")) {
-      InternetExplorerDriverManager.getInstance().setup();
+      WebDriverManager.iedriver().setup();
     } else if (isOpera() && !isSystemPropertySet("webdriver.opera.driver")) {
-      OperaDriverManager.getInstance().setup();
+      WebDriverManager.operadriver().setup();
     } else if (isPhantomjs() && !isSystemPropertySet("phantomjs.binary.path")) {
-      PhantomJsDriverManager.getInstance().setup();
+      WebDriverManager.phantomjs().setup();
     } else if (isFirefox() && !isSystemPropertySet("webdriver.gecko.driver")) {
-      FirefoxDriverManager.getInstance().setup();
+      WebDriverManager.firefoxdriver().setup();
     }
   }
 
