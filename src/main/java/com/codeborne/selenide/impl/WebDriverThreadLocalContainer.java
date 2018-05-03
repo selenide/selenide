544bea1d257211d8e465b8aff54b8d0e4e4a6467
David Phillips <daviddphillips@gmail.com>
2018-05-02 20:55:57 -0700

    Fix code style

--- src/main/java/com/codeborne/selenide/impl/WebDriverThreadLocalContainer.java
+++ src/main/java/com/codeborne/selenide/impl/WebDriverThreadLocalContainer.java
@@ -23,7 +23,6 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Logger;
@@ -286,7 +285,7 @@ public class WebDriverThreadLocalContainer implements WebDriverContainer {
 
     @Override
     public void run() {
-        closeUnusedWebdrivers();
-      }
+      closeUnusedWebdrivers();
+    }
   }
 }
