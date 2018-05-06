8312d61b6581aa379782a1ab6005a38b61da209f
Alexei Vinogradov <alexei@vinogradoff.de>
2018-05-06 11:52:07 +0200

    Fix #725 remote browser binary (#726)
    * fixed #752 use of browserBinary for RemoteWebdriver
    * add info logging for usage of browserBinary
    * added more logging for headless mode

--- src/main/java/com/codeborne/selenide/webdriver/InternetExplorerDriverFactory.java
+++ src/main/java/com/codeborne/selenide/webdriver/InternetExplorerDriverFactory.java
@@ -7,7 +7,13 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.util.logging.Logger;
+
+import static com.codeborne.selenide.Configuration.browserBinary;
+
 class InternetExplorerDriverFactory extends AbstractDriverFactory {
+
+  private static final Logger log = Logger.getLogger(InternetExplorerDriverFactory.class.getName());
 
   @Override
   boolean supports() {
@@ -22,6 +28,10 @@ class InternetExplorerDriverFactory extends AbstractDriverFactory {
   private WebDriver createInternetExplorerDriver(final Proxy proxy) {
     DesiredCapabilities capabilities = createCommonCapabilities(proxy);
     InternetExplorerOptions options = new InternetExplorerOptions(capabilities);
+    if (!browserBinary.isEmpty()) {
+      log.info("Using browser binary: " + browserBinary);
+      log.warning("Changing browser binary not supported in InternetExplorer, setting will be ignored.");
+    }
     return new InternetExplorerDriver(options);
   }
 }
