1ded6334c30ca68dfb89a3d97e32058362fad983
simonk <sek_99@hotmail.co.uk>
2018-07-30 12:56:45 +0100

    change IT assertions to use should operations

--- src/test/java/integration/LastChildTest.java
+++ src/test/java/integration/LastChildTest.java
@@ -4,8 +4,7 @@ import com.codeborne.selenide.ex.ElementNotFound;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.be;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
@@ -18,9 +17,12 @@ public class LastChildTest extends IntegrationTest {
 
   @Test
   void canFindLastChildOnAGivenElement() {
-    assertThat($x("//table[@id='user-table']/thead/tr").lastChild().text())
-      .isEqualTo("Age");
+    $x("//table[@id='user-table']/thead/tr").lastChild().shouldHave(text("Age"));
+  }
 
+  @Test
+  void chainingLastChildFunctionsCorrectly() {
+    $("#multirowTable").lastChild().lastChild().shouldHave(attribute("id", "multirowTableSecondRow"));
   }
 
   @Test
@@ -28,12 +30,6 @@ public class LastChildTest extends IntegrationTest {
     assertThatThrownBy(() -> $x("//span[@id='hello-world']").lastChild().should(be(visible)))
       .isInstanceOf(ElementNotFound.class)
       .hasMessage("Element not found {By.xpath: *[last()]}\nExpected: be visible");
-  }
-
-  @Test
-  void chainingLastChildFunctionsCorrectly() {
-    assertThat($("#multirowTable").lastChild().lastChild().getAttribute("id"))
-      .isEqualTo("multirowTableSecondRow");
   }
 
 }
