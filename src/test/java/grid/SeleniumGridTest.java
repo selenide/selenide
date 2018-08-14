a337667037adb8888c42cd6946e877aa74afb49d
Andrei Solntsev <andrei.solntsev@gmail.com>
2018-08-15 00:07:54 +0300

    improve SeleniumGrid test

--- src/test/java/grid/SeleniumGridTest.java
+++ src/test/java/grid/SeleniumGridTest.java
@@ -1,35 +1,45 @@
 package grid;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.webdriver.WebDriverBinaryManager;
 import integration.IntegrationTest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.grid.selenium.GridLauncherV3;
+import org.openqa.grid.shared.Stoppable;
 import org.openqa.selenium.remote.LocalFileDetector;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.util.Optional;
+
 import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Configuration.FileDownloadMode.PROXY;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.close;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.openqa.selenium.net.PortProber.findFreePort;
 
 class SeleniumGridTest extends IntegrationTest {
+  private Optional<Stoppable> gridHub;
+  private Optional<Stoppable> gridNode;
+
   @BeforeEach
   void setUp() {
     close();
 
     int hubPort = findFreePort();
-    GridLauncherV3.main(new String[]{"-port", "" + hubPort});
+    gridHub = new GridLauncherV3(new String[]{"-port", "" + hubPort}).launch();
 
-    GridLauncherV3.main(new String[]{"-port", "" + findFreePort(),
+    gridNode = new GridLauncherV3(new String[]{"-port", "" + findFreePort(),
       "-role", "node",
       "-hub", "http://localhost:" + hubPort + "/grid/register"
-    });
+    }).launch();
 
     Configuration.remote = "http://localhost:" + hubPort + "/wd/hub";
-    Configuration.browser = "htmlunit";
+    Configuration.browser = "chrome";
+    Configuration.fileDownload = PROXY;
+    new WebDriverBinaryManager().setupBinaryPath();
   }
 
   @Test
@@ -40,7 +50,7 @@ class SeleniumGridTest extends IntegrationTest {
 
   @Test
   void shouldUseLocalFileDetector() {
-    RemoteWebDriver webDriver = (RemoteWebDriver) WebDriverRunner.getWebDriver();
+    RemoteWebDriver webDriver = (RemoteWebDriver) getWebDriver();
     assertThat(webDriver.getFileDetector())
       .isInstanceOf(LocalFileDetector.class);
   }
@@ -48,6 +58,8 @@ class SeleniumGridTest extends IntegrationTest {
   @AfterEach
   void tearDown() {
     close();
+    gridHub.ifPresent(Stoppable::stop);
+    gridNode.ifPresent(Stoppable::stop);
     Configuration.remote = null;
   }
 }
