42827a66a32326aeaf436d4756d1937a237e9212
Andrei Solntsev <andrei.solntsev@gmail.com>
2018-08-22 23:13:44 +0300

    #12 add support for page object fields of type ElementsCollection

--- modules/appium/src/test/java/com/codeborne/selenide/appium/CalculatorPage.java
+++ modules/appium/src/test/java/com/codeborne/selenide/appium/CalculatorPage.java
@@ -2,6 +2,7 @@ package com.codeborne.selenide.appium;
 
 import com.codeborne.selenide.SelenideElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.WebElement;
 
 class CalculatorPage {
   @AndroidFindBy(id = "op_add")
@@ -11,7 +12,7 @@ class CalculatorPage {
   SelenideElement equal;
 
   @AndroidFindBy(id = "digit_2")
-  SelenideElement number2;
+  WebElement number2;
 
   @AndroidFindBy(id = "digit_4")
   SelenideElement number4;
