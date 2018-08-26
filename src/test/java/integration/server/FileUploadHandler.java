01c8f3a8ee9ea731f1392193af8465674c41e48e
Andrei Solntsev <andrei.solntsev@gmail.com>
2018-08-26 23:50:47 +0300

    #785 unify code: rework jetty handlers to return Result instead of printing out response

--- src/test/java/integration/server/FileUploadHandler.java
+++ src/test/java/integration/server/FileUploadHandler.java
@@ -8,12 +8,13 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
 import java.util.logging.Logger;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.logging.Level.SEVERE;
+import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+import static org.apache.http.HttpStatus.SC_OK;
 
 class FileUploadHandler extends BaseHandler {
   private static final Logger log = Logger.getLogger(FileUploadHandler.class.getName());
@@ -24,9 +25,7 @@ class FileUploadHandler extends BaseHandler {
   }
 
   @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    long start = System.nanoTime();
-
+  public Result post(HttpServletRequest request, HttpServletResponse response) {
     DiskFileItemFactory factory = new DiskFileItemFactory();
     factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
     ServletFileUpload upload = new ServletFileUpload(factory);
@@ -39,12 +38,10 @@ class FileUploadHandler extends BaseHandler {
       }
 
       String message = "<h3>Uploaded " + uploadedFiles.size() + " files</h3>" + items;
-      response.setContentType(CONTENT_TYPE_HTML_TEXT);
-      printResponse(response, message.getBytes(UTF_8));
-      logRequest(request, message, start);
+      return new Result(SC_OK, CONTENT_TYPE_HTML_TEXT, message.getBytes(UTF_8));
     } catch (FileUploadException e) {
-      logRequest(request, e.getMessage(), start);
       log.log(SEVERE, e.getMessage(), e);
+      return new Result(SC_INTERNAL_SERVER_ERROR, CONTENT_TYPE_HTML_TEXT, e.toString());
     }
   }
 }
