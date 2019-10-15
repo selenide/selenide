a28079574cfd110460e3e59f2bc29a47c66b72fb
Andrei Solntsev <andrei.solntsev@gmail.com>
2019-10-15 20:04:46 +0300

    #896 Do close the browser in SelenideDriver.close()
    this method didn't close user-provided webdriver - and it caused butthurt for many users.

--- src/test/java/com/codeborne/selenide/drivercommands/WebDriverWrapperTest.java
+++ src/test/java/com/codeborne/selenide/drivercommands/WebDriverWrapperTest.java
@@ -5,17 +5,19 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 class WebDriverWrapperTest {
 
   @Test
-  void name() {
+  void close_closesTheBrowser() {
     WebDriver webDriver = mock(WebDriver.class);
     WebDriverWrapper driver = new WebDriverWrapper(new SelenideConfig(), webDriver, null);
 
     driver.close();
 
+    verify(webDriver).quit();
     verifyNoMoreInteractions(webDriver);
   }
 }
