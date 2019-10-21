5e110bd98563174a9bb62d7e90f587a08b8da2ed
Andrei Solntsev <andrei.solntsev@gmail.com>
2019-10-21 17:13:47 +0300

    fix tests on TravisCI

--- statics/src/test/java/integration/ConfirmTest.java
+++ statics/src/test/java/integration/ConfirmTest.java
@@ -14,6 +14,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.close;
 import static com.codeborne.selenide.Selenide.confirm;
 import static com.codeborne.selenide.Selenide.dismiss;
+import static com.codeborne.selenide.WebDriverRunner.isHtmlUnit;
 import static com.codeborne.selenide.WebDriverRunner.supportsModalDialogs;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -68,7 +69,7 @@ class ConfirmTest extends IntegrationTest {
       .isInstanceOf(DialogTextMismatch.class)
       .hasMessageContaining("Actual: Get out of this page, John Mc'Clane?")
       .hasMessageContaining("Expected: Get out of this page, Maria?")
-      .hasMessageMatching("(?s).*Screenshot: file:.+\\.png.*")
+      .hasMessageMatching(isHtmlUnit() ? "(?s).*" : "(?s).*Screenshot: file:.+\\.png.*")
       .hasMessageMatching("(?s).*Page source: file:.+\\.html.*")
       .hasMessageMatching("(?s).*Timeout: .+ s\\..*");
   }
