241912cd1e9a4a6b5931db60e5976a77e2779e19
SeleniumTestAB <38245002+SeleniumTestAB@users.noreply.github.com>
2019-10-24 23:04:07 +0200

    Moving save page source mechanics to PageSourceExtractor class (#1006)
    * refactor ScreenshotLab
    * Moving save page source mechanics to different class

---
+++ src/main/java/com/codeborne/selenide/impl/PageSourceExtractor.java
@@ -0,0 +1,107 @@
+package com.codeborne.selenide.impl;
+
+import com.codeborne.selenide.Config;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.UnhandledAlertException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class PageSourceExtractor {
+  private static final Logger log = LoggerFactory.getLogger(PageSourceExtractor.class);
+  protected Set<String> printedErrors = new ConcurrentSkipListSet<>();
+  private final WebDriver driver;
+  private final Config config;
+  private final String fileName;
+
+  public PageSourceExtractor(Config config, WebDriver driver, String fileName) {
+    this.config = config;
+    this.driver = driver;
+    this.fileName = fileName;
+  }
+
+  protected File extract(boolean retryIfAlert) {
+    File pageSource = new File(config.reportsFolder(), fileName + ".html");
+    try {
+      writeToFile(driver.getPageSource(), pageSource);
+    } catch (UnhandledAlertException e) {
+      if (retryIfAlert) {
+        retryingExtractionOnAlert(e);
+      } else {
+        printOnce("savePageSourceToFile", e);
+      }
+    } catch (WebDriverException e) {
+      log.warn("Failed to save page source to {}", fileName, e);
+      writeToFile(e.toString(), pageSource);
+      return pageSource;
+    } catch (RuntimeException e) {
+      log.error("Failed to save page source to {}", fileName, e);
+      writeToFile(e.toString(), pageSource);
+    }
+    return pageSource;
+  }
+
+
+  protected void writeToFile(String content, File targetFile) {
+    try (ByteArrayInputStream in = new ByteArrayInputStream(content.getBytes(UTF_8))) {
+      copyFile(in, targetFile);
+    } catch (IOException e) {
+      log.error("Failed to write file {}", targetFile.getAbsolutePath(), e);
+    }
+  }
+
+  protected synchronized void printOnce(String action, Throwable error) {
+    if (!printedErrors.contains(action)) {
+      log.error(error.getMessage(), error);
+      printedErrors.add(action);
+    } else {
+      log.error("Failed to {}: {}", action, error);
+    }
+  }
+
+  protected void copyFile(InputStream in, File targetFile) throws IOException {
+    ensureFolderExists(targetFile);
+
+    try (FileOutputStream out = new FileOutputStream(targetFile)) {
+      byte[] buffer = new byte[1024];
+      int len;
+      while ((len = in.read(buffer)) != -1) {
+        out.write(buffer, 0, len);
+      }
+    }
+  }
+
+  protected void ensureFolderExists(File targetFile) {
+    File folder = targetFile.getParentFile();
+    if (!folder.exists()) {
+      log.info("Creating folder: {}", folder);
+      if (!folder.mkdirs()) {
+        log.error("Failed to create {}", folder);
+      }
+    }
+  }
+
+  private void retryingExtractionOnAlert(Exception e) {
+    try {
+      Alert alert = driver.switchTo().alert();
+      log.error("{}: {}", e, alert.getText());
+      alert.accept();
+      extract(false);
+    } catch (Exception unableToCloseAlert) {
+      log.error("Failed to close alert", unableToCloseAlert);
+    }
+  }
+
+
+}
