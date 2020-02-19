77ec04417dce68529eb29d40ecd8e6e17c66f63c
Alex Strilchuk <52497131+ostrilchuk@users.noreply.github.com>
2020-02-19 17:31:33 +0100

    faced with issue while waiting for element apper
    looks like waiting fails with NoSuchElementException

--- src/main/java/com/codeborne/selenide/conditions/Visible.java
+++ src/main/java/com/codeborne/selenide/conditions/Visible.java
@@ -2,6 +2,7 @@ package com.codeborne.selenide.conditions;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Driver;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 
 public class Visible extends Condition {
@@ -11,7 +12,11 @@ public class Visible extends Condition {
 
   @Override
   public boolean apply(Driver driver, WebElement element) {
-    return element.isDisplayed();
+    try{
+      return element.isDisplayed();
+    } catch (NoSuchElementException e) {
+      return false;
+    }
   }
 
   @Override
