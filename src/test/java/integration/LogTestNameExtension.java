2879a7bbb40c29565b6bfec02bba41e1a6fa87f6
Andrei Solntsev <andrei.solntsev@gmail.com>
2020-04-14 10:21:32 +0300

    remove TextReport in Selenide own tests - it generates too much of output in Travis
    instead, log just "nok" for fast search of failed tests in Travis console

---
+++ src/test/java/integration/LogTestNameExtension.java
@@ -0,0 +1,42 @@
+package integration;
+
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.opentest4j.TestAbortedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static integration.BaseIntegrationTest.browser;
+
+class LogTestNameExtension implements BeforeAllCallback, AfterAllCallback, BeforeEachCallback, AfterEachCallback {
+  private static final Logger log = LoggerFactory.getLogger(LogTestNameExtension.class);
+
+  @Override
+  public void beforeAll(ExtensionContext context) {
+    log.info("Starting {} @ {}", context.getDisplayName(), browser);
+  }
+
+  @Override
+  public void afterAll(ExtensionContext context) {
+    log.info("Finished {} @ {} - {}", context.getDisplayName(), browser, verdict(context));
+  }
+
+  @Override
+  public void beforeEach(ExtensionContext context) {
+    log.info("  starting {} ...", context.getDisplayName());
+  }
+
+  @Override
+  public void afterEach(ExtensionContext context) {
+    log.info("  finished {} - {}", context.getDisplayName(), verdict(context));
+  }
+
+  private String verdict(ExtensionContext context) {
+    return context.getExecutionException().isPresent() ?
+      (context.getExecutionException().get() instanceof TestAbortedException ? "skipped" : "NOK") :
+      "OK";
+  }
+}
