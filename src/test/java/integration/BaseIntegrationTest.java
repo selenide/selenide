5192fca2242315561de57248888bc46b09168b8e
Andrei Solntsev <andrei.solntsev@gmail.com>
2020-04-14 10:21:32 +0300

    remove TextReport in Selenide own tests - it generates too much of output in Travis
    instead, log just "nok" for fast search of failed tests in Travis console

--- src/test/java/integration/BaseIntegrationTest.java
+++ src/test/java/integration/BaseIntegrationTest.java
@@ -1,13 +1,10 @@
 package integration;
 
 import com.codeborne.selenide.Browser;
-import com.codeborne.selenide.junit5.TextReportExtension;
 import integration.server.LocalHttpServer;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.Locale;
@@ -17,28 +14,21 @@ import static com.codeborne.selenide.Browsers.FIREFOX;
 import static java.lang.Boolean.parseBoolean;
 import static org.openqa.selenium.net.PortProber.findFreePort;
 
-@ExtendWith({TextReportExtension.class})
+@ExtendWith({LogTestNameExtension.class})
 public abstract class BaseIntegrationTest {
-  private static final Logger log = LoggerFactory.getLogger(BaseIntegrationTest.class);
-
   private static final boolean SSL = true;
   protected static LocalHttpServer server;
   protected static long averageSeleniumCommandDuration = 100;
   private static String protocol;
   private static int port;
   protected static final String browser = System.getProperty("selenide.browser", FIREFOX);
-  private static final boolean headless = parseBoolean(System.getProperty("selenide.headless", "false"));
+  static final boolean headless = parseBoolean(System.getProperty("selenide.headless", "false"));
 
   @BeforeAll
   static void setUpAll() throws Exception {
     Locale.setDefault(Locale.ENGLISH);
     runLocalHttpServer();
     setUpVideoRecorder();
-  }
-
-  @BeforeAll
-  static void logBrowserName() {
-    log.info("START {}{} TESTS", browser, headless ? " (headless)" : "");
   }
 
   @BeforeEach
