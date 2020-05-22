752a122143bf546f17dc1fac5ff54208f3e0f047
Andrei Solntsev <andrei.solntsev@gmail.com>
2020-05-22 21:02:30 +0300

    #1139 stabilize the test: the order of tabs seems to have changed in latest Firefox

--- src/test/java/integration/TabsTest.java
+++ src/test/java/integration/TabsTest.java
@@ -1,6 +1,6 @@
 package integration;
 
-import com.automation.remarks.video.annotations.Video;
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,8 +10,10 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.Set;
 
+import static com.codeborne.selenide.Condition.or;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assumptions.assumeFalse;
@@ -24,7 +26,6 @@ class TabsTest extends ITest {
   }
 
   @Test
-  @Video
   void userCanBrowseTabs_webdriver_api() {
     setTimeout(1000);
 
@@ -53,7 +54,6 @@ class TabsTest extends ITest {
   }
 
   @Test
-  @Video
   void canSwitchToWindowByTitle() {
     $(byText("Page2: alerts")).click();
     $(byText("Page1: uploads")).click();
@@ -71,7 +71,6 @@ class TabsTest extends ITest {
   }
 
   @Test
-  @Video
   void canSwitchToWindowByIndex_chrome() {
     assumeTrue(browser().isChrome());
     $(byText("Page2: alerts")).click();
@@ -91,7 +90,6 @@ class TabsTest extends ITest {
   }
 
   @Test
-  @Video
   void canSwitchToWindowByIndex_other_browsers_but_chrome() {
     assumeFalse(browser().isChrome());
     $(byText("Page2: alerts")).click();
@@ -99,31 +97,41 @@ class TabsTest extends ITest {
     $(byText("Page3: jquery")).click();
 
     $("h1").shouldHave(text("Tabs"));
+    Condition oneOfTitles = or("one of titles", text("Tabs"), text("Page with alerts"), text("File uploads"), text("Page with JQuery"));
 
     switchTo().window(1);
-    $("h1").shouldHave(text("Page with alerts"));
+    $("h1").shouldHave(oneOfTitles);
+    String title1 = $("h1").text();
+
     switchTo().window(2);
-    $("h1").shouldHave(text("File uploads"));
+    $("h1").shouldHave(oneOfTitles);
+    String title2 = $("h1").text();
+
     switchTo().window(3);
-    $("h1").shouldHave(text("Page with JQuery"));
+    $("h1").shouldHave(oneOfTitles);
+    String title3 = $("h1").text();
+
     switchTo().window(0);
-    $("h1").shouldHave(text("Tabs"));
+    $("h1").shouldHave(oneOfTitles);
+    String title0 = $("h1").text();
+
+    assertThat(asList(title0, title1, title2, title3))
+      .containsExactlyInAnyOrder("Tabs", "Page with alerts", "File uploads", "Page with JQuery");
   }
 
   @Test
-  @Video
   void canSwitchBetweenWindowsWithSameTitles() {
     $(byText("Page4: same title")).click();
     $("h1").shouldHave(text("Tabs"));
 
     switchTo().window("Test::tabs::title");
-    $("body").shouldHave(text("Secret phrase 1"));
+    $("body").shouldHave(or("one of tabs with this title", text("Secret phrase 1"), text("Secret phrase 2"), text("Secret phrase 3")));
 
     switchTo().window(0);
     $("h1").shouldHave(text("Tabs"));
     $(byText("Page5: same title")).click();
     switchTo().window("Test::tabs::title");
-    $("body").shouldHave(text("Secret phrase 1"));
+    $("body").shouldHave(or("one of tabs with this title", text("Secret phrase 1"), text("Secret phrase 2"), text("Secret phrase 3")));
     switchTo().window(0);
     $("h1").shouldHave(text("Tabs"));
   }
