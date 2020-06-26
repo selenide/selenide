72960d1a5c987ce5e6668905f00762e5f5d86745
Andrei Solntsev <andrei.solntsev@gmail.com>
2020-06-26 23:06:27 +0300

    #1212 make webdriver log unique: add threadID
    otherwise many parallel threads would write to the same file

--- src/test/java/com/codeborne/selenide/webdriver/AbstractDriverFactoryTest.java
+++ src/test/java/com/codeborne/selenide/webdriver/AbstractDriverFactoryTest.java
@@ -17,4 +17,21 @@ class AbstractDriverFactoryTest {
     assertThat(factory.majorVersion("6.17134")).isEqualTo(6);
     assertThat(factory.majorVersion("82.0.459.1")).isEqualTo(82);
   }
+
+  @Test
+  void convertStringToNearestObjectType() {
+    assertThat(factory.convertStringToNearestObjectType("false")).as("Boolean").isEqualTo(false);
+    assertThat(factory.convertStringToNearestObjectType("FALsE")).as("Boolean").isEqualTo(false);
+    assertThat(factory.convertStringToNearestObjectType("tRUE")).as("Boolean").isEqualTo(true);
+    assertThat(factory.convertStringToNearestObjectType("true")).as("Boolean").isEqualTo(true);
+
+    assertThat(factory.convertStringToNearestObjectType("0")).as("Integer").isEqualTo(0);
+    assertThat(factory.convertStringToNearestObjectType("-42")).as("Integer").isEqualTo(-42);
+
+    assertThat(factory.convertStringToNearestObjectType("")).as("String").isEqualTo("");
+
+    assertThat(factory.convertStringToNearestObjectType("Hottabych 2 false"))
+      .as("any other value")
+      .isEqualTo("Hottabych 2 false");
+  }
 }
