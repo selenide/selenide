d2bb234daa2bbff78972b5939f28f43e8b172a30
Andrei Solntsev <andrei.solntsev@gmail.com>
2020-09-03 23:35:04 +0300

    add "--no-sandbox" chrome option

--- src/test/java/com/codeborne/selenide/webdriver/ChromeDriverFactoryTest.java
+++ src/test/java/com/codeborne/selenide/webdriver/ChromeDriverFactoryTest.java
@@ -141,6 +141,14 @@ class ChromeDriverFactoryTest implements WithAssertions {
   }
 
   @Test
+  void disablesSandbox() {
+    Capabilities chromeOptions = factory.createCapabilities(config, browser, proxy, browserDownloadsFolder);
+    List<String> optionArguments = getBrowserLaunchArgs(ChromeOptions.CAPABILITY, chromeOptions);
+
+    assertThat(optionArguments).contains("--no-sandbox");
+  }
+
+  @Test
   void parseCSV() {
     assertThat(factory.parseCSV("123")).isEqualTo(singletonList("123"));
     assertThat(factory.parseCSV("foo bar")).isEqualTo(singletonList("foo bar"));
