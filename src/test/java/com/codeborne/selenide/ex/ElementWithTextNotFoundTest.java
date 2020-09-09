d6593ee97dfd33cf896bc102d11fed074d41bdec
Andrei Solntsev <andrei.solntsev@gmail.com>
2020-09-09 21:21:28 +0300

    add forgotten cause to itemWithText condition

--- src/test/java/com/codeborne/selenide/ex/ElementWithTextNotFoundTest.java
+++ src/test/java/com/codeborne/selenide/ex/ElementWithTextNotFoundTest.java
@@ -11,20 +11,22 @@ import static java.util.Arrays.asList;
 public class ElementWithTextNotFoundTest implements WithAssertions {
   private final List<String> actualTexts = asList("Niff", "Naff", "Nuff");
   private final List<String> expectedTexts = asList("Piff", "Paff", "Puff");
+  private final Throwable cause = new org.openqa.selenium.NoSuchElementException("ups");
 
   @Test
   void errorMessage() {
     ElementWithTextNotFound elementWithTextNotFound = new ElementWithTextNotFound(mockCollection(".characters"),
       actualTexts,
       expectedTexts,
-      null, 9000);
+      null, 9000, cause);
 
     assertThat(elementWithTextNotFound).hasMessage(String.format("Element with text not found%n" +
       "Actual: [Niff, Naff, Nuff]%n" +
       "Expected: [Piff, Paff, Puff]%n" +
       "Collection: .characters%n" +
       "Screenshot: null%n" +
-      "Timeout: 9 s."));
+      "Timeout: 9 s.%n" +
+      "Caused by: NoSuchElementException: ups"));
   }
 
   @Test
@@ -32,7 +34,7 @@ public class ElementWithTextNotFoundTest implements WithAssertions {
     ElementWithTextNotFound elementWithTextNotFound = new ElementWithTextNotFound(mockCollection(".characters"),
       actualTexts,
       expectedTexts,
-      "we expect favorite characters", 9000);
+      "we expect favorite characters", 9000, cause);
 
     assertThat(elementWithTextNotFound).hasMessage(String.format("Element with text not found%n" +
       "Actual: [Niff, Naff, Nuff]%n" +
@@ -40,7 +42,8 @@ public class ElementWithTextNotFoundTest implements WithAssertions {
       "Because: we expect favorite characters%n" +
       "Collection: .characters%n" +
       "Screenshot: null%n" +
-      "Timeout: 9 s."));
+      "Timeout: 9 s.%n" +
+      "Caused by: NoSuchElementException: ups"));
 
   }
 }
