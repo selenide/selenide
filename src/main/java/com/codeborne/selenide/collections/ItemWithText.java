18c1d7eff7acf244f17ce2dd30907faf7de5a687
Andrei Solntsev <andrei.solntsev@gmail.com>
2020-09-09 21:21:28 +0300

    add forgotten cause to itemWithText condition

--- src/main/java/com/codeborne/selenide/collections/ItemWithText.java
+++ src/main/java/com/codeborne/selenide/collections/ItemWithText.java
@@ -36,7 +36,7 @@ public class ItemWithText extends CollectionCondition {
                    @Nullable Exception lastError,
                    long timeoutMs) {
     throw new ElementWithTextNotFound(
-      collection, ElementsCollection.texts(elements), singletonList(expectedText), explanation, timeoutMs);
+      collection, ElementsCollection.texts(elements), singletonList(expectedText), explanation, timeoutMs, lastError);
   }
 
   @CheckReturnValue
--- src/main/java/com/codeborne/selenide/ex/ElementWithTextNotFound.java
+++ src/main/java/com/codeborne/selenide/ex/ElementWithTextNotFound.java
@@ -9,13 +9,14 @@ import static java.lang.System.lineSeparator;
 public class ElementWithTextNotFound extends UIAssertionError {
 
   public ElementWithTextNotFound(WebElementsCollection collection, List<String> actualTexts,
-                                 List<String> expectedTexts, String explanation, long timeoutMs) {
+                                 List<String> expectedTexts, String explanation, long timeoutMs, Throwable lastError) {
     super(collection.driver(),
       "Element with text not found" +
         lineSeparator() + "Actual: " + actualTexts +
         lineSeparator() + "Expected: " + expectedTexts +
         (explanation == null ? "" : lineSeparator() + "Because: " + explanation) +
-        lineSeparator() + "Collection: " + collection.description());
+        lineSeparator() + "Collection: " + collection.description(),
+      lastError);
     super.timeoutMs = timeoutMs;
   }
 }
