d9cd1857a03128aa56bf8127255523dbef50581c
Dmytro Stekanov <dstekanov.tech@gmail.com>
2020-09-20 15:08:37 +0300

    add ability to access LocalStorage

---
+++ statics/src/test/java/integration/LocalStorageTest.java
@@ -0,0 +1,49 @@
+package integration;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LocalStorageTest extends IntegrationTest {
+  @AfterAll
+  static void tearDown() {
+    closeWebDriver();
+  }
+
+  @BeforeEach
+  void openTestPage() {
+    openFile("empty.html");
+  }
+
+  @Test
+  void setAndGetItem() {
+    Selenide.localStorage().setItem("cat", "Tom");
+    Selenide.localStorage().setItem("mouse", "Jerry");
+
+    String cat = Selenide.localStorage().getItem("cat");
+    String mouse = Selenide.localStorage().getItem("mouse");
+
+    assertThat(cat).isEqualTo("Tom");
+    assertThat(mouse).isEqualTo("Jerry");
+  }
+
+  @Test
+  void removeItem() {
+    Selenide.localStorage().setItem("cat", "Tom");
+    Selenide.localStorage().removeItem("cat");
+    String cat = Selenide.localStorage().getItem("cat");
+    assertThat(cat).isEqualTo(null);
+  }
+
+  @Test
+  void clearAndSizeLocalStorage() {
+    Selenide.localStorage().setItem("cat", "Tom");
+    Selenide.localStorage().setItem("mouse", "Jerry");
+    Selenide.localStorage().clear();
+    assertThat(Selenide.localStorage().size()).isEqualTo(0);
+  }
+}
