d3271337774bb69e748c74ebdeaaf44bdfc52ead
Andrei Solntsev <andrei.solntsev@gmail.com>
2020-10-01 23:56:40 +0300

    #1051 make Describer safe: it should not fail in case of any webdriver failures

--- src/main/java/com/codeborne/selenide/appium/AppiumElementDescriber.java
+++ src/main/java/com/codeborne/selenide/appium/AppiumElementDescriber.java
@@ -3,56 +3,63 @@ package com.codeborne.selenide.appium;
 import com.codeborne.selenide.Driver;
 import com.codeborne.selenide.impl.ElementDescriber;
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.function.Consumer;
 
 /**
+ * Appium-specific element describer.
+ * <p>
  * Sample output:
- *
+ * <p>
  * Element should have text '666' {By.id: result}
  * Element: '<TextView class="android.widget.TextView" id="com.android.calculator2:id/result" name="6" displayed="true" checked="false" enabled="true" focused="false" bounds="[0,183][1080,584]" contentDescription="null" contentSize="{"width":1080,"height":401,"top":183,"left":0,"scrollableOffset":0,"touchPadding":24}" package="com.android.calculator2">6</TextView>'
  * Screenshot: file:/Users/andrei/projects/selenide-appium/build/reports/tests/1599256941895.0.png
  * Page source: file:/Users/andrei/projects/selenide-appium/build/reports/tests/1599256941895.0.html
- *
- * TODO Make all these calls "safe": if element has disappeared meanwhile, we should print at least of element details.
  */
 @ParametersAreNonnullByDefault
 public class AppiumElementDescriber implements ElementDescriber {
+  private static final Logger logger = LoggerFactory.getLogger(AppiumElementDescriber.class);
+
   @Nonnull
   @Override
   public String fully(Driver driver, @Nullable WebElement element) {
-    String klass = element.getAttribute("class"); // android.widget.TextView
-    String tagName = klass.replaceFirst(".+\\.(.+)", "$1");
-    String id = element.getAttribute("resource-id");
-    String checkable = element.getAttribute("checkable");
-    String checked = element.getAttribute("checked");
-    String clickable = element.getAttribute("clickable");
-    String contentDescription = element.getAttribute("content-desc");
-    String enabled = element.getAttribute("enabled");
-    String focusable = element.getAttribute("focusable");
-    String focused = element.getAttribute("focused");
-    String packageName = element.getAttribute("package");
-    String text = element.getAttribute("text");
-    String name = element.getAttribute("name");
-    String bounds = element.getAttribute("bounds");
-    String displayed = element.getAttribute("displayed");
-    String contentSize = element.getAttribute("contentSize");
-    return String.format("<%s class=\"%s\" id=\"%s\" name=\"%s\" displayed=\"%s\" checked=\"%s\" enabled=\"%s\" focused=\"%s\" bounds=\"%s\" contentDescription=\"%s\" contentSize=\"%s\" package=\"%s\">%s</%s>",
-      tagName, klass, id, name, displayed, checked, enabled, focused, bounds, contentDescription, contentSize, packageName, text, tagName);
+    if (element == null) {
+      return "null";
+    }
+
+    return new Builder(element)
+      .appendTagName()
+      .appendAttribute("resource-id")
+      .appendAttribute("checked")
+      .appendAttribute("content-desc")
+      .appendAttribute("enabled")
+      .appendAttribute("focused")
+      .appendAttribute("package")
+      .appendAttribute("name")
+      .appendAttribute("bounds")
+      .appendAttribute("displayed")
+      .appendAttribute("contentSize")
+      .finish()
+      .build();
   }
 
   @Nonnull
   @Override
   public String briefly(Driver driver, @Nonnull WebElement element) {
-    String klass = element.getAttribute("class"); // android.widget.TextView
-    String tagName = klass.replaceFirst(".+\\.(.+)", "$1");
-    String id = element.getAttribute("resource-id");
-    String text = element.getAttribute("text");
-    return String.format("<%s class=\"%s\" id=\"%s\">%s</%s>", tagName, klass, id, text, tagName);
+    return new Builder(element)
+      .appendTagName()
+      .appendAttribute("resource-id")
+      .finish()
+      .build();
   }
 
   @Override
@@ -65,5 +72,83 @@ public class AppiumElementDescriber implements ElementDescriber {
         .replace("By.cssSelector: ", "");
     }
     return selector.toString();
+  }
+
+  @ParametersAreNonnullByDefault
+  private static class Builder {
+    private final WebElement element;
+    private String className = "?";
+    private String tagName = "?";
+    private final StringBuilder sb = new StringBuilder();
+    private StaleElementReferenceException staleElementException;
+
+    private Builder(WebElement element) {
+      this.element = element;
+    }
+
+    private Builder appendTagName() {
+      getAttribute("class", (value) -> {
+        className = element.getAttribute("class");
+        tagName = className.replaceFirst(".+\\.(.+)", "$1");
+      });
+      sb.append("<").append(tagName).append(" class=\"").append(className).append("\"");
+      return this;
+    }
+
+    private Builder appendAttribute(String name) {
+      getAttribute(name, (value) -> {
+        sb.append(" ").append(name).append("=\"").append(value).append("\"");
+      });
+      return this;
+    }
+
+    private void getAttribute(String name, Consumer<String> attributeHandler) {
+      if (staleElementException != null) return;
+
+      try {
+        String value = element.getAttribute(name);
+        if (value != null) {
+          attributeHandler.accept(value);
+        }
+      }
+      catch (StaleElementReferenceException e) {
+        staleElementException = e;
+        logger.debug("Failed to get attribute {}: {}", name, e.toString());
+      }
+      catch (WebDriverException e) {
+        logger.info("Failed to get attribute {}: {}", name, e.toString());
+      }
+      catch (RuntimeException e) {
+        logger.warn("Failed to get attribute {}", name, e);
+      }
+    }
+
+    public Builder finish() {
+      sb.append(">");
+
+      if (staleElementException != null) {
+        sb.append(staleElementException);
+      }
+      else {
+        appendText();
+      }
+
+      sb.append("</").append(tagName).append(">");
+      return this;
+    }
+
+    private void appendText() {
+      try {
+        String text = element.getAttribute("text");
+        sb.append(text);
+      }
+      catch (WebDriverException e) {
+        sb.append(e.toString());
+      }
+    }
+
+    private String build() {
+      return sb.toString();
+    }
   }
 }
---
+++ src/test/java/com/codeborne/selenide/appium/AppiumElementDescriberTest.java
@@ -0,0 +1,87 @@
+package com.codeborne.selenide.appium;
+
+import com.codeborne.selenide.Driver;
+import com.codeborne.selenide.DriverStub;
+import org.junit.Test;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class AppiumElementDescriberTest {
+  private final AppiumElementDescriber describer = new AppiumElementDescriber();
+  private final Driver driver = new DriverStub();
+  private final WebElement element = mock(WebElement.class);
+
+  @Test
+  public void extractsTagNameFromClassName() {
+    when(element.getAttribute("class")).thenReturn("android.widget.TextView");
+    when(element.getAttribute("text")).thenReturn("Hello, world");
+
+    assertThat(describer.briefly(driver, element))
+      .isEqualTo("<TextView class=\"android.widget.TextView\">Hello, world</TextView>");
+  }
+
+  @Test
+  public void addsElementId() {
+    when(element.getAttribute("class")).thenReturn("android.widget.TextView");
+    when(element.getAttribute("text")).thenReturn("Hello, world");
+    when(element.getAttribute("resource-id")).thenReturn("submitPaymentButton");
+
+    assertThat(describer.briefly(driver, element))
+      .isEqualTo("<TextView class=\"android.widget.TextView\" resource-id=\"submitPaymentButton\">Hello, world</TextView>");
+  }
+
+  @Test
+  public void fully() {
+    when(element.getAttribute("class")).thenReturn("android.widget.TextView");
+    when(element.getAttribute("text")).thenReturn("Hello, world");
+
+    assertThat(describer.fully(driver, element))
+      .isEqualTo("<TextView class=\"android.widget.TextView\">Hello, world</TextView>");
+  }
+
+  @Test
+  public void addAllPossibleAttributes() {
+    when(element.getAttribute("class")).thenReturn("android.widget.TextView");
+    when(element.getAttribute("text")).thenReturn("Hello, world");
+    when(element.getAttribute("resource-id")).thenReturn("com.android.calculator2:id/result");
+    when(element.getAttribute("checked")).thenReturn("true");
+    when(element.getAttribute("content-desc")).thenReturn("blah");
+    when(element.getAttribute("enabled")).thenReturn("true");
+    when(element.getAttribute("focused")).thenReturn("false");
+    when(element.getAttribute("package")).thenReturn("com.android.calculator2");
+    when(element.getAttribute("bounds")).thenReturn("[0,183][1080,584]");
+    when(element.getAttribute("displayed")).thenReturn("true");
+    when(element.getAttribute("contentSize")).thenReturn("{\"width\":1080,\"height\":401,\"top\":183,\"left\":0,\"touchPadding\":24}");
+
+    assertThat(describer.fully(driver, element))
+      .isEqualTo("<TextView class=\"android.widget.TextView\"" +
+        " resource-id=\"com.android.calculator2:id/result\" checked=\"true\"" +
+        " content-desc=\"blah\" enabled=\"true\" focused=\"false\"" +
+        " package=\"com.android.calculator2\"" +
+        " bounds=\"[0,183][1080,584]\"" +
+        " displayed=\"true\"" +
+        " contentSize=\"{\"width\":1080,\"height\":401,\"top\":183,\"left\":0,\"touchPadding\":24}\">" +
+        "Hello, world</TextView>");
+  }
+
+  @Test
+  public void ignoresErrorsWhenGettingAnyOfAttributes() {
+    when(element.getAttribute(anyString())).thenThrow(new WebDriverException("Not implemented"));
+    doReturn("android.widget.TextView").when(element).getAttribute("class");
+    doReturn("Hello, world").when(element).getAttribute("text");
+    doReturn("com.android.calculator2:id/result").when(element).getAttribute("resource-id");
+    doReturn("com.android.calculator2").when(element).getAttribute("package");
+
+    assertThat(describer.fully(driver, element))
+      .isEqualTo("<TextView class=\"android.widget.TextView\"" +
+        " resource-id=\"com.android.calculator2:id/result\"" +
+        " package=\"com.android.calculator2\">" +
+        "Hello, world</TextView>");
+  }
+}
