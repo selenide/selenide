1cd16ad6fb747191e41b1a218ad2643461b1182a
Andrei Solntsev <andrei.solntsev@gmail.com>
2020-12-03 01:02:17 +0200

    #1136 add method $.shouldBe(condition, timeout)
    ... as a replacement for $.waitUntil(condition, timeout)

--- src/main/java/com/codeborne/selenide/impl/Arguments.java
+++ src/main/java/com/codeborne/selenide/impl/Arguments.java
@@ -1,7 +1,10 @@
 package com.codeborne.selenide.impl;
 
+import javax.annotation.CheckReturnValue;
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.Arrays;
+import java.util.Optional;
 
 public class Arguments {
   private final Object[] args;
@@ -23,5 +26,18 @@ public class Arguments {
 
   public int length() {
     return args == null ? 0 : args.length;
+  }
+
+  @CheckReturnValue
+  @Nonnull
+  public <T> Optional<T> ofType(@Nonnull Class<T> klass) {
+    if (args == null) return Optional.empty();
+
+    for (Object arg : args) {
+      if (klass.isAssignableFrom(arg.getClass()))
+        //noinspection unchecked
+        return Optional.of((T) arg);
+    }
+    return Optional.empty();
   }
 }
