430f816a0242077789115bb5918ebf85620798ca
Andrei Solntsev <andrei.solntsev@gmail.com>
2020-12-17 00:38:47 +0200

    #694 support page object fields of generic types
    * I am not sure if somebody really needs it
    * but at least Selenide doesn't crash in this case
    * this <T> will be an instance of ElementsContainer

--- src/main/java/com/codeborne/selenide/impl/SelenideFieldDecorator.java
+++ src/main/java/com/codeborne/selenide/impl/SelenideFieldDecorator.java
@@ -12,6 +12,8 @@ import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.pagefactory.Annotations;
 import org.openqa.selenium.support.pagefactory.DefaultElementLocatorFactory;
 import org.openqa.selenium.support.pagefactory.DefaultFieldDecorator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
@@ -19,14 +21,17 @@ import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @ParametersAreNonnullByDefault
 public class SelenideFieldDecorator extends DefaultFieldDecorator {
+  private static final Logger logger = LoggerFactory.getLogger(SelenideFieldDecorator.class);
   private final SelenidePageFactory pageFactory;
   private final Driver driver;
   private final SearchContext searchContext;
@@ -38,25 +43,38 @@ public class SelenideFieldDecorator extends DefaultFieldDecorator {
     this.searchContext = searchContext;
   }
 
-  @Override
   @CheckReturnValue
   @Nullable
-  public Object decorate(ClassLoader loader, Field field) {
+  @Override
+  public final Object decorate(ClassLoader loader, Field field) {
+    Type[] classGenericTypes = field.getDeclaringClass().getGenericInterfaces();
+    return decorate(loader, field, classGenericTypes);
+  }
+
+  @CheckReturnValue
+  @Nullable
+  public final Object decorate(ClassLoader loader, Field field, Type[] genericTypes) {
     if (ElementsContainer.class.equals(field.getDeclaringClass()) && "self".equals(field.getName())) {
-      return searchContext;
+      if (searchContext instanceof SelenideElement) {
+        return searchContext;
+      }
+      else {
+        logger.warn("Cannot initialize field {}", field);
+        return null;
+      }
     }
     By selector = new Annotations(field).buildBy();
     if (WebElement.class.isAssignableFrom(field.getType())) {
       return ElementFinder.wrap(driver, searchContext, selector, 0);
     }
-    if (ElementsCollection.class.isAssignableFrom(field.getType()) || isDecoratableList(field, WebElement.class)) {
+    if (ElementsCollection.class.isAssignableFrom(field.getType()) || isDecoratableList(field, genericTypes, WebElement.class)) {
       return new ElementsCollection(new BySelectorCollection(driver, searchContext, selector));
     }
     else if (ElementsContainer.class.isAssignableFrom(field.getType())) {
       return createElementsContainer(selector, field);
     }
-    else if (isDecoratableList(field, ElementsContainer.class)) {
-      return createElementsContainerList(field);
+    else if (isDecoratableList(field, genericTypes, ElementsContainer.class)) {
+      return createElementsContainerList(field, genericTypes);
     }
 
     return super.decorate(loader, field);
@@ -64,9 +82,9 @@ public class SelenideFieldDecorator extends DefaultFieldDecorator {
 
   @CheckReturnValue
   @Nonnull
-  private List<ElementsContainer> createElementsContainerList(Field field) {
+  private List<ElementsContainer> createElementsContainerList(Field field, Type[] genericTypes) {
     List<ElementsContainer> result = new ArrayList<>();
-    Class<?> listType = getListGenericType(field);
+    Class<?> listType = getListGenericType(field, genericTypes);
     if (listType == null) {
       throw new IllegalArgumentException("Cannot detect list type for " + field);
     }
@@ -74,10 +92,15 @@ public class SelenideFieldDecorator extends DefaultFieldDecorator {
     try {
       List<SelenideElement> selfList = SelenideElementListProxy.wrap(driver, factory.createLocator(field));
       for (SelenideElement element : selfList) {
-        result.add(initElementsContainer(listType, element));
+        Type[] types = ((ParameterizedType) field.getGenericType()).getActualTypeArguments();
+        result.add(initElementsContainer(field, element, listType, types));
       }
       return result;
-    } catch (Exception e) {
+    }
+    catch (RuntimeException e) {
+      throw e;
+    }
+    catch (Exception e) {
       throw new RuntimeException("Failed to create elements container list for field " + field.getName(), e);
     }
   }
@@ -87,41 +110,76 @@ public class SelenideFieldDecorator extends DefaultFieldDecorator {
   private ElementsContainer createElementsContainer(By selector, Field field) {
     try {
       SelenideElement self = ElementFinder.wrap(driver, searchContext, selector, 0);
-      return initElementsContainer(field.getType(), self);
-    } catch (Exception e) {
+      return initElementsContainer(field, self);
+    }
+    catch (RuntimeException e) {
+      throw e;
+    }
+    catch (Exception e) {
       throw new RuntimeException("Failed to create elements container for field " + field.getName(), e);
     }
   }
 
+  private ElementsContainer initElementsContainer(Field field, SelenideElement self) throws Exception {
+    Type[] genericTypes = field.getGenericType() instanceof ParameterizedType ?
+      ((ParameterizedType) field.getGenericType()).getActualTypeArguments() : new Type[0];
+    return initElementsContainer(field, self, field.getType(), genericTypes);
+  }
+
   @CheckReturnValue
   @Nonnull
-  private ElementsContainer initElementsContainer(Class<?> type, SelenideElement self)
-      throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+  private ElementsContainer initElementsContainer(Field field,
+                                                  SelenideElement self,
+                                                  Class<?> type,
+                                                  Type[] genericTypes) throws Exception {
+    if (Modifier.isInterface(type.getModifiers())) {
+      throw new IllegalArgumentException("Cannot initialize field " + field + ": " + type + " is interface");
+    }
+    if (Modifier.isAbstract(type.getModifiers())) {
+      throw new IllegalArgumentException("Cannot initialize field " + field + ": " + type + " is abstract");
+    }
     Constructor<?> constructor = type.getDeclaredConstructor();
     constructor.setAccessible(true);
     ElementsContainer result = (ElementsContainer) constructor.newInstance();
-    pageFactory.initElements(new SelenideFieldDecorator(pageFactory, driver, self), result);
+    pageFactory.initElements(new SelenideFieldDecorator(pageFactory, driver, self), result, genericTypes);
     return result;
   }
 
   @CheckReturnValue
-  private boolean isDecoratableList(Field field, Class<?> type) {
+  private boolean isDecoratableList(Field field, Type[] genericTypes, Class<?> type) {
     if (!List.class.isAssignableFrom(field.getType())) {
       return false;
     }
 
-    Class<?> listType = getListGenericType(field);
+    Class<?> listType = getListGenericType(field, genericTypes);
 
     return listType != null && type.isAssignableFrom(listType)
-        && (field.getAnnotation(FindBy.class) != null || field.getAnnotation(FindBys.class) != null);
+      && (field.getAnnotation(FindBy.class) != null || field.getAnnotation(FindBys.class) != null);
   }
 
   @CheckReturnValue
   @Nullable
-  private Class<?> getListGenericType(Field field) {
-    Type genericType = field.getGenericType();
-    if (!(genericType instanceof ParameterizedType)) return null;
+  private Class<?> getListGenericType(Field field, Type[] genericTypes) {
+    Type fieldType = field.getGenericType();
+    if (!(fieldType instanceof ParameterizedType)) return null;
 
-    return (Class<?>) ((ParameterizedType) genericType).getActualTypeArguments()[0];
+    Type[] actualTypeArguments = ((ParameterizedType) fieldType).getActualTypeArguments();
+    Type firstType = actualTypeArguments[0];
+    if (firstType instanceof TypeVariable) {
+      int indexOfType = indexOf(field.getDeclaringClass(), firstType);
+      return (Class<?>) genericTypes[indexOfType];
+    }
+    else if (firstType instanceof Class) {
+      return (Class<?>) firstType;
+    }
+    throw new IllegalArgumentException("Cannot detect list type of " + field);
+  }
+
+  private int indexOf(Class<?> klass, Type firstArgument) {
+    Object[] objects = Arrays.stream(klass.getTypeParameters()).toArray();
+    for (int i = 0; i < objects.length; i++) {
+      if (objects[i].equals(firstArgument)) return i;
+    }
+    return -1;
   }
 }
---
+++ statics/src/test/java/integration/pageobjects/FieldOfGenericTypeTest.java
@@ -0,0 +1,80 @@
+package integration.pageobjects;
+
+import com.codeborne.selenide.ElementsContainer;
+import com.codeborne.selenide.SelenideElement;
+import integration.IntegrationTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.page;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class FieldOfGenericTypeTest extends IntegrationTest {
+  @BeforeEach
+  void openTestPage() {
+    openFile("page_with_selects_without_jquery.html");
+  }
+
+  @Test
+  void injectsFoundSelenideElementAsSelf() {
+    DummyPage page = page(DummyPage.class);
+    assertThat(page).isInstanceOf(DummyPage.class);
+    assertThat(page.body).isInstanceOf(DummyTypedElement.class);
+    assertThat(page.body.selects).isInstanceOf(List.class);
+    assertThat(page.body.selects).hasSize(3);
+    assertThat(page.body.getSelf()).isEqualTo($("body"));
+    assertThat(page.body.selects.get(0)).isEqualTo($("select[name=domain]"));
+    assertThat(page.body.selects.get(1)).isEqualTo($("select#hero"));
+    assertThat(page.body.selects.get(2)).isEqualTo($("select#gender"));
+  }
+
+  @Test
+  void injectsFoundSelenideElementAsSelf2() {
+    AnotherPage page = page(AnotherPage.class);
+    assertThat(page.body.selects).hasSize(3);
+    assertThat(page.body).isInstanceOf(ElementsContainer.class);
+    assertThat(page.body.getSelf()).isEqualTo($("body"));
+    assertThat(page.body.selects.get(0)).isInstanceOf(WebElement.class);
+    assertThat(page.body.selects.get(0)).isEqualTo($("select[name=domain]"));
+    assertThat(page.body.selects.get(1)).isEqualTo($("select#hero"));
+    assertThat(page.body.selects.get(2)).isEqualTo($("select#gender"));
+  }
+
+  @Test
+  void injectsFoundSelenideElementAsSelf3() {
+    assertThatThrownBy(() -> page(YetAnotherPage.class))
+      .isInstanceOf(RuntimeException.class)
+      .hasMessageMatching("Failed to create new instance of class .*\\.FieldOfGenericTypeTest.YetAnotherPage.*")
+      .getCause()
+      .isInstanceOf(IllegalArgumentException.class)
+      .hasMessageMatching("Cannot initialize field .*List .*DummyTypedElement.selects: .*ElementsContainer is abstract");
+  }
+
+  static class DummyTypedElement<S, T> extends ElementsContainer {
+    private List<S> names;
+
+    @FindBy(tagName = "select")
+    private List<T> selects;
+  }
+
+  static class DummyPage {
+    @FindBy(tagName = "body")
+    private DummyTypedElement<String, SelenideElement> body;
+  }
+
+  static class AnotherPage {
+    @FindBy(tagName = "body")
+    private DummyTypedElement<Integer, WebElement> body;
+  }
+
+  static class YetAnotherPage {
+    @FindBy(tagName = "body")
+    private DummyTypedElement<Boolean, ElementsContainer> body;
+  }
+}
