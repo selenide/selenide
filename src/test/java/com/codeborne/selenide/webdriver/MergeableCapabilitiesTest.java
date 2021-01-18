3f7b7dcd2df6b6188ce577055fd8221ff7d25543
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-01-18 20:10:23 +0200

    #1366 detect mismatching "browserName" capability

--- src/test/java/com/codeborne/selenide/webdriver/MergeableCapabilitiesTest.java
+++ src/test/java/com/codeborne/selenide/webdriver/MergeableCapabilitiesTest.java
@@ -3,12 +3,14 @@ package com.codeborne.selenide.webdriver;
 import com.google.common.collect.ImmutableMap;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.util.Map;
 
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 final class MergeableCapabilitiesTest {
   @Test
@@ -71,5 +73,15 @@ final class MergeableCapabilitiesTest {
     assertThat(result.asMap()).containsKeys("goog:chromeOptions");
     Object excludeSwitches = ((Map<String, Object>) result.asMap().get("goog:chromeOptions")).get("excludeSwitches");
     assertThat(excludeSwitches).isEqualTo(asList("foo", "bar", "blah", "bluh"));
+  }
+
+  @Test
+  void cannotMergeDifferentBrowsers() {
+    ChromeOptions base = new ChromeOptions();
+    FirefoxOptions extra = new FirefoxOptions();
+
+    assertThatThrownBy(() -> new MergeableCapabilities(base, extra))
+      .isInstanceOf(IllegalArgumentException.class)
+      .hasMessage("Conflicting browser name: 'chrome' vs. 'firefox'");
   }
 }
