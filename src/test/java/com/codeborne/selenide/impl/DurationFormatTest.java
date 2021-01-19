399e72e41936742f616f84f431b44dcccc300dee
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-01-19 22:25:55 +0200

    #1376 display duration argument in report as "1s" or "300 ms"

---
+++ src/test/java/com/codeborne/selenide/impl/DurationFormatTest.java
@@ -0,0 +1,42 @@
+package com.codeborne.selenide.impl;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+final class DurationFormatTest {
+  private final DurationFormat df = new DurationFormat();
+
+  @Test
+  void zero() {
+    assertThat(df.format(0)).isEqualTo("0 ms.");
+  }
+
+  @Test
+  void lessThanSecond() {
+    assertThat(df.format(1)).isEqualTo("1 ms.");
+    assertThat(df.format(999)).isEqualTo("999 ms.");
+  }
+
+  @Test
+  void integerSeconds() {
+    assertThat(df.format(1000)).isEqualTo("1 s.");
+    assertThat(df.format(2000)).isEqualTo("2 s.");
+  }
+
+  @Test
+  void greaterThanSecond() {
+    assertThat(df.format(1500)).isEqualTo("1.500 s.");
+    assertThat(df.format(1567)).isEqualTo("1.567 s.");
+    assertThat(df.format(2001)).isEqualTo("2.001 s.");
+  }
+
+  @Test
+  void greaterThanMinute() {
+    assertThat(df.format(Duration.ofMinutes(1))).isEqualTo("60 s.");
+    assertThat(df.format(Duration.ofMinutes(2).plusSeconds(2))).isEqualTo("122 s.");
+    assertThat(df.format(Duration.ofMinutes(3).plusMillis(300))).isEqualTo("180.300 s.");
+  }
+}
--- src/test/java/com/codeborne/selenide/logevents/SelenideLoggerTest.java
+++ src/test/java/com/codeborne/selenide/logevents/SelenideLoggerTest.java
@@ -9,8 +9,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.logevents.LogEvent.EventStatus.FAIL;
 import static com.codeborne.selenide.logevents.LogEvent.EventStatus.PASS;
+import static com.codeborne.selenide.logevents.SelenideLogger.readableArguments;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
@@ -42,24 +46,24 @@ final class SelenideLoggerTest implements WithAssertions {
 
   @Test
   void printsReadableArgumentsValues() {
-    assertThat(SelenideLogger.readableArguments((Object[]) null))
-      .isEqualTo("");
-    assertThat(SelenideLogger.readableArguments(111))
-      .isEqualTo("111");
-    assertThat(SelenideLogger.readableArguments(1, 2, 3))
-      .isEqualTo("[1, 2, 3]");
-    assertThat(SelenideLogger.readableArguments((Object[]) new String[]{"a"}))
-      .isEqualTo("a");
-    assertThat(SelenideLogger.readableArguments((Object[]) new String[]{"a", "bb"}))
-      .isEqualTo("[a, bb]");
-    assertThat(SelenideLogger.readableArguments((Object[]) new String[]{null}))
-      .isEqualTo("null");
-    assertThat(SelenideLogger.readableArguments((Object[]) new String[]{null, "a", null}))
-      .isEqualTo("[null, a, null]");
-    assertThat(SelenideLogger.readableArguments((Object) new int[]{1}))
-      .isEqualTo("1");
-    assertThat(SelenideLogger.readableArguments((Object) new int[]{1, 2}))
-      .isEqualTo("[1, 2]");
+    assertThat(readableArguments((Object[]) null)).isEqualTo("");
+    assertThat(readableArguments(111)).isEqualTo("111");
+    assertThat(readableArguments(1, 2, 3)).isEqualTo("[1, 2, 3]");
+    assertThat(readableArguments((Object[]) new String[]{"a"})).isEqualTo("a");
+    assertThat(readableArguments((Object[]) new String[]{"a", "bb"})).isEqualTo("[a, bb]");
+    assertThat(readableArguments((Object[]) new String[]{null})).isEqualTo("null");
+    assertThat(readableArguments((Object[]) new String[]{null, "a", null})).isEqualTo("[null, a, null]");
+    assertThat(readableArguments((Object) new int[]{1})).isEqualTo("1");
+    assertThat(readableArguments((Object) new int[]{1, 2})).isEqualTo("[1, 2]");
+  }
+
+  @Test
+  void printsDurationAmongArguments() {
+    assertThat(readableArguments(Duration.ofMillis(900))).isEqualTo("900 ms.");
+    assertThat(readableArguments(visible, Duration.ofSeconds(42))).isEqualTo("[visible, 42 s.]");
+    assertThat(readableArguments(visible, Duration.ofMillis(8500))).isEqualTo("[visible, 8.500 s.]");
+    assertThat(readableArguments(visible, Duration.ofMillis(900))).isEqualTo("[visible, 900 ms.]");
+    assertThat(readableArguments(visible, Duration.ofNanos(0))).isEqualTo("[visible, 0 ms.]");
   }
 
   @Test
