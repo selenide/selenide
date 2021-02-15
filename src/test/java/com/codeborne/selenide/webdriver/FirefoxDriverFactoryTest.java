fd41d6407b83a013c5c1f65058d3ea02317175a5
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-02-16 00:19:09 +0200

    upgrade to selenium 4.0.0-beta-1: it seems that options.getProfile() now returns an empty FirefoxProfile instead if null

--- src/test/java/com/codeborne/selenide/webdriver/FirefoxDriverFactoryTest.java
+++ src/test/java/com/codeborne/selenide/webdriver/FirefoxDriverFactoryTest.java
@@ -146,7 +146,6 @@ final class FirefoxDriverFactoryTest implements WithAssertions {
     assertThat((String) prefs.get("browser.helperApps.neverAsk.saveToDisk")).contains("application/pdf");
     assertThat(prefs.get("pdfjs.disabled")).isEqualTo(true);
     assertThat(prefs.get("browser.download.folderList")).isEqualTo(2);
-    assertThat(options.getProfile()).isNull();
   }
 
   @Test
