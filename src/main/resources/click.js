67f4db015617bbb4019314f6d0eec07845f79cbf
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-02-23 18:41:31 +0200

    #1406 fix JS code for `$.click()` in InternetExplorer

--- src/main/resources/click.js
+++ src/main/resources/click.js
@@ -1,10 +1,25 @@
 (function (element, offsetX, offsetY) {
   const rect = element.getBoundingClientRect();
-  element.dispatchEvent(new MouseEvent('click', {
-    'view': window,
-    'bubbles': true,
-    'cancelable': true,
-    'clientX': rect.left + rect.width / 2 + offsetX,
-    'clientY': rect.top + rect.height / 2 + offsetY
-  }));
+
+  function mouseEvent() {
+    if (typeof (Event) === 'function') {
+      return new MouseEvent('click', {
+        'view': window,
+        'bubbles': true,
+        'cancelable': true,
+        'clientX': rect.left + rect.width / 2 + offsetX,
+        'clientY': rect.top + rect.height / 2 + offsetY
+      });
+    }
+    else {
+      const event = document.createEvent('MouseEvent');
+      event.initEvent('click', true, true);
+      event.type = 'click'
+      event.view = window;
+      event.clientX = rect.left + rect.width / 2 + offsetX
+      event.clientY = rect.top + rect.height / 2 + offsetY
+      return event;
+    }
+  }
+  element.dispatchEvent(mouseEvent());
 })(arguments[0], arguments[1], arguments[2]);
