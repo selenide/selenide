c80d40c678dd0bc4c66429b1906de46aa99cfcc2
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-04-07 23:49:07 +0300

    re-run flaky StopwatchTest 1000 times

--- src/test/java/com/codeborne/selenide/StopwatchTest.java
+++ src/test/java/com/codeborne/selenide/StopwatchTest.java
@@ -1,19 +1,19 @@
 package com.codeborne.selenide;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 final class StopwatchTest {
   private final Stopwatch stopwatch = new Stopwatch(100);
 
-  @Test
+  @RepeatedTest(1000)
   void timeout_is_not_reached() throws InterruptedException {
     Thread.sleep(20);
     assertThat(stopwatch.isTimeoutReached()).isFalse();
   }
 
-  @Test
+  @RepeatedTest(1000)
   void timeout_is_reached() throws InterruptedException {
     Thread.sleep(101);
     assertThat(stopwatch.isTimeoutReached()).isTrue();
