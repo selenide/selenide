e84681268bb98a28c5b0365ab814d76a6b428dab
Aaftakhov <simple1elf@gmail.com>
2021-04-12 20:02:58 +0500

    fixes #1448: add BrowserPerTestStrategyExtension to close browser after each test

---
+++ statics/src/main/java/com/codeborne/selenide/junit5/BrowserPerTestStrategyExtension.java
@@ -0,0 +1,31 @@
+package com.codeborne.selenide.junit5;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
+
+/**
+ * By using this extension browser will be automatically closed after each test.
+ * <br>
+ * To use this extension, extend your test class with it:
+ * <br>
+ * {@code @ExtendWith({BrowserPerTestStrategyExtension.class}}
+ * <br>
+ * Or register extension in test class:
+ * <br>
+ * {@code @RegisterExtension static BrowserPerTestStrategyExtension browserPerTestStrategy = new BrowserPerTestStrategyExtension();}
+ * <br>
+ *
+ * @author simple-elf
+ * @since 5.20.2
+ */
+@ParametersAreNonnullByDefault
+public class BrowserPerTestStrategyExtension implements AfterEachCallback {
+  @Override
+  public void afterEach(final ExtensionContext context) {
+    closeWebDriver();
+  }
+}
