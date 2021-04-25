74481f30df4aefd6c893e4102474db052feb7a55
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-04-25 22:05:23 +0300

    improve examples of checking radio buttons

--- statics/src/test/java/integration/RadioTest.java
+++ statics/src/test/java/integration/RadioTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.checked;
+import static com.codeborne.selenide.Condition.exactValue;
 import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.getSelectedRadio;
@@ -23,8 +26,7 @@ final class RadioTest extends IntegrationTest {
   @Test
   void userCanSelectRadioButtonByValue() {
     $(By.name("me")).selectRadio("cat");
-    assertThat(getSelectedRadio(By.name("me")).val())
-      .isEqualTo("cat");
+    getSelectedRadio(By.name("me")).shouldHave(exactValue("cat"));
   }
 
   @Test
@@ -38,15 +40,23 @@ final class RadioTest extends IntegrationTest {
   void userCanSelectRadioButtonUsingSetValue() {
     Configuration.versatileSetValue = true;
     $(byName("me")).setValue("margarita");
-    assertThat(getSelectedRadio(By.name("me")).val())
-      .isEqualTo("margarita");
+    getSelectedRadio(By.name("me")).shouldHave(exactValue("margarita"));
   }
 
   @Test
   void selenideElement_selectRadio() {
     $(By.name("me")).selectRadio("margarita");
-    assertThat(getSelectedRadio(By.name("me")).val())
-      .isEqualTo("margarita");
+    getSelectedRadio(By.name("me")).shouldHave(exactValue("margarita"));
+  }
+
+  @Test
+  void assertCheckedRadioButton() {
+    $("[name=me][value=margarita]").shouldNotBe(checked);
+
+    $(byName("me")).selectRadio("margarita");
+
+    $("[name=me][value=margarita]").shouldBe(checked);
+    $("[name=me][value=margarita]").shouldHave(attribute("checked")); // just an alternative
   }
 
   @Test
