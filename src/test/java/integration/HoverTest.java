95615e2fa1f6bd706455119657322d6dea4b8066
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-05-04 09:00:18 +0300

    #1447 Add hover with offset

--- src/test/java/integration/HoverTest.java
+++ src/test/java/integration/HoverTest.java
@@ -3,8 +3,15 @@ package integration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.HoverOptions.withOffset;
 
+/**
+ * Hover with offset - calculates offset from the center of the element.
+ * Element '#hoverable' is 800x401 -> its center is 400x200.
+ * Hover to (400+123, 200+122) -> (523, 322)
+ */
 final class HoverTest extends ITest {
   @BeforeEach
   void openTestPageWithJQuery() {
@@ -14,8 +21,16 @@ final class HoverTest extends ITest {
   @Test
   void canEmulateHover() {
     $("#hoverable").hover().shouldHave(text("It's hover"));
+    $("#coords").shouldHave(text("(400, 200)"));
 
     $("h1").hover();
     $("#hoverable").shouldHave(text("It's not hover"));
+    $("#coords").shouldHave(exactText(""));
+  }
+
+  @Test
+  void hoverWithOffset() {
+    $("#hoverable").hover(withOffset(123, 122)).shouldHave(text("It's hover"));
+    $("#coords").shouldHave(text("(523, 322)"));
   }
 }
