425cbe694bcdc2242b83a191a058e0a2ef1dac86
Oleg Berezhnoy <bereg2k@gmail.com>
2021-06-03 18:50:05 +0300

    Add support to Condition.textCaseSensitive for selected options in 'select' element

--- src/main/java/com/codeborne/selenide/conditions/CaseSensitiveText.java
+++ src/main/java/com/codeborne/selenide/conditions/CaseSensitiveText.java
@@ -4,8 +4,10 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Driver;
 import com.codeborne.selenide.impl.Html;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
 
 @ParametersAreNonnullByDefault
 public class CaseSensitiveText extends Condition {
@@ -18,7 +20,19 @@ public class CaseSensitiveText extends Condition {
 
   @Override
   public boolean apply(Driver driver, WebElement element) {
-    return Html.text.containsCaseSensitive(element.getText(), expectedText);
+    String elementText = "select".equalsIgnoreCase(element.getTagName()) ?
+      getSelectedOptionsTexts(element) :
+      element.getText();
+    return Html.text.containsCaseSensitive(elementText, expectedText);
+  }
+
+  private String getSelectedOptionsTexts(WebElement element) {
+    List<WebElement> selectedOptions = new Select(element).getAllSelectedOptions();
+    StringBuilder sb = new StringBuilder();
+    for (WebElement selectedOption : selectedOptions) {
+      sb.append(selectedOption.getText());
+    }
+    return sb.toString();
   }
 
   @Override
