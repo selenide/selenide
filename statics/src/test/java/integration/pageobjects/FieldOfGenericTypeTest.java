bf27202e88e1aaca657923495b8f456b148418cc
Oleg Berezhnoy <bereg2k@gmail.com>
2021-06-03 20:08:45 +0300

    Fixes for unit tests

--- statics/src/test/java/integration/pageobjects/FieldOfGenericTypeTest.java
+++ statics/src/test/java/integration/pageobjects/FieldOfGenericTypeTest.java
@@ -28,30 +28,32 @@ public class FieldOfGenericTypeTest extends IntegrationTest {
     assertThat(page.body).isInstanceOf(DummyTypedElement.class);
     assertThat(page.body.names).isNull();
     assertThat(page.body.selects).isInstanceOf(List.class);
-    assertThat(page.body.selects).hasSize(3);
+    assertThat(page.body.selects).hasSize(4);
     assertThat(page.body.getSelf()).isEqualTo($("body"));
     assertThat(page.body.selects.get(0)).isEqualTo($("select[name=domain]"));
     assertThat(page.body.selects.get(1)).isEqualTo($("select#hero"));
     assertThat(page.body.selects.get(2)).isEqualTo($("select#gender"));
+    assertThat(page.body.selects.get(3)).isEqualTo($("select#cars"));
   }
 
   @Test
   void injectsFoundSelenideElementAsSelf2() {
     AnotherPage page = page(AnotherPage.class);
-    assertThat(page.body.selects).hasSize(3);
+    assertThat(page.body.selects).hasSize(4);
     assertThat(page.body).isInstanceOf(ElementsContainer.class);
     assertThat(page.body.getSelf()).isEqualTo($("body"));
     assertThat(page.body.selects.get(0)).isInstanceOf(WebElement.class);
     assertThat(page.body.selects.get(0)).isEqualTo($("select[name=domain]"));
     assertThat(page.body.selects.get(1)).isEqualTo($("select#hero"));
     assertThat(page.body.selects.get(2)).isEqualTo($("select#gender"));
+    assertThat(page.body.selects.get(3)).isEqualTo($("select#cars"));
   }
 
   @Test
   void injectsFoundSelenideElementAsSelf3() {
     YetAnotherPage page = page(YetAnotherPage.class);
     assertThat(page.body).isNotNull();
-    assertThat(page.body.selects).hasSize(3);
+    assertThat(page.body.selects).hasSize(4);
 
     assertThatThrownBy(() -> page.body.selects.get(0))
       .isInstanceOf(RuntimeException.class)
