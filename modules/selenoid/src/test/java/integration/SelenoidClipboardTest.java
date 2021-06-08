34977a37b029e7fb3eed0ec74f624b2883287c52
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-06-08 22:26:18 +0300

    fix flaky test: remove the cookies consent popup

--- modules/selenoid/src/test/java/integration/SelenoidClipboardTest.java
+++ modules/selenoid/src/test/java/integration/SelenoidClipboardTest.java
@@ -7,33 +7,38 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SelenoidClipboardTest {
 
-    @BeforeEach
-    public void prepare() {
-        Configuration.remote = "http://localhost:4444/wd/hub";
-    }
+  @BeforeEach
+  public void prepare() {
+    Configuration.remote = "http://localhost:4444/wd/hub";
+    open("https://www.w3schools.com/howto/howto_js_copy_clipboard.asp");
+    $(".snigel-cmp-framework").shouldBe(visible);
+    executeJavaScript("const popup = document.getElementById('snigel-cmp-framework'); popup.parentNode.removeChild(popup);");
+  }
 
-    @Test
-    public void getClipboardContent() {
-        open("https://www.w3schools.com/howto/howto_js_copy_clipboard.asp");
-        $("#myInput").should(Condition.attribute("value", "Hello World"));
-        $("[onclick='myFunction()']").should(Condition.visible).click();
-        assertEquals("Hello World", Selenide.clipboard().getText(), "clipboard content doesn't match");
-    }
+  @Test
+  public void getClipboardContent() {
+    $("#myInput").should(Condition.attribute("value", "Hello World"));
+    $("[onclick='myFunction()']").should(Condition.visible).click();
+    assertEquals("Hello World", Selenide.clipboard().getText(), "clipboard content doesn't match");
+  }
 
-    @Test
-    public void setClipboardContent(){
-        open("https://www.w3schools.com/howto/howto_js_copy_clipboard.asp");
-        Selenide.clipboard().setText("John Wick");
-        assertEquals("John Wick", Selenide.clipboard().getText(), "clipboard content doesn't match");
-    }
+  @Test
+  public void setClipboardContent() {
+    Selenide.clipboard().setText("John Wick");
+    assertEquals("John Wick", Selenide.clipboard().getText(), "clipboard content doesn't match");
+  }
 
-    @AfterEach
-    public void tearDown(){
-        closeWebDriver();
-    }
+  @AfterEach
+  public void tearDown() {
+    closeWebDriver();
+  }
 }
