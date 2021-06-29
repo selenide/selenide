1695189247df76c98fd96087e8a5822c9a5549f7
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-06-29 12:12:10 +0300

    #1493 support uploading files from inside of JAR files

--- src/main/java/com/codeborne/selenide/commands/UploadFileFromClasspath.java
+++ src/main/java/com/codeborne/selenide/commands/UploadFileFromClasspath.java
@@ -10,11 +10,16 @@ import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static com.codeborne.selenide.commands.Util.firstOf;
 import static java.lang.Thread.currentThread;
+import static org.apache.commons.io.FileUtils.copyURLToFile;
+import static org.apache.commons.io.FilenameUtils.getName;
 
 @ParametersAreNonnullByDefault
 public class UploadFileFromClasspath implements Command<File> {
@@ -41,10 +46,18 @@ public class UploadFileFromClasspath implements Command<File> {
       throw new IllegalArgumentException("File not found in classpath: " + name);
     }
     try {
-      return new File(resource.toURI());
+      URI uri = resource.toURI();
+      return uri.isOpaque() ? saveAsTemporaryFile(resource) : new File(uri);
     }
-    catch (URISyntaxException e) {
+    catch (URISyntaxException | IOException e) {
       throw new IllegalArgumentException(e);
     }
+  }
+
+  private File saveAsTemporaryFile(URL classpathResource) throws IOException {
+    Path directory = Files.createTempDirectory("selenide-upload-");
+    File fileToUpload = new File(directory.toFile(), getName(classpathResource.getFile()));
+    copyURLToFile(classpathResource, fileToUpload);
+    return fileToUpload;
   }
 }
