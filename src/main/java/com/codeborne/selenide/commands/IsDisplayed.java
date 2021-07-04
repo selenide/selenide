d2d23cfc7f3e8dde1fc5caa6c370c5f7a5064c42
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-07-05 00:07:33 +0300

    #1474 add workaround for NPE in RemoteWebElement.isDisplayed()

--- src/main/java/com/codeborne/selenide/commands/IsDisplayed.java
+++ src/main/java/com/codeborne/selenide/commands/IsDisplayed.java
@@ -7,6 +7,9 @@ import com.codeborne.selenide.impl.Cleanup;
 import com.codeborne.selenide.impl.WebElementSource;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nullable;
@@ -14,12 +17,14 @@ import javax.annotation.ParametersAreNonnullByDefault;
 
 @ParametersAreNonnullByDefault
 public class IsDisplayed implements Command<Boolean> {
+  private static final Logger logger = LoggerFactory.getLogger(IsDisplayed.class);
+
   @Override
   @CheckReturnValue
   public Boolean execute(SelenideElement proxy, WebElementSource locator, @Nullable Object[] args) {
     try {
       WebElement element = locator.getWebElement();
-      return element.isDisplayed();
+      return isDisplayedPatched(element);
     }
     catch (WebDriverException | ElementNotFound elementNotFound) {
       if (Cleanup.of.isInvalidSelectorError(elementNotFound)) {
@@ -29,6 +34,19 @@ public class IsDisplayed implements Command<Boolean> {
     }
     catch (IndexOutOfBoundsException invalidElementIndex) {
       return false;
+    }
+  }
+
+  private boolean isDisplayedPatched(WebElement element) {
+    try {
+      return element.isDisplayed();
+    }
+    catch (NullPointerException e) {
+      if (element instanceof RemoteWebElement) {
+        logger.warn("NPE in RemoteWebElement.isDisplayed, see https://github.com/SeleniumHQ/selenium/issues/9266", e);
+        return false;
+      }
+      throw e;
     }
   }
 }
