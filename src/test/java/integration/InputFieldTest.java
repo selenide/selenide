be4748b0731e84781f1647ef7bd4d3c570fa8132
Alexei Vinogradov <6568032+vinogradoff@users.noreply.github.com>
2021-07-05 22:56:34 +0200

    fixed test to make it stable -> using selenide should/instead of asserts

--- src/test/java/integration/InputFieldTest.java
+++ src/test/java/integration/InputFieldTest.java
@@ -4,7 +4,8 @@ import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static com.codeborne.selenide.Condition.empty;
+import static com.codeborne.selenide.Condition.value;
 
 final class InputFieldTest extends ITest {
   @BeforeEach
@@ -15,22 +16,22 @@ final class InputFieldTest extends ITest {
   @Test
   void selenideClearTest() {
     SelenideElement input = $("#id1");
-    assertThat(input.getValue()).isNullOrEmpty();
+    input.shouldBe(empty);
 
     input.clear();
     input.setValue(",.123");
     input.clear();
     input.setValue("456");
-    assertThat(input.getValue()).isEqualTo("456");
+    input.shouldHave(value("456"));
 
     input.clear();
     input.setValue(",.123");
     input.clear();
     input.setValue("456");
-    assertThat(input.getValue()).isEqualTo("456");
+    input.shouldHave(value("456"));
 
     input.setValue("456");
     input.setValue(null);
-    assertThat(input.getValue()).isEqualTo("");
+    input.shouldBe(empty);
   }
 }
