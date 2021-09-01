417e0daef2613ad003cf7abd5b835bfab30d5f9d
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-09-01 13:57:16 +0300

    use Java 16 features, but still compile selenide.jar for Java8

--- statics/src/main/java/com/codeborne/selenide/WebDriverRunner.java
+++ statics/src/main/java/com/codeborne/selenide/WebDriverRunner.java
@@ -164,7 +164,7 @@ public class WebDriverRunner {
 
   public static void using(WebDriver driver, Runnable lambda) {
     if (hasWebDriverStarted()) {
-      WebDriver previous = getWebDriver();
+      var previous = getWebDriver();
       try {
         lambda.run();
       }
