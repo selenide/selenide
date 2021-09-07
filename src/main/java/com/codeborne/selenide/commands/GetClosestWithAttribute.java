5835f7fef207b10b8bcdc3d02dd6d08179c0f2ee
Vitali Plagov <vitalii.plagov@gmail.com>
2021-09-07 10:03:08 +0300

    Add missing annotations

--- src/main/java/com/codeborne/selenide/commands/GetClosestWithAttribute.java
+++ src/main/java/com/codeborne/selenide/commands/GetClosestWithAttribute.java
@@ -4,14 +4,20 @@ import com.codeborne.selenide.Command;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.impl.WebElementSource;
 
+import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 public class GetClosestWithAttribute implements Command<SelenideElement> {
 
-  @Nullable
   @Override
-  public SelenideElement execute(@Nonnull SelenideElement proxy, WebElementSource locator, @Nullable Object[] args) {
+  @Nullable
+  @CheckReturnValue
+  public SelenideElement execute(
+    @Nonnull SelenideElement proxy,
+    @Nonnull WebElementSource locator,
+    @Nullable Object[] args
+  ) {
     return new GetClosestWithAttributeAndValue().execute(proxy, locator, args);
   }
 }
