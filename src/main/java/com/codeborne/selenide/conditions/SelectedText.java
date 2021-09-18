16684604aaea30ff8f911f4a824a812d4893345b
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-09-19 02:07:41 +0300

    #1313 make SelectedText extend TextCondition

--- src/main/java/com/codeborne/selenide/conditions/SelectedText.java
+++ src/main/java/com/codeborne/selenide/conditions/SelectedText.java
@@ -1,36 +1,25 @@
 package com.codeborne.selenide.conditions;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Driver;
+import com.codeborne.selenide.impl.Html;
 import org.openqa.selenium.WebElement;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
 @ParametersAreNonnullByDefault
-public class SelectedText extends Condition {
-  private final String expectedText;
-  private final ThreadLocal<String> actualResult = ThreadLocal.withInitial(() -> "");
-
+public class SelectedText extends TextCondition {
   public SelectedText(String expectedText) {
-    super("selectedText");
-    this.expectedText = expectedText;
+    super("selected text", expectedText);
   }
 
   @Override
-  public boolean apply(Driver driver, WebElement element) {
-    String selectedText = driver.executeJavaScript(
+  protected boolean match(String actualText, String expectedText) {
+    return Html.text.containsCaseSensitive(actualText, expectedText);
+  }
+
+  @Override
+  protected String getText(Driver driver, WebElement element) {
+    return driver.executeJavaScript(
       "return arguments[0].value.substring(arguments[0].selectionStart, arguments[0].selectionEnd);", element);
-    actualResult.set(selectedText);
-    return actualResult.get().equals(expectedText);
-  }
-
-  @Override
-  public String actualValue(Driver driver, WebElement element) {
-    return "'" + actualResult.get() + "'";
-  }
-
-  @Override
-  public String toString() {
-    return String.format("%s '%s'", getName(), expectedText);
   }
 }
