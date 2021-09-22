ee8dba13ac28e1124425c63c073bff814a158e5b
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-09-23 00:42:38 +0300

    #1571 use standard method WebDriver.getScreenshotAs()
    ... to get a screenshot of a single element.
    
     it seems to work better (than our previous home-made code) on Retina
     display.
    
     NB! I had to change parameter type from WebElement to SelenideElement
     because WebElement cannot be found before switching to frame, that's
     why it need to be (lazy-loaded) SelenideElement.

--- statics/src/test/java/integration/ScreenshotTest.java
+++ statics/src/test/java/integration/ScreenshotTest.java
@@ -2,26 +2,25 @@ package integration;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.impl.ScreenShotLaboratory;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
-import static org.apache.commons.lang3.SystemUtils.IS_OS_WINDOWS;
 import static org.assertj.core.api.Assertions.assertThat;
 
 final class ScreenshotTest extends IntegrationTest {
-  private final String dir = System.getProperty("user.dir");
+  private static final Logger log = LoggerFactory.getLogger(ScreenshotTest.class);
+
+  private static final Color RED = new Color(255, 0, 0);
+  private static final Color BLUE = new Color(0, 255, 255);
 
   @BeforeEach
   void openTestPage() {
@@ -32,58 +31,30 @@ final class ScreenshotTest extends IntegrationTest {
   void canTakeScreenshotOfElement() throws IOException {
     SelenideElement element = $("#small_div");
     File screenshot = element.screenshot();
-    String info = "(Screenshot of element: " + screenshot.getAbsolutePath() + ") ";
-    String path = dir + '/' + Configuration.reportsFolder;
-    String screenshotPath = IS_OS_WINDOWS ? path.replace("/", "\\") : path;
+    assertThat(screenshot).isNotNull();
+    log.info("Analyze the element screenshot {}", screenshot.getAbsolutePath());
+
+    String screenshotFolder = new File(System.getProperty("user.dir"), Configuration.reportsFolder).getCanonicalPath();
+    assertThat(screenshot.getPath())
+      .as(String.format("Screenshot file should be located in %s, but was: %s", screenshotFolder, screenshot.getPath()))
+      .startsWith(screenshotFolder);
 
     BufferedImage img = ImageIO.read(screenshot);
-    assertThat(element.getSize().getWidth())
-      .withFailMessage("Screenshot doesn't fit width " + info)
-      .isEqualTo(img.getWidth());
-    assertThat(element.getSize().getHeight())
-      .withFailMessage("Screenshot doesn't fit height " + info)
-      .isEqualTo(img.getHeight());
-    assertThat(screenshot.getPath())
-      .withFailMessage(String.format("Screenshot file should be located in %s, but was: %s", screenshotPath, screenshot.getPath()))
-      .startsWith(screenshotPath);
+
+    assertThat(element.getSize().getWidth()).isBetween(img.getWidth() / 2, img.getWidth());
+    assertThat(element.getSize().getHeight()).isBetween(img.getHeight() / 2, img.getHeight());
+
+    assertColor(img, "top left corner", 1, 1, RED);
+    assertColor(img, "bottom left corner", 1, img.getHeight() - 1, RED);
+    assertColor(img, "top right corner", img.getWidth() - 1, 1, RED);
+    assertColor(img, "bottom right corner", img.getWidth() - 1, img.getHeight() - 1, RED);
+    assertColor(img, "top left inside of border", 11, 11, BLUE);
+    assertColor(img, "bottom left inside of border", 11, img.getHeight() - 11, BLUE);
+    assertColor(img, "top right inside of border", img.getWidth() - 11, 11, BLUE);
+    assertColor(img, "bottom right inside of border", img.getWidth() - 11, img.getHeight() - 11, BLUE);
   }
 
-  @Test
-  @Disabled
-  void resizeBigImageWidth() {
-    SelenideElement element = $("#wide_div");
-    BufferedImage img = element.screenshotAsImage();
-    assertThat(img.getWidth())
-      .withFailMessage("Screenshot doesn't fit width")
-      .isLessThan(element.getSize().getWidth());
-  }
-
-  @Test
-  @Disabled
-  void resizeBigImageHeight() {
-    SelenideElement element = $("#big_div");
-    BufferedImage img = ScreenShotLaboratory.getInstance().takeScreenshotAsImage(null, element);
-    assertThat(img.getHeight())
-      .withFailMessage("Screenshot doesn't fit height")
-      .isLessThan(element.getSize().getHeight());
-  }
-
-  @Test
-  @Disabled
-  void resizeBigImage() throws IOException {
-    SelenideElement element = $("#huge_div");
-    BufferedImage img = $("#huge_div").screenshotAsImage();
-
-    byte[] screen = ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
-    BufferedImage tmp = ImageIO.read(new ByteArrayInputStream(screen));
-    String errorDetails = String.format("element.location: %s, element.size: %s, screen.size: (%s,%s)",
-      element.getLocation(), element.getSize(), tmp.getWidth(), tmp.getHeight());
-
-    assertThat(img.getWidth())
-      .withFailMessage("Screenshot doesn't fit width - " + errorDetails)
-      .isLessThan(element.getSize().getWidth());
-    assertThat(img.getHeight())
-      .withFailMessage("Screenshot doesn't fit height - " + errorDetails)
-      .isLessThan(element.getSize().getHeight());
+  private void assertColor(BufferedImage img, String description, int x, int y, Color expectedColor) {
+    assertThat(new Color(img.getRGB(x, y))).as(description).isEqualTo(expectedColor);
   }
 }
