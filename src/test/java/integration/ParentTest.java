cbf2abbbb1785f8ff83ee55c04784cedfcac622d
Vitalii Plagov <vitalii.plagov@gmail.com>
2021-09-24 23:37:09 +0300

    Bring back accidentally removed tests
    These two tests have been removed accidentally while resolving the merge
    conflict.

--- src/test/java/integration/ParentTest.java
+++ src/test/java/integration/ParentTest.java
@@ -27,6 +27,31 @@ final class ParentTest extends ITest {
   }
 
   @Test
+  void canGetClosestMatchingAncestorByTagName() {
+    assertThat($("#theHiddenElement").closest("body"))
+      .isEqualTo($("body"));
+    assertThat($("h2").closest("body"))
+      .isEqualTo($("body"));
+    assertThat($(By.name("domain")).closest("div"))
+      .isEqualTo($("#dropdown-list-container"));
+  }
+
+  @Test
+  void canGetClosestMatchingAncestorByClassName() {
+    assertThat($(By.name("domain")).closest(".container"))
+      .isEqualTo($("#dropdown-list-container"));
+
+    assertThat($(".second_row").closest("tr"))
+      .isEqualTo($("#multirowTableSecondRow"));
+    assertThat($(".second_row").closest("table"))
+      .isEqualTo($("#multirowTable"));
+    assertThat($(".second_row").closest(".table"))
+      .isEqualTo($("#multirowTable"));
+    assertThat($(".second_row").closest(".multirow_table"))
+      .isEqualTo($("#multirowTable"));
+  }
+
+  @Test
   void canGetAncestorByTagName() {
     SelenideElement element = $(By.name("domain"));
 
