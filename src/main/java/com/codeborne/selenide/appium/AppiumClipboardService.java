526c85304c7a1303203cd21140c03039ed28eae5
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-09-28 09:50:34 +0300

    release selenide-appium 1.7.0: upgrade to selenide 5.25.0

--- src/main/java/com/codeborne/selenide/appium/AppiumClipboardService.java
+++ src/main/java/com/codeborne/selenide/appium/AppiumClipboardService.java
@@ -4,10 +4,17 @@ import com.codeborne.selenide.Clipboard;
 import com.codeborne.selenide.ClipboardService;
 import com.codeborne.selenide.Driver;
 
+import javax.annotation.CheckReturnValue;
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
+
 /**
  * @since 1.6.10
  */
+@ParametersAreNonnullByDefault
 public class AppiumClipboardService extends ClipboardService {
+  @Nonnull
+  @CheckReturnValue
   @Override
   public Clipboard getClipboard(Driver driver) {
     return new AppiumClipboard(driver);
