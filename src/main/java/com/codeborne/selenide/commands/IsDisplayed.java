9a5ae0c06c18f316be228ad1d72bf883ed9e4786
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-10-21 10:21:53 +0300

    #1474 remove workaround for NPE in RemoteWebElement.isDisplayed()
    it's needed anymore because https://github.com/SeleniumHQ/selenium/issues/9266 was fixed in Selenium 4.0.0

--- src/main/java/com/codeborne/selenide/commands/IsDisplayed.java
+++ src/main/java/com/codeborne/selenide/commands/IsDisplayed.java
@@ -7,9 +7,6 @@ import com.codeborne.selenide.impl.Cleanup;
 import com.codeborne.selenide.impl.WebElementSource;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.RemoteWebElement;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nullable;
@@ -17,14 +14,12 @@ import javax.annotation.ParametersAreNonnullByDefault;
 
 @ParametersAreNonnullByDefault
 public class IsDisplayed implements Command<Boolean> {
-  private static final Logger logger = LoggerFactory.getLogger(IsDisplayed.class);
-
   @Override
   @CheckReturnValue
   public Boolean execute(SelenideElement proxy, WebElementSource locator, @Nullable Object[] args) {
     try {
       WebElement element = locator.getWebElement();
-      return isDisplayedPatched(element);
+      return element.isDisplayed();
     }
     catch (WebDriverException | ElementNotFound elementNotFound) {
       if (Cleanup.of.isInvalidSelectorError(elementNotFound)) {
@@ -34,19 +29,6 @@ public class IsDisplayed implements Command<Boolean> {
     }
     catch (IndexOutOfBoundsException invalidElementIndex) {
       return false;
-    }
-  }
-
-  private boolean isDisplayedPatched(WebElement element) {
-    try {
-      return element.isDisplayed();
-    }
-    catch (NullPointerException e) {
-      if (element instanceof RemoteWebElement) {
-        logger.warn("NPE in RemoteWebElement.isDisplayed, see https://github.com/SeleniumHQ/selenium/issues/9266", e);
-        return false;
-      }
-      throw e;
     }
   }
 }
