889a40b7fce8c9bfecff75e1cd03c7f2f8b9385e
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-01-09 20:43:29 +0200

    log number of firefox processes

--- src/test/java/integration/LogTestNameExtension.java
+++ src/test/java/integration/LogTestNameExtension.java
@@ -1,5 +1,7 @@
 package integration;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
@@ -7,28 +9,41 @@ import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.opentest4j.TestAbortedException;
 
+import javax.annotation.Nonnull;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
 import static integration.BaseIntegrationTest.browser;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.slf4j.LoggerFactory.getLogger;
 
 final class LogTestNameExtension implements BeforeAllCallback, AfterAllCallback, BeforeEachCallback, AfterEachCallback {
   @Override
-  public void beforeAll(ExtensionContext context) {
-    getLogger(context.getDisplayName()).info("Starting tests @ {} {}", browser, memory());
+  public void beforeAll(ExtensionContext context) throws IOException, InterruptedException {
+    getLogger(context.getDisplayName()).info("Starting tests @ {} {}, firefoxes:{}", browser, memory(), firefoxes());
   }
 
   @Override
-  public void afterAll(ExtensionContext context) {
-    getLogger(context.getDisplayName()).info("Finished tests @ {} - {} {}", browser, verdict(context), memory());
+  public void afterAll(ExtensionContext context) throws IOException, InterruptedException {
+    getLogger(context.getDisplayName()).info("Finished tests @ {} - {} {}, firefoxes:{}", browser, verdict(context), memory(), firefoxes());
   }
 
   @Override
-  public void beforeEach(ExtensionContext context) {
-    getLogger(context.getRequiredTestClass().getName()).info("starting {} {}...", context.getDisplayName(), memory());
+  public void beforeEach(ExtensionContext context) throws IOException, InterruptedException {
+    getLogger(context.getRequiredTestClass().getName())
+      .info("starting {} {}, firefoxes:{}...", context.getDisplayName(), memory(), firefoxes());
   }
 
   @Override
-  public void afterEach(ExtensionContext context) {
-    getLogger(context.getRequiredTestClass().getName()).info("finished {} - {} {}", context.getDisplayName(), verdict(context), memory());
+  public void afterEach(ExtensionContext context) throws IOException, InterruptedException {
+    getLogger(context.getRequiredTestClass().getName())
+      .info("finished {} - {} {}, firefoxes:{}", context.getDisplayName(), verdict(context), memory(), firefoxes());
   }
 
   private String verdict(ExtensionContext context) {
@@ -47,5 +62,41 @@ final class LogTestNameExtension implements BeforeAllCallback, AfterAllCallback,
 
   private long mb(long bytes) {
     return bytes / 1024 / 1024;
+  }
+
+  private String firefoxes() throws IOException, InterruptedException {
+    Process process = Runtime.getRuntime().exec(new String[] {"ps", "aux"});
+    try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()))) {
+      writer.append("");
+    }
+
+    if (!process.waitFor(2, TimeUnit.SECONDS)) {
+      System.out.println("Process not closed");
+//      process.destroyForcibly();
+//      process.waitFor(2, TimeUnit.SECONDS);
+      return readOutput(process);
+    }
+    if (process.exitValue() == 0) {
+      return readOutput(process);
+    }
+    else {
+      String b = IOUtils.toString(process.getErrorStream(), UTF_8);
+      return b;
+    }
+  }
+
+  @Nonnull
+  private String readOutput(Process process) throws IOException {
+    try (BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+      List<String> a = in.lines().filter(this::isFirefox).collect(Collectors.toList());
+
+      String s = "Firefoxes: " + a.size() + ":\n" + StringUtils.join(a, "\n");
+      return s;
+    }
+  }
+
+  private boolean isFirefox(String line) {
+    String l = line.toLowerCase();
+    return l.contains("firefox") && !l.contains("java");
   }
 }
