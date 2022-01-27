b4c626452c2f392f7e07d5f702bc6b18e12d9201
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-01-27 16:10:42 +0200

    stabilize flaky Selenium Grid tests
    re-try to start Grid if the port is occasionally already used

--- modules/grid/src/test/java/integration/AbstractGridTest.java
+++ modules/grid/src/test/java/integration/AbstractGridTest.java
@@ -5,12 +5,18 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.grid.Main;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.UncheckedIOException;
 
 import static com.codeborne.selenide.Configuration.timeout;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static org.openqa.selenium.net.PortProber.findFreePort;
 
 abstract class AbstractGridTest extends IntegrationTest {
+  private final Logger log = LoggerFactory.getLogger(getClass());
+
   int hubPort;
 
   @BeforeEach
@@ -26,8 +32,17 @@ abstract class AbstractGridTest extends IntegrationTest {
     }
     closeWebDriver();
 
-    hubPort = findFreePort();
-    Main.main(new String[]{"standalone", "--port", String.valueOf(hubPort)});
+    for (int tries = 0; tries < 3; tries++) {
+      int port = findFreePort();
+      try {
+        Main.main(new String[]{"standalone", "--port", String.valueOf(port)});
+        hubPort = port;
+        break;
+      }
+      catch (UncheckedIOException portAlreadyUsed) {
+        log.warn("Failed to start Selenium Grid on port {}", port, portAlreadyUsed);
+      }
+    }
 
     timeout = 4000;
   }
