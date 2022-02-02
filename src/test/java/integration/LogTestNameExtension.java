3aa39d56caa2f5e5c6d179e1701bbeb84750fb23
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-02-02 09:05:26 +0200

    log memory usage before and after every test

--- src/test/java/integration/LogTestNameExtension.java
+++ src/test/java/integration/LogTestNameExtension.java
@@ -13,27 +13,39 @@ import static org.slf4j.LoggerFactory.getLogger;
 final class LogTestNameExtension implements BeforeAllCallback, AfterAllCallback, BeforeEachCallback, AfterEachCallback {
   @Override
   public void beforeAll(ExtensionContext context) {
-    getLogger(context.getDisplayName()).info("Starting tests @ {}", browser);
+    getLogger(context.getDisplayName()).info("Starting tests @ {} {}", browser, memory());
   }
 
   @Override
   public void afterAll(ExtensionContext context) {
-    getLogger(context.getDisplayName()).info("Finished tests @ {} - {}", browser, verdict(context));
+    getLogger(context.getDisplayName()).info("Finished tests @ {} - {} {}", browser, verdict(context), memory());
   }
 
   @Override
   public void beforeEach(ExtensionContext context) {
-    getLogger(context.getRequiredTestClass().getName()).info("starting {} ...", context.getDisplayName());
+    getLogger(context.getRequiredTestClass().getName()).info("starting {} {}...", context.getDisplayName(), memory());
   }
 
   @Override
   public void afterEach(ExtensionContext context) {
-    getLogger(context.getRequiredTestClass().getName()).info("finished {} - {}", context.getDisplayName(), verdict(context));
+    getLogger(context.getRequiredTestClass().getName()).info("finished {} - {} {}", context.getDisplayName(), verdict(context), memory());
   }
 
   private String verdict(ExtensionContext context) {
     return context.getExecutionException().isPresent() ?
       (context.getExecutionException().get() instanceof TestAbortedException ? "skipped" : "NOK") :
       "OK";
+  }
+
+  private String memory() {
+    long freeMemory = Runtime.getRuntime().freeMemory();
+    long maxMemory = Runtime.getRuntime().maxMemory();
+    long totalMemory = Runtime.getRuntime().totalMemory();
+    long usedMemoty = totalMemory - freeMemory;
+    return "used:" + mb(usedMemoty) + ", free:" + mb(freeMemory) + ", total:" + mb(totalMemory) + ", max:" + mb(maxMemory);
+  }
+
+  private long mb(long bytes) {
+    return bytes / 1024 / 1024;
   }
 }
