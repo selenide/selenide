7fac57bfba4c19fa30d95b4ce54a0384f92ec5de
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-02-06 14:02:02 +0200

    #1650 reuse code: ByText and ByTagAndText use the same xpath

--- src/main/java/com/codeborne/selenide/selector/ByText.java
+++ src/main/java/com/codeborne/selenide/selector/ByText.java
@@ -1,21 +1,13 @@
 package com.codeborne.selenide.selector;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.Quotes;
-
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
 
-import static com.codeborne.selenide.selector.Xpath.NORMALIZE_SPACE_XPATH;
-
 @ParametersAreNonnullByDefault
-public class ByText extends By.ByXPath {
-  protected final String elementText;
-
+public class ByText extends ByTagAndText {
   public ByText(String elementText) {
-    super(".//*/text()[" + NORMALIZE_SPACE_XPATH + " = " + Quotes.escape(elementText) + "]/parent::*");
-    this.elementText = elementText;
+    super("*", elementText);
   }
 
   @Override
@@ -23,11 +15,5 @@ public class ByText extends By.ByXPath {
   @Nonnull
   public String toString() {
     return "by text: " + elementText;
-  }
-
-  @CheckReturnValue
-  @Nonnull
-  String getXPath() {
-    return super.toString().replace("By.xpath: ", "");
   }
 }
