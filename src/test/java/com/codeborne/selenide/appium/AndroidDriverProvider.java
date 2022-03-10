73e330c85d7e52e463022410b63d22d72d087e41
Milena Sobolewska-Lesniewska <milena@MilenaGskMacBookPro.local>
2022-03-10 10:19:12 +0100

    using options not Capabilities

--- src/test/java/com/codeborne/selenide/appium/AndroidDriverProvider.java
+++ src/test/java/com/codeborne/selenide/appium/AndroidDriverProvider.java
@@ -2,22 +2,17 @@ package com.codeborne.selenide.appium;
 
 import com.codeborne.selenide.WebDriverProvider;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.remote.AutomationName;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 
-import static io.appium.java_client.remote.AndroidMobileCapabilityType.APP_ACTIVITY;
-import static io.appium.java_client.remote.AndroidMobileCapabilityType.APP_PACKAGE;
-import static io.appium.java_client.remote.MobileCapabilityType.DEVICE_NAME;
-import static io.appium.java_client.remote.MobileCapabilityType.FULL_RESET;
-import static io.appium.java_client.remote.MobileCapabilityType.NEW_COMMAND_TIMEOUT;
-import static io.appium.java_client.remote.MobileCapabilityType.PLATFORM_NAME;
-import static io.appium.java_client.remote.MobileCapabilityType.VERSION;
 import static org.openqa.selenium.remote.CapabilityType.APPLICATION_NAME;
 
 public class AndroidDriverProvider implements WebDriverProvider {
@@ -25,18 +20,19 @@ public class AndroidDriverProvider implements WebDriverProvider {
   @Nonnull
   @Override
   public WebDriver createDriver(@Nonnull Capabilities capabilities) {
-    DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-    desiredCapabilities.merge(capabilities);
-    desiredCapabilities.setCapability(PLATFORM_NAME, "Android");
-    desiredCapabilities.setCapability(DEVICE_NAME, "Android Emulator");
-    desiredCapabilities.setCapability(VERSION, "9.0");
-    desiredCapabilities.setCapability(APPLICATION_NAME, "Appium");
-    desiredCapabilities.setCapability(APP_PACKAGE, "com.android.calculator2");
-    desiredCapabilities.setCapability(APP_ACTIVITY, "com.android.calculator2.Calculator");
-    desiredCapabilities.setCapability(NEW_COMMAND_TIMEOUT, 11);
-    desiredCapabilities.setCapability(FULL_RESET, false);
+    UiAutomator2Options options = new UiAutomator2Options();
+    options.merge(capabilities);
+    options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
+    options.setPlatformName("Android");
+    options.setDeviceName("Android Emulator");
+    options.setPlatformVersion("9.0");
+    options.setCapability(APPLICATION_NAME, "Appium");
+    options.setAppPackage("com.android.calculator2");
+    options.setAppActivity("com.android.calculator2.Calculator");
+    options.setNewCommandTimeout(Duration.ofSeconds(11));
+    options.setFullReset(false);
     try {
-      return new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+      return new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), options);
     } catch (MalformedURLException e) {
       throw new RuntimeException(e);
     }
