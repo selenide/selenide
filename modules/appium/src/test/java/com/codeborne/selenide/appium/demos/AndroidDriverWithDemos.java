cba9856a71ae4055ff2656c84512b39bcc88d0fa
Milena Sobolewska-Lesniewska <milena@MilenaGskMacBookPro.local>
2022-03-10 10:19:12 +0100

    using options not Capabilities

--- modules/appium/src/test/java/com/codeborne/selenide/appium/demos/AndroidDriverWithDemos.java
+++ modules/appium/src/test/java/com/codeborne/selenide/appium/demos/AndroidDriverWithDemos.java
@@ -2,11 +2,12 @@ package com.codeborne.selenide.appium.demos;
 
 import com.codeborne.selenide.WebDriverProvider;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.remote.AutomationName;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
+import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
 import java.io.File;
 import java.io.IOException;
@@ -15,39 +16,43 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import static org.apache.commons.io.FileUtils.copyInputStreamToFile;
+import static org.openqa.selenium.remote.CapabilityType.APPLICATION_NAME;
 
 public class AndroidDriverWithDemos implements WebDriverProvider {
-    @Override
-    public WebDriver createDriver(@Nonnull Capabilities capabilities) {
-        File app = downloadApk();
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.merge(capabilities);
-        desiredCapabilities.setCapability(MobileCapabilityType.VERSION, "4.4.2");
-        desiredCapabilities.setCapability("automationName", "Appium");
-        desiredCapabilities.setCapability("platformName", "Android");
-        desiredCapabilities.setCapability("deviceName", "Android Emulator");
-        desiredCapabilities.setCapability("app", app.getAbsolutePath());
-        desiredCapabilities.setCapability("appPackage", "io.appium.android.apis");
-        desiredCapabilities.setCapability("appActivity", ".ApiDemos");
+  @CheckReturnValue
+  @Nonnull
+  @Override
+  public WebDriver createDriver(@Nonnull Capabilities capabilities) {
+    File app = downloadApk();
 
-        try {
-            return new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    UiAutomator2Options options = new UiAutomator2Options();
+    options.merge(capabilities);
+    options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
+    options.setPlatformName("Android");
+    options.setDeviceName("Android Emulator");
+    options.setPlatformVersion("4.4.2");
+    options.setCapability(APPLICATION_NAME, "Appium");
+    options.setApp(app.getAbsolutePath());
+    options.setAppPackage("io.appium.android.apis");
+    options.setAppActivity(".ApiDemos");
 
-    private File downloadApk() {
-        File apk = new File("build/ApiDemos-debug.apk");
-        if (!apk.exists()) {
-            String url = "https://github.com/appium/sample-code/blob/master/sample-code/apps/ApiDemos/bin/ApiDemos-debug.apk?raw=true";
-            try (InputStream in = new URL(url).openStream()) {
-                copyInputStreamToFile(in, apk);
-            }
-            catch (IOException e) {
-                throw new AssertionError("Failed to download apk", e);
-            }
-        }
-        return apk;
+    try {
+      return new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), options);
+    } catch (MalformedURLException e) {
+      throw new RuntimeException(e);
     }
+  }
+
+  private File downloadApk() {
+    File apk = new File("build/ApiDemos-debug.apk");
+    if (!apk.exists()) {
+      String url = "https://github.com/appium/sample-code/blob/master/sample-code/apps/ApiDemos/bin/ApiDemos-debug.apk?raw=true";
+      try (InputStream in = new URL(url).openStream()) {
+        copyInputStreamToFile(in, apk);
+      } catch (IOException e) {
+        throw new AssertionError("Failed to download apk", e);
+      }
+    }
+    return apk;
+  }
 }
