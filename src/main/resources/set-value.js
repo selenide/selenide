593e6fc900500d90dd2be53d4025a9bc213324cb
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-04-27 00:33:18 +0300

    trigger "blur" event on previous active element
    * I hope it fixes flaky test FastSetValueTest
    * now we set value and trigger JS events with a single JS call - it should be a bit faster than 2 JS calls.

--- src/main/resources/set-value.js
+++ src/main/resources/set-value.js
@@ -1,9 +1,27 @@
 (function(webelement, text) {
-  if (webelement.getAttribute('readonly') != undefined) return 'Cannot change value of readonly element';
-  if (webelement.getAttribute('disabled') != undefined) return 'Cannot change value of disabled element';
+
+  function trigger(target, ...eventNames) {
+    for (const i in eventNames) {
+      try {
+        const event = document.createEvent('HTMLEvents');
+        event.initEvent(eventNames[i], true, true);
+        target.dispatchEvent(event);
+      }
+      catch (staleElementException) {
+        console.log('failed to trigger event', eventNames[i])
+      }
+    }
+  }
+
+  if (webelement.getAttribute('readonly') !== null) return 'Cannot change value of readonly element';
+  if (webelement.getAttribute('disabled') !== null) return 'Cannot change value of disabled element';
+
+  trigger(document.activeElement, 'blur');
   webelement.focus();
-  var maxlength = webelement.getAttribute('maxlength') == null ? -1 : parseInt(webelement.getAttribute('maxlength'));
-  webelement.value = maxlength == -1 ? text : text.length <= maxlength ? text : text.substring(0, maxlength);
+  const maxlength = webelement.getAttribute('maxlength') == null ? -1 : parseInt(webelement.getAttribute('maxlength'));
+  webelement.value = maxlength === -1 ? text : text.length <= maxlength ? text : text.substring(0, maxlength);
+  trigger(webelement, 'focus', 'keydown', 'keypress', 'input', 'keyup', 'change');
+
   return "";
 })(arguments[0], arguments[1]);
 
