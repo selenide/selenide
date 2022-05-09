ae60cd2bd949717a2569c34ed1283d4594eece61
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-05-09 19:11:55 +0300

    #1433 remove code that was not needed after introducing SelenideNettyClientFactory
    and didn't work anyway because method `config.readTimeout(readTimeout)` returns a new instance.

--- src/main/java/com/codeborne/selenide/webdriver/SelenideNettyClientFactory.java
+++ src/main/java/com/codeborne/selenide/webdriver/SelenideNettyClientFactory.java
@@ -5,16 +5,20 @@ import org.openqa.selenium.remote.http.ClientConfig;
 import org.openqa.selenium.remote.http.HttpClient;
 import org.openqa.selenium.remote.http.HttpClientName;
 import org.openqa.selenium.remote.http.netty.NettyClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.codeborne.selenide.webdriver.HttpClientTimeouts.defaultReadTimeout;
 
 @AutoService(HttpClient.Factory.class)
 @HttpClientName("selenide-netty-client-factory")
 public class SelenideNettyClientFactory extends NettyClient.Factory {
+  private static final Logger logger = LoggerFactory.getLogger(SelenideNettyClientFactory.class);
 
   @Override
   public HttpClient createClient(ClientConfig config) {
     ClientConfig configWithShorterTimeout = config.readTimeout(defaultReadTimeout);
+    logger.info("Changed readTimeout from {} to {}", config.readTimeout(), configWithShorterTimeout.readTimeout());
     return super.createClient(configWithShorterTimeout);
   }
 }
