30208f058e804ff349e6b55efc39c13d3c5d8d84
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-05-20 00:39:05 +0300

    2.0.1: upgrade to Selenide 6.5.0

---
+++ modules/appium/src/main/java/com/codeborne/selenide/appium/commands/AppiumClear.java
@@ -0,0 +1,27 @@
+package com.codeborne.selenide.appium.commands;
+
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.commands.Clear;
+import com.codeborne.selenide.impl.WebElementSource;
+import io.appium.java_client.AppiumDriver;
+
+import javax.annotation.CheckReturnValue;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
+public class AppiumClear extends Clear {
+  @Nonnull
+  @CheckReturnValue
+  @Override
+  public SelenideElement execute(SelenideElement proxy, WebElementSource locator, @Nullable Object[] args) {
+    if (locator.driver().getWebDriver() instanceof AppiumDriver) {
+      locator.findAndAssertElementIsInteractable().clear();
+      return proxy;
+    }
+    else {
+      return super.execute(proxy, locator, args);
+    }
+  }
+}
