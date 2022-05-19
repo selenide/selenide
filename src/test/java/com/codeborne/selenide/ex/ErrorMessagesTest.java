848e5d0724353c5906e6315a71b1dbd7b9a2b12b
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-05-19 20:07:24 +0300

    #1696 run tests on TR locale to detect invalid usages of toLowerCase()
    e.g. "WHAT IS SELENIDE".toLowerCase() -> "what ıs selenıde" which is not equal to "what is selenide".

--- src/test/java/com/codeborne/selenide/ex/ErrorMessagesTest.java
+++ src/test/java/com/codeborne/selenide/ex/ErrorMessagesTest.java
@@ -1,17 +1,20 @@
 package com.codeborne.selenide.ex;
 
 import com.codeborne.selenide.SelenideConfig;
+import integration.UseLocaleExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.Locale;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 final class ErrorMessagesTest {
+  @RegisterExtension
+  private static final UseLocaleExtension useLocale = new UseLocaleExtension("en");
+
   private final ChromeDriver webDriver = mock(ChromeDriver.class);
   private final SelenideConfig config = new SelenideConfig().reportsFolder("build/reports/tests");
 
@@ -24,7 +27,6 @@ final class ErrorMessagesTest {
 
   @Test
   void formatsTimeoutToReadable() {
-    Locale.setDefault(Locale.UK);
     assertThat(ErrorMessages.timeout(0))
       .isEqualToIgnoringNewLines("Timeout: 0 ms.");
     assertThat(ErrorMessages.timeout(1))
