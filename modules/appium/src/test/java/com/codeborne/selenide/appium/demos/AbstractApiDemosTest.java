0da28916509e26288943ed0c4780002915b723c4
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-05-31 09:04:30 +0300

    remove unused method driver()

--- modules/appium/src/test/java/com/codeborne/selenide/appium/demos/AbstractApiDemosTest.java
+++ modules/appium/src/test/java/com/codeborne/selenide/appium/demos/AbstractApiDemosTest.java
@@ -1,12 +1,10 @@
 package com.codeborne.selenide.appium.demos;
 
 import com.codeborne.selenide.Configuration;
-import io.appium.java_client.android.AndroidDriver;
 import org.junit.jupiter.api.BeforeEach;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public abstract class AbstractApiDemosTest {
   @BeforeEach
@@ -15,10 +13,5 @@ public abstract class AbstractApiDemosTest {
     Configuration.browserSize = null;
     Configuration.browser = AndroidDriverWithDemos.class.getName();
     open();
-  }
-
-  @SuppressWarnings("unchecked")
-  protected AndroidDriver driver() {
-    return (AndroidDriver) getWebDriver();
   }
 }
