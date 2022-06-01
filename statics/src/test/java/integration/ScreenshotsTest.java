492076264d0871a7df87ac3d70e1c06afc9ee378
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-06-01 17:44:44 +0300

    #1799 fix test: we open browser of size 1200x960 in tests

--- statics/src/test/java/integration/ScreenshotsTest.java
+++ statics/src/test/java/integration/ScreenshotsTest.java
@@ -59,7 +59,7 @@ final class ScreenshotsTest extends IntegrationTest {
     BufferedImage img = ImageIO.read(screenshot);
 
     assertThat(img.getWidth()).isBetween(1200, 3000);
-    assertThat(img.getHeight()).isBetween(2000, 3000);
+    assertThat(img.getHeight()).isBetween(960, 3000);
 
     assertThat(screenshots.getContextScreenshots().get(0)).isSameAs(screenshot);
   }
