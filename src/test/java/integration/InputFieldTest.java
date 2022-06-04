beec0f320e7838ef4f65f3ce6a586d91d831c7f4
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-06-04 12:53:59 +0300

    #1497 revert $.clear implementation
    * it didn't work in Safari anyway
    * it started failing in Firefox after some of its updates
    * it didn't work with TextArea (it cleared only one line of multi-line TextArea)

--- src/test/java/integration/InputFieldTest.java
+++ src/test/java/integration/InputFieldTest.java
@@ -24,6 +24,7 @@ final class InputFieldTest extends ITest {
     input.clear();
     input.setValue(",.123");
     input.clear();
+    assertThat(input.getValue()).isEqualTo("");
     input.setValue("456");
     assertThat(input.getValue()).isEqualTo("456");
 
