78a8974a6cbb6625e8291a033eb452aa9e0521eb
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-06-05 20:22:01 +0300

    store screenshots in the same folders where are corresponding test reports

--- statics/src/test/java/integration/errormessages/Helper.java
+++ statics/src/test/java/integration/errormessages/Helper.java
@@ -5,33 +5,43 @@ import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.ex.UIAssertionError;
 import org.assertj.core.api.Condition;
 import org.openqa.selenium.TakesScreenshot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.MalformedURLException;
+import java.nio.file.Paths;
 
 import static com.codeborne.selenide.WebDriverRunner.isFirefox;
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class Helper {
-  static void assertScreenshot(UIAssertionError expected) {
-    if (WebDriverRunner.getWebDriver() instanceof TakesScreenshot) {
-      assertThat(expected.getScreenshot().getImage())
-        .contains(Configuration.reportsFolder);
-    }
-  }
+  private static final Logger log = LoggerFactory.getLogger(Helper.class);
 
   static Condition<Throwable> screenshot() {
-    return new Condition<Throwable>("Screenshot in folder " + Configuration.reportsFolder) {
+    return new Condition<>("Screenshot in folder " + Configuration.reportsFolder) {
       @Override
       public boolean matches(Throwable assertionError) {
         if (!(assertionError instanceof UIAssertionError)) return false;
         if (!(WebDriverRunner.getWebDriver() instanceof TakesScreenshot)) return true;
 
         String image = ((UIAssertionError) assertionError).getScreenshot().getImage();
-        return image != null && image.contains(Configuration.reportsFolder);
+        log.info("image: {}", image);
+        log.info("reportsFolder: {}", Configuration.reportsFolder);
+        log.info("reportsFolder.abs: {}", Paths.get(Configuration.reportsFolder).toAbsolutePath());
+        try {
+          log.info("reportsFolder.url: {}", Paths.get(Configuration.reportsFolder).toAbsolutePath().toUri().toURL().toExternalForm());
+
+          return image != null && image.contains(
+            Paths.get(Configuration.reportsFolder).toAbsolutePath().toUri().toURL().toExternalForm());
+        }
+        catch (MalformedURLException e) {
+          throw new RuntimeException(e);
+        }
       }
     };
   }
 
   static Condition<Throwable> screenshot(String path) {
-    return new Condition<Throwable>("Screenshot in sub-folder " + path) {
+    return new Condition<>("Screenshot in sub-folder " + path) {
       @Override
       public boolean matches(Throwable assertionError) {
         if (!(assertionError instanceof UIAssertionError)) return false;
@@ -46,7 +56,7 @@ public class Helper {
   }
 
   static Condition<Throwable> webElementNotFound(String selector) {
-    return new Condition<Throwable>("Selenium error message for missing element " + selector) {
+    return new Condition<>("Selenium error message for missing element " + selector) {
       @Override
       public boolean matches(Throwable assertionError) {
         if (isFirefox()) {
