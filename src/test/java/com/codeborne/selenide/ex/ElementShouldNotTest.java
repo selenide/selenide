b2ced32a039cb9f5aa5471b825a86a42cdc4f3c7
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-06-06 12:14:20 +0300

    fix wording of some conditions to sound grammatically correct
    * "Element should visible" -> "Element should be visible"
    * "Element should hidden" -> "Element should be hidden"

--- src/test/java/com/codeborne/selenide/ex/ElementShouldNotTest.java
+++ src/test/java/com/codeborne/selenide/ex/ElementShouldNotTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebElement;
 
 import static com.codeborne.selenide.CheckResult.Verdict.REJECT;
-import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.visible;
 import static java.lang.System.lineSeparator;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -18,7 +18,7 @@ final class ElementShouldNotTest {
   @Test
   void testToString() {
     CheckResult checkResult = new CheckResult(REJECT, "visible:false");
-    ElementShouldNot elementShould = new ElementShouldNot(driver, "by.name: selenide", "be ", appear, checkResult,
+    ElementShouldNot elementShould = new ElementShouldNot(driver, "by.name: selenide", "be ", visible, checkResult,
       mock(WebElement.class), new Throwable("Error message"));
     assertThat(elementShould).hasMessage("Element should not be visible {by.name: selenide}" + lineSeparator() +
       "Element: '<null displayed:false></null>'" + lineSeparator() +
