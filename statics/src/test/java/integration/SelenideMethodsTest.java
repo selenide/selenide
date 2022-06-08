b6b21ef338268f44bac11248298ba9bf7c75acdd
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-06-08 11:52:33 +0300

    #1497 move setValue/getValue tests from SelenideMethodsTest to a separate class GetSetValueTest
    SelenideMethodsTest was too long, need to split it even more.

--- statics/src/test/java/integration/SelenideMethodsTest.java
+++ statics/src/test/java/integration/SelenideMethodsTest.java
@@ -1,7 +1,6 @@
 package integration;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.ex.ElementNotFound;
@@ -17,7 +16,6 @@ import org.openqa.selenium.WebElement;
 import java.time.Duration;
 
 import static com.codeborne.selenide.CollectionCondition.size;
-import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Condition.be;
 import static com.codeborne.selenide.Condition.cssClass;
 import static com.codeborne.selenide.Condition.cssValue;
@@ -181,72 +179,6 @@ final class SelenideMethodsTest extends IntegrationTest {
     assertThat($("br").innerText()).isEqualTo("");
     assertThatThrownBy(() -> $("#missing").innerText())
       .isInstanceOf(ElementNotFound.class);
-  }
-
-  @Test
-  void getValue_canBeEmpty() {
-    assertThat($(By.name("password")).val()).isEqualTo("");
-    assertThat($(By.name("password")).getValue()).isEqualTo("");
-  }
-
-  @Test
-  void getValue_canBeNull() {
-    assertThat($("h2").val()).isNull();
-    assertThat($("h2").getValue()).isNull();
-  }
-
-  @Test
-  void getValue_throwsError_ifElementNotFound() {
-    assertThatThrownBy(() -> $("#missing").val())
-      .isInstanceOf(ElementNotFound.class);
-    assertThatThrownBy(() -> $("#missing").getValue())
-      .isInstanceOf(ElementNotFound.class);
-  }
-
-  @Test
-  void userCanSetValueToTextField() {
-    $(By.name("password")).setValue("john");
-    $(By.name("password")).val("sherlyn");
-    $(By.name("password")).shouldHave(value("sherlyn"));
-    $(By.name("password")).shouldHave(value("sherlyn"), ofMillis(1000));
-    assertThat($(By.name("password")).val())
-      .isEqualTo("sherlyn");
-  }
-
-  @Test
-  void userCanSetValueToTextArea() {
-    Configuration.fastSetValue = false;
-    $("#empty-text-area").val("text for textarea");
-    $("#empty-text-area").shouldHave(value("text for textarea"));
-
-    Configuration.fastSetValue = true;
-    $("#empty-text-area").val("another text");
-    $("#empty-text-area").shouldHave(value("another text"));
-  }
-
-  @Test
-  void userCannotSetValueLongerThanMaxLength() {
-    $(By.name("password")).shouldHave(attribute("maxlength", "24"));
-    $(By.name("password")).val("123456789_123456789_123456789_");
-    $(By.name("password")).shouldHave(value("123456789_123456789_1234"));
-  }
-
-  @Test
-  void valueCheckIgnoresDifferenceInInvisibleCharacters() {
-    $(By.name("password")).setValue("john   \u00a0 Malkovich");
-    $(By.name("password")).shouldHave(value("john Malkovich"));
-
-    $("#text-area").setValue("john   \u00a0 \r\nMalkovich\n");
-    $("#text-area").shouldHave(value("john Malkovich"));
-  }
-
-  @Test
-  void userCanAppendValueToTextfield() {
-    $(By.name("password")).val("Sherlyn");
-    $(By.name("password")).append(" theron");
-    $(By.name("password")).shouldHave(value("Sherlyn theron"));
-    assertThat($(By.name("password")).val())
-      .isEqualTo("Sherlyn theron");
   }
 
   @Test
