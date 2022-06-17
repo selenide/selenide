2779d7d2a64058d16d81fb24ecdb720a13302dd5
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-06-18 02:30:20 +0300

    #1581 add tests for laziness of $.getSelectedOptions()
    it's lazy-loaded, but the implementation is not really effective: it iterates all the options one-by-one. It may be slow for long options list.

--- src/test/java/integration/MultipleSelectTest.java
+++ src/test/java/integration/MultipleSelectTest.java
@@ -6,9 +6,11 @@ import com.codeborne.selenide.ex.ElementNotFound;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.CollectionCondition.exactTextsCaseSensitive;
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.text;
+import static java.time.Duration.ofSeconds;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 final class MultipleSelectTest extends ITest {
@@ -73,5 +75,26 @@ final class MultipleSelectTest extends ITest {
     select.$("option[value=cat]").setSelected(false);
 
     select.getSelectedOptions().shouldHave(size(0));
+  }
+
+  @Test
+  void getSelectedOptions_isLazyLoaded() {
+    $("#button-select-people").click(); // selects options after 500 ms
+    select.getSelectedOptions().shouldHave(
+      exactTextsCaseSensitive("Мастер", "Маргарита"), ofSeconds(1)
+    );
+  }
+
+  @Test
+  void getSelectedOptions_isLazyLoaded2() {
+    SelenideElement select = $("#cars"); // initially empty, options are loaded after 600 ms.
+    select.getSelectedOptions().shouldHave(
+      exactTextsCaseSensitive("Toyota", "Mazda"), ofSeconds(1)
+    );
+
+    $("#button-select-german-cars").click(); // selects options after 600 ms
+    select.getSelectedOptions().shouldHave(
+      exactTextsCaseSensitive("Mercedes", "BMW", "Audi"), ofSeconds(1)
+    );
   }
 }
