9d2f7e94b94b6ae973916fd5c8b6e7083a886803
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-08-03 10:24:50 +0300

    restore IE support in setValue (#1907)
    * restore IE support in setValue
    
    it was occasionally removed in Selenide 6.5.0

--- src/main/resources/set-value.js
+++ src/main/resources/set-value.js
@@ -1,11 +1,21 @@
 (function(webelement, text) {
 
-  function trigger(target, ...eventNames) {
+  function triggerEvent(target, eventName) {
+    if (document.createEventObject) {
+      let event = document.createEventObject();
+      target.fireEvent('on' + eventName, event);
+    }
+    else {
+      let event = document.createEvent('HTMLEvents');
+      event.initEvent(eventName, true, true);
+      target.dispatchEvent(event);
+    }
+  }
+
+  function trigger(target, eventNames) {
     for (const i in eventNames) {
       try {
-        const event = document.createEvent('HTMLEvents');
-        event.initEvent(eventNames[i], true, true);
-        target.dispatchEvent(event);
+        triggerEvent(target, eventNames[i]);
       }
       catch (staleElementException) {
         console.log('failed to trigger event', eventNames[i])
@@ -16,11 +26,11 @@
   if (webelement.getAttribute('readonly') !== null) return 'Cannot change value of readonly element';
   if (webelement.getAttribute('disabled') !== null) return 'Cannot change value of disabled element';
 
-  trigger(document.activeElement, 'blur');
+  trigger(document.activeElement, ['blur']);
   webelement.focus();
   const maxlength = webelement.getAttribute('maxlength') == null ? -1 : parseInt(webelement.getAttribute('maxlength'));
   webelement.value = maxlength === -1 ? text : text.length <= maxlength ? text : text.substring(0, maxlength);
-  trigger(webelement, 'focus', 'keydown', 'keypress', 'input', 'keyup', 'change');
+  trigger(webelement, ['focus', 'keydown', 'keypress', 'input', 'keyup', 'change']);
 
   return "";
 })(arguments[0], arguments[1]);
