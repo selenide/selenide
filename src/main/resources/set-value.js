f3e748e988610b451e3b48b53da418a94e26f3b5
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-08-03 09:47:13 +0300

    use array instead of ...params - hopefully it works better in IE

--- src/main/resources/set-value.js
+++ src/main/resources/set-value.js
@@ -12,7 +12,7 @@
     }
   }
 
-  function trigger(target, ...eventNames) {
+  function trigger(target, eventNames) {
     for (const i in eventNames) {
       try {
         triggerEvent(target, eventNames[i]);
@@ -26,11 +26,11 @@
   if (webelement.getAttribute('readonly') !== null) return 'Cannot change value of readonly element';
   if (webelement.getAttribute('disabled') !== null) return 'Cannot change value of disabled element';
 
-  trigger(document.activeElement, 'blur');
+  trigger(document.activeElement, ['blur']);
   webelement.focus();
   const maxlength = webelement.getAttribute('maxlength') == null ? -1 : parseInt(webelement.getAttribute('maxlength'));
   webelement.value = maxlength === -1 ? text : text.length <= maxlength ? text : text.substring(0, maxlength);
-  trigger(webelement, 'focus', 'keydown', 'keypress', 'input', 'keyup', 'change');
+  trigger(webelement, ['focus', 'keydown', 'keypress', 'input', 'keyup', 'change']);
 
   return "";
 })(arguments[0], arguments[1]);
