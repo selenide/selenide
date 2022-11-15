7a05cccd20bb4aba70180e4d42de123b09c89c60
Boris Osipov <osipov.boris@gmail.com>
2022-11-15 16:25:19 +0300

    Fix an issue when a new tab size in headless chrome has incorrect size.

--- src/main/java/com/codeborne/selenide/webdriver/BrowserResizer.java
+++ src/main/java/com/codeborne/selenide/webdriver/BrowserResizer.java
@@ -5,14 +5,20 @@ import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.regex.Pattern;
 
 class BrowserResizer {
+  private static final Pattern DIMENSION_REGEX = Pattern.compile("\\d+x\\d+");
   private static final Logger log = LoggerFactory.getLogger(BrowserResizer.class);
 
   void adjustBrowserPosition(Config config, WebDriver driver) {
-    if (config.browserPosition() != null) {
-      log.info("Set browser position to {}", config.browserPosition());
-      String[] coordinates = config.browserPosition().split("x");
+    String browserPosition = config.browserPosition();
+    if (browserPosition != null) {
+      if (!isValidDimension(browserPosition)) {
+        throw new IllegalArgumentException(String.format("Browser position %s is incorrect", browserPosition));
+      }
+      log.info("Set browser position to {}", browserPosition);
+      String[] coordinates = browserPosition.split("x");
       int x = Integer.parseInt(coordinates[0]);
       int y = Integer.parseInt(coordinates[1]);
       Point target = new Point(x, y);
@@ -24,12 +30,20 @@ class BrowserResizer {
   }
 
   void adjustBrowserSize(Config config, WebDriver driver) {
-    if (config.browserSize() != null) {
-      log.info("Set browser size to {}", config.browserSize());
-      String[] dimension = config.browserSize().split("x");
+    String browserSize = config.browserSize();
+    if (browserSize != null) {
+      if (!isValidDimension(browserSize)) {
+        throw new IllegalArgumentException(String.format("Browser size %s is incorrect", browserSize));
+      }
+      log.info("Set browser size to {}", browserSize);
+      String[] dimension = browserSize.split("x");
       int width = Integer.parseInt(dimension[0]);
       int height = Integer.parseInt(dimension[1]);
       driver.manage().window().setSize(new org.openqa.selenium.Dimension(width, height));
     }
+  }
+
+  static boolean isValidDimension(String dimension) {
+    return DIMENSION_REGEX.matcher(dimension).matches();
   }
 }
--- src/test/java/com/codeborne/selenide/webdriver/BrowserResizerTest.java
+++ src/test/java/com/codeborne/selenide/webdriver/BrowserResizerTest.java
@@ -2,10 +2,17 @@ package com.codeborne.selenide.webdriver;
 
 import com.codeborne.selenide.SelenideConfig;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
@@ -32,5 +39,42 @@ final class BrowserResizerTest {
     factory.adjustBrowserPosition(config, webdriver);
 
     verify(webdriver.manage().window()).setPosition(new Point(20, 40));
+  }
+
+  @Test
+  void throwErrorIfBrowserWindowSizeIsIncorrect() {
+    config.browserSize("1600,800");
+
+    assertThatThrownBy(() -> factory.adjustBrowserSize(config, webdriver))
+      .isInstanceOf(IllegalArgumentException.class)
+      .hasMessage("Browser size 1600,800 is incorrect");
+  }
+
+  @Test
+  void throwErrorIfBrowserPositionIsIncorrect() {
+    config.browserPosition("1600,800");
+
+    assertThatThrownBy(() -> factory.adjustBrowserPosition(config, webdriver))
+      .isInstanceOf(IllegalArgumentException.class)
+      .hasMessage("Browser position 1600,800 is incorrect");
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideSize")
+  void validateDimensionTest(String input, boolean expected) {
+    assertThat(BrowserResizer.isValidDimension(input)).isEqualTo(expected);
+  }
+
+  private static Stream<Arguments> provideSize() {
+    return Stream.of(
+      Arguments.of("1920x1080", true),
+      Arguments.of("0x0", true),
+      Arguments.of("123X123", false),
+      Arguments.of("123х123", false),
+      Arguments.of("456", false),
+      Arguments.of("123,123", false),
+      Arguments.of("", false),
+      Arguments.of(" ", false)
+    );
   }
 }
