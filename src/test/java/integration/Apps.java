f056dc5cd86943bcc39717ea77611695b1210655
amuthan-sakthivel <amuthan.sakthivel@clipboardhealth.com>
2022-11-22 21:14:18 +0530

    #99 added code to launch deep links in SelenideAppium. Added ios scroll test and sauce lab demo apps

--- src/test/java/integration/Apps.java
+++ src/test/java/integration/Apps.java
@@ -8,15 +8,28 @@ import java.net.URL;
 import static org.apache.commons.io.FileUtils.copyInputStreamToFile;
 
 public class Apps {
+
+  private static final String APPIUM_APPS_URL = "https://github.com/appium/appium/raw/master/packages/appium/sample-code/apps/";
+  private static final String SAUCE_LAB_APPS_URL = "https://github.com/saucelabs/my-demo-app-rn/releases/download/v1.3.0/";
+
   public static File downloadAndroidApp() {
-    return downloadSampleApp("ApiDemos-debug.apk");
+    return downloadSampleApp(APPIUM_APPS_URL, "ApiDemos-debug.apk");
   }
 
   public static File downloadIosApp() {
-    return downloadSampleApp("TestApp.app.zip");
+    return downloadSampleApp(APPIUM_APPS_URL, "TestApp.app.zip");
   }
 
-  private static File downloadSampleApp(String filename) {
+  //can be used later if needed
+  public static File downloadSauceLabAndroidApp() {
+    return downloadSampleApp(SAUCE_LAB_APPS_URL, "Android-MyDemoAppRN.1.3.0.build-244.apk");
+  }
+
+  public static File downloadSauceLabIosApp() {
+    return downloadSampleApp(SAUCE_LAB_APPS_URL, "iOS-Simulator-MyRNDemoApp.1.3.0-162.zip");
+  }
+
+  private static File downloadSampleApp(String remoteFilePath, String filename) {
     File app = new File("build/apps", filename);
     if (app.exists()) {
       return app;
@@ -26,7 +39,7 @@ public class Apps {
       throw new RuntimeException("Failed to create dir " + app.getParentFile().getAbsolutePath());
     }
 
-    String url = "https://github.com/appium/appium/raw/master/packages/appium/sample-code/apps/" + filename;
+    String url =  remoteFilePath + filename;
     try (InputStream in = new URL(url).openStream()) {
       copyInputStreamToFile(in, app);
     } catch (IOException e) {
