7e7040bfbc2ec674355f9a43f79beebd18589027
vrossello <vrossello@travelcompositor.com>
2022-12-09 00:01:14 +0100

    Selenide#2062: $.select* : don't fire change event if value is unchanged

--- src/main/resources/select-options-by-partial-text.js
+++ src/main/resources/select-options-by-partial-text.js
@@ -18,15 +18,20 @@
     return {disabledOptions: disabledOptionsTexts};
   }
 
+  const previousSelectedIndex = select.selectedIndex;
+  var fireChangeEvent = false;
   for (let requestedPartialText of texts) {
     optionByText(requestedPartialText).selected = 'selected';
+    fireChangeEvent = fireChangeEvent || previousSelectedIndex !== select.selectedIndex;
   }
 
   const event = document.createEvent('HTMLEvents');
   event.initEvent('click', true, true);
   select.dispatchEvent(event);
-  event.initEvent('change', true, true);
-  select.dispatchEvent(event);
+  if (fireChangeEvent) {
+    event.initEvent('change', true, true);
+    select.dispatchEvent(event);
+  }
 
   return {};
 })(arguments[0], arguments[1])
