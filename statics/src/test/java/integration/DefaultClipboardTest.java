9f8f42a20b9d33ba493a7e598a11d21e6f6554df
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-02-21 09:26:15 +0200

    hope to fix test: first wait until the button is initialized

--- statics/src/test/java/integration/DefaultClipboardTest.java
+++ statics/src/test/java/integration/DefaultClipboardTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.ClipboardConditions.content;
 import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.enabled;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.clipboard;
@@ -25,20 +26,20 @@ public class DefaultClipboardTest extends IntegrationTest {
   @Test
   public void getClipboard() {
     $("#text-to-copy").shouldHave(attribute("value", "Hello World"));
-    $("#button-copy-text").shouldBe(visible).click();
+    $("#button-copy-text").shouldBe(visible, enabled).click();
     assertThat(clipboard().getText()).isEqualTo("Hello World");
   }
 
   @Test
   public void waitForClipboardContent() {
     $("#text-to-copy").val("Hello slow World");
-    $("#button-copy-text-slowly").click();
+    $("#button-copy-text-slowly").shouldBe(visible, enabled).click();
     clipboard().shouldHave(content("Hello slow World"), ofMillis(1500));
   }
 
   @Test
   public void errorMessage() {
-    $("#button-copy-text").shouldBe(visible).click();
+    $("#button-copy-text").shouldBe(visible, enabled).click();
     assertThatThrownBy(() ->
       clipboard().shouldHave(content("Goodbye World"), ofMillis(22))
     )
@@ -50,7 +51,7 @@ public class DefaultClipboardTest extends IntegrationTest {
 
   @Test
   public void errorMessage_negativeCase() {
-    $("#button-copy-text").click();
+    $("#button-copy-text").shouldBe(visible, enabled).click();
     assertThatThrownBy(() ->
       clipboard().shouldNotHave(content("Hello World"), ofMillis(300))
     )
