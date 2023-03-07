27cf5133efbfe343e6e43f5192c6ffc640887407
amuthan-sakthivel <amuthan.sakthivel@clipboardhealth.com>
2023-03-07 13:05:13 +0530

    added commands to SelenideAppiumElement

--- modules/appium/src/main/java/com/codeborne/selenide/appium/SelenideAppiumElement.java
+++ modules/appium/src/main/java/com/codeborne/selenide/appium/SelenideAppiumElement.java
@@ -4,4 +4,6 @@ import com.codeborne.selenide.SelenideElement;
 
 public interface SelenideAppiumElement extends SelenideElement {
   SelenideAppiumElement hideKeyboard();
+  SelenideAppiumElement scrollTo();
+  SelenideAppiumElement scroll(AppiumScrollOptions appiumScrollOptions);
 }
