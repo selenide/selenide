8af905b21c6a604fc95cfd5d344ce62e7db31437
amuthan-sakthivel <amuthan.sakthivel@clipboardhealth.com>
2023-03-22 13:40:58 +0530

    fixed terminateApp not working

--- modules/appium/src/main/java/com/codeborne/selenide/appium/AppiumNavigator.java
+++ modules/appium/src/main/java/com/codeborne/selenide/appium/AppiumNavigator.java
@@ -1,15 +1,15 @@
 package com.codeborne.selenide.appium;
 
-import static com.codeborne.selenide.appium.WebdriverUnwrapper.instanceOf;
-
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
-import java.util.HashMap;
-import java.util.function.Supplier;
+
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.function.Supplier;
+
+import static com.codeborne.selenide.appium.WebdriverUnwrapper.instanceOf;
 
 @ParametersAreNonnullByDefault
 public class AppiumNavigator {
@@ -22,15 +22,13 @@ public class AppiumNavigator {
 
   public void terminateApp(AppiumDriver driver, String appId) {
     SelenideLogger.run("terminate app", appId, () -> {
-      HashMap<String, String> params = new HashMap<>();
       if (instanceOf(driver, AndroidDriver.class)) {
-        params.put("appId", appId);
+        AppiumDriverRunner.getAndroidDriver().terminateApp(appId);
       } else if (instanceOf(driver, IOSDriver.class)) {
-        params.put("bundleId", appId);
+        AppiumDriverRunner.getIosDriver().terminateApp(appId);
       } else {
         throw new IllegalArgumentException("Cannot terminate application for unknown driver " + driver.getClass());
       }
-      driver.executeScript("mobile:terminateApp", params);
     });
   }
 
