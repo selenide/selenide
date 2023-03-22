26487c5701061295cd3ba44c4461f50fb4ad212d
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-03-22 20:38:53 +0200

    #76 #139 don't allow to call $.scroll(options) in web browser

--- modules/appium/src/main/java/com/codeborne/selenide/appium/commands/AppiumScrollTo.java
+++ modules/appium/src/main/java/com/codeborne/selenide/appium/commands/AppiumScrollTo.java
@@ -13,12 +13,15 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.interactions.PointerInput;
 import org.openqa.selenium.interactions.Sequence;
 
+import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.Arrays;
 import java.util.Optional;
 
+import static com.codeborne.selenide.appium.AppiumScrollOptions.with;
+import static com.codeborne.selenide.appium.ScrollDirection.DOWN;
 import static com.codeborne.selenide.appium.WebdriverUnwrapper.cast;
 import static com.codeborne.selenide.commands.Util.firstOf;
 import static java.time.Duration.ofMillis;
@@ -26,37 +29,55 @@ import static java.util.Collections.singletonList;
 
 @ParametersAreNonnullByDefault
 public class AppiumScrollTo implements Command<SelenideElement> {
+  private static final AppiumScrollOptions DEFAULT_OPTIONS = with(DOWN, 30);
   private final ScrollTo webImplementation = new ScrollTo();
 
   @Override
   @Nonnull
   public SelenideElement execute(SelenideElement proxy, WebElementSource locator, @Nullable Object[] args) {
+    AppiumScrollOptions scrollOptions = extractOptions(args);
+
     Optional<AppiumDriver> driver = cast(locator.driver(), AppiumDriver.class);
     if (!driver.isPresent()) {
-      return webImplementation.execute(proxy, locator, args);
+      scrollInWebBrowser(proxy, locator, args, scrollOptions);
     }
+    else {
+      scrollInMobile(driver.get(), locator, scrollOptions);
+    }
+    return proxy;
+  }
 
-    AppiumDriver appiumDriver = driver.get();
-    AppiumScrollOptions appiumScrollOptions;
-
+  @Nonnull
+  @CheckReturnValue
+  private AppiumScrollOptions extractOptions(@Nullable Object[] args) {
     if (args == null || args.length == 0) {
-      appiumScrollOptions = AppiumScrollOptions.with(ScrollDirection.DOWN, 30);
+      return DEFAULT_OPTIONS;
     } else if (args.length == 1) {
-      appiumScrollOptions = firstOf(args);
+      return firstOf(args);
     } else {
       throw new IllegalArgumentException("Unsupported scroll arguments: " + Arrays.toString(args));
     }
+  }
+
+  private void scrollInWebBrowser(SelenideElement proxy, WebElementSource locator, @Nullable Object[] args,
+                                             AppiumScrollOptions appiumScrollOptions) {
+    if (appiumScrollOptions != DEFAULT_OPTIONS) {
+      throw new IllegalArgumentException("Scrolling with options is only supported for mobile drivers");
+    }
+    webImplementation.execute(proxy, locator, args);
+  }
+
+  private void scrollInMobile(AppiumDriver appiumDriver, WebElementSource locator, AppiumScrollOptions scrollOptions) {
     int currentSwipeCount = 0;
     String previousPageSource = "";
 
     while (isElementNotDisplayed(locator)
-      && isNotEndOfPage(appiumDriver, previousPageSource)
-      && isLessThanMaxSwipeCount(currentSwipeCount, appiumScrollOptions.getMaxSwipeCounts())) {
+           && isNotEndOfPage(appiumDriver, previousPageSource)
+           && isLessThanMaxSwipeCount(currentSwipeCount, scrollOptions.getMaxSwipeCounts())) {
       previousPageSource = appiumDriver.getPageSource();
-      performScroll(appiumDriver, appiumScrollOptions);
+      performScroll(appiumDriver, scrollOptions.getScrollDirection());
       currentSwipeCount++;
     }
-    return proxy;
   }
 
   private boolean isLessThanMaxSwipeCount(int currentSwipeCount, int maxSwipeCounts) {
@@ -79,16 +100,16 @@ public class AppiumScrollTo implements Command<SelenideElement> {
     return appiumDriver.manage().window().getSize();
   }
 
-  private void performScroll(AppiumDriver appiumDriver, AppiumScrollOptions scrollOptions) {
+  private void performScroll(AppiumDriver appiumDriver, ScrollDirection scrollDirection) {
     Dimension size = getMobileDeviceSize(appiumDriver);
-    AppiumScrollCoordinates scrollCoordinates = getScrollCoordinates(scrollOptions, size);
+    AppiumScrollCoordinates scrollCoordinates = getScrollCoordinates(scrollDirection, size);
     PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
     Sequence sequenceToPerformScroll = getSequenceToPerformScroll(finger, scrollCoordinates);
     appiumDriver.perform(singletonList(sequenceToPerformScroll));
   }
 
-  private AppiumScrollCoordinates getScrollCoordinates(AppiumScrollOptions scrollOptions, Dimension size) {
-    if (scrollOptions.getScrollDirection() == ScrollDirection.UP) {
+  private AppiumScrollCoordinates getScrollCoordinates(ScrollDirection scrollDirection, Dimension size) {
+    if (scrollDirection == ScrollDirection.UP) {
       return new AppiumScrollCoordinates(size.getWidth() / 2, (int) (size.getHeight() * 0.25),
                                          size.getWidth() / 2, size.getHeight() / 2);
     } else {
