4886115711de7f85baa1cbd22aaca2c942516356
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-03-22 19:42:06 +0200

    #145 #146 simplify code for app termination

--- modules/appium/src/main/java/com/codeborne/selenide/appium/AppiumNavigator.java
+++ modules/appium/src/main/java/com/codeborne/selenide/appium/AppiumNavigator.java
@@ -3,13 +3,12 @@ package com.codeborne.selenide.appium;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.InteractsWithApps;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.function.Supplier;
 
-import static com.codeborne.selenide.appium.WebdriverUnwrapper.instanceOf;
+import static com.codeborne.selenide.appium.WebdriverUnwrapper.cast;
 
 @ParametersAreNonnullByDefault
 public class AppiumNavigator {
@@ -22,13 +21,9 @@ public class AppiumNavigator {
 
   public void terminateApp(AppiumDriver driver, String appId) {
     SelenideLogger.run("terminate app", appId, () -> {
-      if (instanceOf(driver, AndroidDriver.class)) {
-        AppiumDriverRunner.getAndroidDriver().terminateApp(appId);
-      } else if (instanceOf(driver, IOSDriver.class)) {
-        AppiumDriverRunner.getIosDriver().terminateApp(appId);
-      } else {
-        throw new IllegalArgumentException("Cannot terminate application for unknown driver " + driver.getClass());
-      }
+      cast(driver, InteractsWithApps.class)
+        .map(mobileDriver -> mobileDriver.terminateApp(appId))
+        .orElseThrow(() -> new UnsupportedOperationException("Driver does not support app termination: " + driver.getClass()));
     });
   }
 
