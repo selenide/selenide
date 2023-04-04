eb826bffda8ae68b8fcca873a7c7d7de29f80149
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-04-04 10:35:32 +0300

    improve logging when downloading files

--- src/main/java/com/codeborne/selenide/BrowserDownloadsFolder.java
+++ src/main/java/com/codeborne/selenide/BrowserDownloadsFolder.java
@@ -1,14 +1,19 @@
 package com.codeborne.selenide;
 
 import com.codeborne.selenide.impl.FileHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
 
+import static java.util.stream.Collectors.toList;
 import static org.apache.commons.io.FileUtils.cleanDirectory;
+import static org.apache.commons.io.FileUtils.listFiles;
 
 /**
  * A unique folder per browser.
@@ -16,6 +21,7 @@ import static org.apache.commons.io.FileUtils.cleanDirectory;
  */
 @ParametersAreNonnullByDefault
 public final class BrowserDownloadsFolder extends DownloadsFolder {
+  private static final Logger log = LoggerFactory.getLogger(BrowserDownloadsFolder.class);
   private BrowserDownloadsFolder(File folder) {
     super(folder);
   }
@@ -24,12 +30,21 @@ public final class BrowserDownloadsFolder extends DownloadsFolder {
   public void cleanupBeforeDownload() {
     try {
       if (folder.exists()) {
+        log.debug("Going to clean folder {} - found files: {}", folder, filesIn(folder));
         cleanDirectory(folder);
+        log.debug("After clean folder {}: {}", folder, filesIn(folder));
+      }
+      else {
+        log.debug("Folder {} doesn't exist. Nothing to clean up.", folder);
       }
     }
     catch (IOException e) {
       throw new IllegalStateException("Failed to cleanup folder " + folder.getAbsolutePath(), e);
     }
+  }
+
+  private Collection<String> filesIn(File dir) {
+    return listFiles(dir, null, false).stream().map(f -> f.getName()).collect(toList());
   }
 
   @Override
