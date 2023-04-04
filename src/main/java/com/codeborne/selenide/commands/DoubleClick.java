ec4c95493295bc4792ca50b73b1b375a698ac4f3
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-04-04 12:10:29 +0300

    #2135 #2133 fix double-click with offset
    it didn't really use given offset because method `Actions.doubleClick(element)` moves cursor to the center point of the element.

--- src/main/java/com/codeborne/selenide/commands/DoubleClick.java
+++ src/main/java/com/codeborne/selenide/commands/DoubleClick.java
@@ -1,13 +1,9 @@
 package com.codeborne.selenide.commands;
 
 import com.codeborne.selenide.Driver;
-import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.impl.JavaScript;
-import com.codeborne.selenide.impl.WebElementSource;
 import org.openqa.selenium.WebElement;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 @ParametersAreNonnullByDefault
@@ -16,23 +12,22 @@ public class DoubleClick extends Click {
   private final JavaScript jsSource = new JavaScript("dblclick.js");
 
   @Override
-  @Nonnull
-  public SelenideElement execute(SelenideElement proxy, WebElementSource locator, @Nullable Object[] args) {
-    return super.execute(proxy, locator, args);
-  }
-
-  @Override
   protected void clickViaJS(Driver driver, WebElement element, int offsetX, int offsetY) {
     jsSource.execute(driver, element, offsetX, offsetY);
   }
 
   @Override
-  protected void defaultClick(WebElement element, Driver driver) {
-    driver.actions().doubleClick(element).build().perform();
+  protected void defaultClick(Driver driver, WebElement element) {
+    driver.actions()
+      .doubleClick(element)
+      .build().perform();
   }
 
   @Override
   protected void defaultClick(Driver driver, WebElement element, int offsetX, int offsetY) {
-    driver.actions().moveByOffset(offsetX, offsetY).doubleClick(element).build().perform();
+    driver.actions()
+      .moveToElement(element, offsetX, offsetY)
+      .doubleClick()
+      .build().perform();
   }
 }
