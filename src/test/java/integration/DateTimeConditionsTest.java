b6444f911d321e07785ad00dedb94f81bead94cd
Maksim <au6ojlut@users.noreply.github.com>
2023-05-12 09:20:13 +0300

    #2268 Fix test. Looks like windows has different line breaks

--- src/test/java/integration/DateTimeConditionsTest.java
+++ src/test/java/integration/DateTimeConditionsTest.java
@@ -34,22 +34,22 @@ public class DateTimeConditionsTest extends ITest {
       $("#birthdate").shouldHave(datetime(birthday.minusDays(1), "yyyy/MM/dd HH:mm:ss"))
     )
       .isInstanceOf(ElementShould.class)
-      .hasMessageStartingWith("""
-        Element should have datetime value: "2022/10/10 12:13:14" (with datetime value format: "yyyy/MM/dd HH:mm:ss") {#birthdate}
-        Element: '<input id="birthdate" type="text" value="2022/10/11 12:13:14"></input>'
-        Actual value: 2022/10/11 12:13:14"""
-      );
+      .hasMessageStartingWith(
+        "Element should have datetime value: \"2022/10/10 12:13:14\" (with datetime value format: \"yyyy/MM/dd HH:mm:ss\") {#birthdate}"
+      )
+      .hasMessageContaining("Element: '<input id=\"birthdate\" type=\"text\" value=\"2022/10/11 12:13:14\"></input>'")
+      .hasMessageContaining("Actual value: 2022/10/11 12:13:14");
 
     assertThatThrownBy(() ->
       $("#birthdate").shouldHave(datetimeBetween(birthday.minusDays(2), birthday.minusDays(1), "yyyy/MM/dd HH:mm:ss"))
     )
       .isInstanceOf(ElementShould.class)
-      .hasMessageStartingWith("""
-        Element should have datetime value between ["2022/10/09 12:13:14", "2022/10/10 12:13:14"] \
-        (with datetime value format: "yyyy/MM/dd HH:mm:ss") {#birthdate}
-        Element: '<input id="birthdate" type="text" value="2022/10/11 12:13:14"></input>'
-        Actual value: 2022/10/11 12:13:14"""
-      );
+      .hasMessageStartingWith(
+        "Element should have datetime value between [\"2022/10/09 12:13:14\", \"2022/10/10 12:13:14\"] " +
+        "(with datetime value format: \"yyyy/MM/dd HH:mm:ss\") {#birthdate}"
+      )
+      .hasMessageContaining("Element: '<input id=\"birthdate\" type=\"text\" value=\"2022/10/11 12:13:14\"></input>'")
+      .hasMessageContaining("Actual value: 2022/10/11 12:13:14");
   }
 
   @Test
@@ -59,31 +59,29 @@ public class DateTimeConditionsTest extends ITest {
       $("#birthdate").shouldHave(datetime(birthday, "yyyy-MM-dd HH:mm:ss"))
     )
       .isInstanceOf(ElementShould.class)
-      .hasMessageStartingWith("""
-        Element should have datetime value: "2022-10-11 12:13:14" (with datetime value format: "yyyy-MM-dd HH:mm:ss") {#birthdate}
-        Element: '<input id="birthdate" type="text" value="2022/10/11 12:13:14"></input>'
-        Actual value: 2022/10/11 12:13:14"""
-      );
+      .hasMessageStartingWith(
+        "Element should have datetime value: \"2022-10-11 12:13:14\" (with datetime value format: \"yyyy-MM-dd HH:mm:ss\") {#birthdate}"
+      )
+      .hasMessageContaining("Element: '<input id=\"birthdate\" type=\"text\" value=\"2022/10/11 12:13:14\"></input>'")
+      .hasMessageContaining("Actual value: 2022/10/11 12:13:14");
 
     assertThatThrownBy(() ->
       $("#birthdate").shouldHave(datetimeBetween(birthday.minusDays(1), birthday.plusDays(1), "yyyy-MM-dd HH:mm:ss"))
     )
       .isInstanceOf(ElementShould.class)
-      .hasMessageStartingWith("""
-        Element should have datetime value between ["2022-10-10 12:13:14", "2022-10-12 12:13:14"] \
-        (with datetime value format: "yyyy-MM-dd HH:mm:ss") {#birthdate}
-        Element: '<input id="birthdate" type="text" value="2022/10/11 12:13:14"></input>'
-        Actual value: 2022/10/11 12:13:14"""
-      );
+      .hasMessageStartingWith(
+        "Element should have datetime value between [\"2022-10-10 12:13:14\", \"2022-10-12 12:13:14\"] " +
+        "(with datetime value format: \"yyyy-MM-dd HH:mm:ss\") {#birthdate}"
+      )
+      .hasMessageContaining("Element: '<input id=\"birthdate\" type=\"text\" value=\"2022/10/11 12:13:14\"></input>'")
+      .hasMessageContaining("Actual value: 2022/10/11 12:13:14");
 
     assertThatThrownBy(() ->
       $("#birthdate").shouldHave(datetimeFormat("yyyy-MM-dd HH:mm:ss"))
     )
       .isInstanceOf(ElementShould.class)
-      .hasMessageStartingWith("""
-        Element should have datetime value format: "yyyy-MM-dd HH:mm:ss" {#birthdate}
-        Element: '<input id="birthdate" type="text" value="2022/10/11 12:13:14"></input>'
-        Actual value: 2022/10/11 12:13:14"""
-      );
+      .hasMessageStartingWith("Element should have datetime value format: \"yyyy-MM-dd HH:mm:ss\" {#birthdate}")
+      .hasMessageContaining("Element: '<input id=\"birthdate\" type=\"text\" value=\"2022/10/11 12:13:14\"></input>'")
+      .hasMessageContaining("Actual value: 2022/10/11 12:13:14");
   }
 }
