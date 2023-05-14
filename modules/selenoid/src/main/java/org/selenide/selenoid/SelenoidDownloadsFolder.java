d5a0939bcdda4643d101aface9e0ab9d0b7b69a1
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-05-14 14:20:16 +0300

    #19 [refactoring] conver abstract class DownloadsFolder to interface

--- modules/selenoid/src/main/java/org/selenide/selenoid/SelenoidDownloadsFolder.java
+++ modules/selenoid/src/main/java/org/selenide/selenoid/SelenoidDownloadsFolder.java
@@ -1,6 +1,5 @@
 package org.selenide.selenoid;
 
-import com.codeborne.selenide.Config;
 import com.codeborne.selenide.DownloadsFolder;
 import com.codeborne.selenide.Driver;
 import com.codeborne.selenide.files.DownloadedFile;
@@ -14,11 +13,10 @@ import java.util.stream.Collectors;
 import static java.util.Collections.emptyMap;
 import static java.util.stream.Collectors.toList;
 
-public class SelenoidDownloadsFolder extends DownloadsFolder {
+public class SelenoidDownloadsFolder implements DownloadsFolder {
   private final SelenoidClient selenoidClient;
 
   public SelenoidDownloadsFolder(Driver driver) {
-    super(new File("."));
     this.selenoidClient = new SelenoidClient(driver.config().remote(), driver.getSessionId().toString());
   }
 
@@ -31,13 +29,17 @@ public class SelenoidDownloadsFolder extends DownloadsFolder {
   public void deleteIfEmpty() {
   }
 
-  @CheckReturnValue
   @Nonnull
+  @CheckReturnValue
+  @Override
   public List<File> files() {
     List<String> files = selenoidClient.downloads();
     return files.stream().map(name -> new File(name)).collect(Collectors.toList());
   }
 
+  @Nonnull
+  @CheckReturnValue
+  @Override
   public List<DownloadedFile> filesNewerThan(long modifiedAfterTs) {
     return files().stream()
       .map(file -> new DownloadedFile(file, emptyMap()))
