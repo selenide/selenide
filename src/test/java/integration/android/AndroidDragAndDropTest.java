322572e310e35e59f73c566861901b59c9e61312
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-05-14 23:11:25 +0300

    2.8.0: support new Selenide method $.dragAndDrop(options)

--- src/test/java/integration/android/AndroidDragAndDropTest.java
+++ src/test/java/integration/android/AndroidDragAndDropTest.java
@@ -1,16 +1,17 @@
 package integration.android;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
-
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.appium.SelenideAppium;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.DragAndDropOptions.to;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 
 /**
  * Copied from <a href="https://bit.ly/3NE6QsD">Appium repository</a>
@@ -35,6 +36,24 @@ class AndroidDragAndDropTest extends BaseApiDemosTest {
     $(dragText).shouldHave(exactText(""));
 
     from.dragAndDropTo(to);
+
+    $(dragText)
+        .shouldBe(visible)
+        .shouldHave(text("Dropped!"));
+  }
+
+  @Test
+  void dragAndDropWithOptions() {
+    $(By.xpath(".//*[@text='Views']")).click();
+    $(By.xpath(".//*[@text='Drag and Drop']")).click();
+
+    SelenideElement from = $(By.id("io.appium.android.apis:id/drag_dot_1")).shouldBe(visible);
+    SelenideElement to = $(By.id("io.appium.android.apis:id/drag_dot_2")).shouldBe(visible);
+    By dragText = By.id("io.appium.android.apis:id/drag_result_text");
+
+    $(dragText).shouldHave(exactText(""));
+
+    from.dragAndDrop(to(to).usingSeleniumActions());
 
     $(dragText)
         .shouldBe(visible)
