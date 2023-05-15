cf5d9a3a0b1dcf0cff7b5a2dee86ffb260683d97
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-05-15 21:13:00 +0300

    #155 restore support for `$.click(ClickOptions)`
    nowadays we assume users should use a new method `$.click(AppiumClickOptions)`, but technically it's still possible to call `$.click(ClickOptions)`.

--- modules/appium/src/test/java/integration/android/AndroidClickOptionsTest.java
+++ modules/appium/src/test/java/integration/android/AndroidClickOptionsTest.java
@@ -1,11 +1,14 @@
 package integration.android;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.appium.SelenideAppium;
 import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.ClickOptions.usingDefaultMethod;
+import static com.codeborne.selenide.ClickOptions.usingJavaScript;
+import static com.codeborne.selenide.ClickOptions.withTimeout;
+import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 import static com.codeborne.selenide.appium.AppiumClickOptions.doubleTap;
@@ -13,6 +16,8 @@ import static com.codeborne.selenide.appium.AppiumClickOptions.longPress;
 import static com.codeborne.selenide.appium.AppiumClickOptions.tap;
 import static com.codeborne.selenide.appium.AppiumClickOptions.tapWithOffset;
 import static com.codeborne.selenide.appium.SelenideAppium.$;
+import static java.time.Duration.ofSeconds;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class AndroidClickOptionsTest extends BaseApiDemosTest {
   @BeforeEach
@@ -22,19 +27,19 @@ class AndroidClickOptionsTest extends BaseApiDemosTest {
   }
 
   @Test
-  void testAndroidTap()  {
+  void androidTap() {
     $(AppiumBy.xpath(".//*[@text='Views']")).click(tap());
     $(AppiumBy.xpath(".//*[@text='Animation']")).shouldHave(visible);
   }
 
   @Test
-  void testAndroidTapWithOffset()  {
+  void androidTapWithOffset() {
     $(AppiumBy.xpath(".//*[@text='Views']")).click(tapWithOffset(0, -200)); //Find view but click Text
     $(AppiumBy.xpath(".//*[@text='KeyEventText']")).shouldHave(visible);
   }
 
   @Test
-  void testAndroidLongPress() {
+  void androidLongPress() {
     $(AppiumBy.xpath(".//*[@text='Views']")).click();
     $(AppiumBy.xpath(".//*[@text='Expandable Lists']")).click();
     $(AppiumBy.xpath(".//*[@text='1. Custom Adapter']")).click();
@@ -43,12 +48,30 @@ class AndroidClickOptionsTest extends BaseApiDemosTest {
   }
 
   @Test
-  void testAndroidDoubleTap() {
+  void androidDoubleTap() {
     $(AppiumBy.xpath(".//*[@text='Preference']")).click();
     $(AppiumBy.xpath(".//*[@text='1. Preferences from XML']")).click();
     $(AppiumBy.xpath(".//android.widget.CheckBox")).click(doubleTap());
-    $(AppiumBy.xpath(".//android.widget.CheckBox"))
-      .shouldHave(Condition.attribute("checked", "false"));
+    $(AppiumBy.xpath(".//android.widget.CheckBox")).shouldHave(attribute("checked", "false"));
   }
 
+  @Test
+  void selenideClickOptions() {
+    $(AppiumBy.xpath(".//*[@text='Preference']")).click(usingDefaultMethod().offset(3, -3));
+    $(AppiumBy.xpath(".//*[@text='1. Preferences from XML']")).shouldBe(visible);
+  }
+
+  @Test
+  void clickUsingJavascript_notSupported() {
+    assertThatThrownBy(() -> $(AppiumBy.xpath(".//*[@text='Preference']")).click(withTimeout(ofSeconds(2))))
+      .isInstanceOf(UnsupportedOperationException.class)
+      .hasMessage("Click timeout is not supported in mobile");
+  }
+
+  @Test
+  void clickWithTimeout_notSupported() {
+    assertThatThrownBy(() -> $(AppiumBy.xpath(".//*[@text='Preference']")).click(usingJavaScript()))
+      .isInstanceOf(UnsupportedOperationException.class)
+      .hasMessage("Click using JavaScript is not supported in mobile");
+  }
 }
