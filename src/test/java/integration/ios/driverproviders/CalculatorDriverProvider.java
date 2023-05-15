c6e99796741b19eadc16f4b66c54226fbf7bb461
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-05-15 09:33:49 +0300

    add logging when downloading an external app

--- src/test/java/integration/ios/driverproviders/CalculatorDriverProvider.java
+++ src/test/java/integration/ios/driverproviders/CalculatorDriverProvider.java
@@ -1,23 +1,12 @@
 package integration.ios.driverproviders;
 
-import static integration.Apps.downloadIosApp;
-
-import io.appium.java_client.ios.options.XCUITestOptions;
 import java.io.File;
-import java.time.Duration;
+
+import static integration.Apps.downloadIosApp;
 
 public class CalculatorDriverProvider extends IosDriverProvider {
   @Override
   protected File getApplicationUnderTest() {
     return downloadIosApp();
-  }
-
-  protected XCUITestOptions getXcuiTestOptions() {
-    XCUITestOptions options = new XCUITestOptions();
-    // on github actions first test run maybe extremely slow
-    options.setWdaLaunchTimeout(Duration.ofMinutes(10));
-    options.setDeviceName("iPhone 12");
-    options.setFullReset(false);
-    return options;
   }
 }
