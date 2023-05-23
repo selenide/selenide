e96113c53b897ddda38ba43d3cb0450a69a66e2a
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-05-23 19:04:19 +0300

    fix flaky test in "selenide-selenoid": first wait until clipboard has been changed.

--- modules/selenoid/src/test/java/it/selenoid/SelenoidClipboardTest.java
+++ modules/selenoid/src/test/java/it/selenoid/SelenoidClipboardTest.java
@@ -12,7 +12,7 @@ import static com.codeborne.selenide.FileDownloadMode.HTTPGET;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.clipboard;
 import static com.codeborne.selenide.Selenide.open;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SelenoidSetup.class)
 public class SelenoidClipboardTest {
@@ -30,21 +30,21 @@ public class SelenoidClipboardTest {
     $("#text-input").shouldHave(attribute("value", "Hello World"));
     $("#copy-button").shouldBe(visible).click();
     clipboard().shouldHave(content("Hello World"));
-    assertEquals("Hello World", clipboard().getText());
+    assertThat(clipboard().getText()).isEqualTo("Hello World");
   }
 
   @Test
   public void setClipboardContent() {
     clipboard().setText("John Wick");
-    assertEquals("John Wick", clipboard().getText());
     clipboard().shouldHave(content("John Wick"));
+    assertThat(clipboard().getText()).isEqualTo("John Wick");
   }
 
   @Test
   public void setAndGetClipboardMultilineContent() {
     String multilineText = "John\nWick\r\nThe\nGreat\r";
     clipboard().setText(multilineText);
-    assertEquals(multilineText, clipboard().getText());
     clipboard().shouldHave(content(multilineText));
+    assertThat(clipboard().getText()).isEqualTo(multilineText);
   }
 }
