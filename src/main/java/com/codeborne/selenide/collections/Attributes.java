585f8e3559efc89e08db60d2f020cfbc4af9b7ec
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-05-26 01:30:49 +0300

    refactor $$.attributes to use new method `fail(CheckResult)`
    It will speed up the tests: we will not fetch actual attribute values when generating an error message

--- src/main/java/com/codeborne/selenide/collections/Attributes.java
+++ src/main/java/com/codeborne/selenide/collections/Attributes.java
@@ -3,7 +3,6 @@ package com.codeborne.selenide.collections;
 import com.codeborne.selenide.CheckResult;
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Driver;
-import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.ex.AttributesMismatch;
 import com.codeborne.selenide.ex.AttributesSizeMismatch;
 import com.codeborne.selenide.ex.ElementNotFound;
@@ -14,12 +13,14 @@ import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 import static com.codeborne.selenide.CheckResult.Verdict.ACCEPT;
 import static com.codeborne.selenide.CheckResult.Verdict.REJECT;
 import static java.util.Collections.unmodifiableList;
+import static java.util.Objects.requireNonNull;
 
 @ParametersAreNonnullByDefault
 public class Attributes extends CollectionCondition {
@@ -42,12 +43,15 @@ public class Attributes extends CollectionCondition {
       return new CheckResult(REJECT, elements.size());
     }
 
+    List<String> actualAttributeValues = new ArrayList<>(expectedValues.size());
     for (int i = 0; i < expectedValues.size(); i++) {
       WebElement element = elements.get(i);
       String expectedValue = expectedValues.get(i);
       String actualAttributeValue = element.getAttribute(attribute);
+      actualAttributeValues.add(actualAttributeValue);
+
       if (!Objects.equals(actualAttributeValue, expectedValue)) {
-        return new CheckResult(REJECT, actualAttributeValue);
+        return new CheckResult(REJECT, actualAttributeValues);
       }
     }
     return new CheckResult(ACCEPT, null);
@@ -55,18 +59,21 @@ public class Attributes extends CollectionCondition {
 
   @Override
   public void fail(CollectionSource collection,
-                   @Nullable List<WebElement> elements,
+                   CheckResult lastCheckResult,
                    @Nullable Exception cause,
                    long timeoutMs) {
-    if (elements == null || elements.isEmpty()) {
-      throw new ElementNotFound(collection, toString(), timeoutMs, cause);
-    } else if (elements.size() != expectedValues.size()) {
-      throw new AttributesSizeMismatch(collection.driver(), attribute, collection, expectedValues,
-        ElementsCollection.attributes(attribute, elements), explanation, timeoutMs, cause);
-    } else {
-      throw new AttributesMismatch(collection.driver(), attribute, collection, expectedValues,
-        ElementsCollection.attributes(attribute, elements), explanation, timeoutMs, cause);
+    if (lastCheckResult.actualValue() instanceof Integer actualSize) {
+      if (actualSize == 0) {
+        throw new ElementNotFound(collection, toString(), timeoutMs, cause);
+      }
+      else {
+        throw new AttributesSizeMismatch(collection.driver(), attribute, collection, expectedValues,
+          actualSize, explanation, timeoutMs, cause);
+      }
     }
+
+    throw new AttributesMismatch(collection.driver(), attribute, collection, expectedValues,
+      requireNonNull(lastCheckResult.getActualValue()), explanation, timeoutMs, cause);
   }
 
   @Override
