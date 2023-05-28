97e24c65165d7a79fb428435679db5a7831e9aec
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-05-28 11:32:20 +0300

    refactor tests for $$.exactTextsCaseSensitiveInAnyOrder
    replaced useless unit-tests by analogous integration test

--- src/test/java/com/codeborne/selenide/collections/ExactTextsCaseSensitiveInAnyOrderTest.java
+++ src/test/java/com/codeborne/selenide/collections/ExactTextsCaseSensitiveInAnyOrderTest.java
@@ -2,54 +2,9 @@ package com.codeborne.selenide.collections;
 
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Mocks.mockElement;
-import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 final class ExactTextsCaseSensitiveInAnyOrderTest {
-
-  @Test
-  void shouldMatchWithSameOrder() {
-    ExactTextsCaseSensitiveInAnyOrder texts = new ExactTextsCaseSensitiveInAnyOrder(asList("One", "Two", "Three"));
-    assertThat(texts.test(asList(mockElement("One"), mockElement("Two"), mockElement("Three"))))
-      .isTrue();
-  }
-
-  @Test
-  void shouldMatchWithDifferentOrder() {
-    ExactTextsCaseSensitiveInAnyOrder texts = new ExactTextsCaseSensitiveInAnyOrder(asList("One", "Three", "Two"));
-    assertThat(texts.test(asList(mockElement("One"), mockElement("Two"), mockElement("Three"))))
-      .isTrue();
-  }
-
-  @Test
-  void shouldNotMatchWithSameOrderAndDifferentCase() {
-    ExactTextsCaseSensitiveInAnyOrder texts = new ExactTextsCaseSensitiveInAnyOrder("one", "two", "three");
-    assertThat(texts.test(asList(mockElement("One"), mockElement("Two"), mockElement("Three"))))
-      .isFalse();
-  }
-
-  @Test
-  void shouldNotMatchWithDifferentOrderAndDifferentCase() {
-    ExactTextsCaseSensitiveInAnyOrder texts = new ExactTextsCaseSensitiveInAnyOrder("one", "three", "two");
-    assertThat(texts.test(asList(mockElement("One"), mockElement("Two"), mockElement("Three"))))
-      .isFalse();
-  }
-
-  @Test
-  void shouldNotMatchWithSmallerListSize() {
-    ExactTextsCaseSensitiveInAnyOrder texts = new ExactTextsCaseSensitiveInAnyOrder("One", "Two");
-    assertThat(texts.test(asList(mockElement("One"), mockElement("Two"), mockElement("Three"))))
-      .isFalse();
-  }
-
-  @Test
-  void shouldNotMatchWithBiggerListSize() {
-    ExactTextsCaseSensitiveInAnyOrder texts = new ExactTextsCaseSensitiveInAnyOrder("One", "Two", "Three", "four");
-    assertThat(texts.test(asList(mockElement("One"), mockElement("Two"), mockElement("Three"))))
-      .isFalse();
-  }
-
   @Test
   void shouldHaveCorrectToString() {
     assertThat(new ExactTextsCaseSensitiveInAnyOrder("One", "Two"))
