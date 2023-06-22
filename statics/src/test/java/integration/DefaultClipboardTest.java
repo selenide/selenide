f86835cd122d3b2f84ebafadca069ffcada976c1
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-06-22 21:36:48 +0300

    disable clipboard tests in headless mode

--- statics/src/test/java/integration/DefaultClipboardTest.java
+++ statics/src/test/java/integration/DefaultClipboardTest.java
@@ -21,6 +21,7 @@ public class DefaultClipboardTest extends IntegrationTest {
   public void openTestPage() {
     assumeClipboardSupported();
     openFile("clipboard.html");
+    clipboard().setText("");
   }
 
   @Test
