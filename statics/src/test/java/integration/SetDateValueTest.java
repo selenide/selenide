bfa10f90f95fc827df2a19f514644e14c9a11439
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-06-28 19:28:48 +0300

    add methods $.setValue(withDateTime) and $.setValue(withTime)

--- statics/src/test/java/integration/SetDateValueTest.java
+++ statics/src/test/java/integration/SetDateValueTest.java
@@ -4,11 +4,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
+import static com.codeborne.selenide.Condition.exactValue;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.SetValueMethod.JS;
 import static com.codeborne.selenide.SetValueOptions.withDate;
+import static com.codeborne.selenide.SetValueOptions.withDateTime;
+import static com.codeborne.selenide.SetValueOptions.withTime;
 
 final class SetDateValueTest extends IntegrationTest {
   @BeforeEach
@@ -28,5 +33,21 @@ final class SetDateValueTest extends IntegrationTest {
     LocalDate birthday = LocalDate.parse("1978-11-30");
     $("#birthday").setValue(withDate(birthday).usingMethod(JS));
     $("#user-summary").shouldHave(text("User birthday: 1978-11-30"));
+  }
+
+  @Test
+  void canSetDateTime() {
+    openFile("page_with_datetime_inputs.html");
+    $("#birthdate").setValue(withDateTime(LocalDateTime.of(1981, 6, 8, 23, 58, 59)));
+    $("#birthdate").shouldHave(exactValue("1981-06-08T23:58:59.000"));
+  }
+
+  @Test
+  void canSetTime() {
+    openFile("page_with_datetime_inputs.html");
+    $("#open").setValue(withTime(LocalTime.of(7, 12)));
+    $("#close").setValue(withTime(LocalTime.of(17, 59, 58)));
+    $("#open").shouldHave(exactValue("07:12:00.000"));
+    $("#close").shouldHave(exactValue("17:59:58.000"));
   }
 }
