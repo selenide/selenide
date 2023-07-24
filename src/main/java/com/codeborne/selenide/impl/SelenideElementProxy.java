7d84ac83ff304f7b907e51121cad5f62e5638770
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-07-24 11:00:36 +0300

    restore built-in waiting for method $.getWrappedElement()
    this reverts commit be1601f1
    
    When I added method "getWrappedElement" to "methodsToSkipLogging", it didn't wait for element anymore. It broke some tests.

--- src/main/java/com/codeborne/selenide/impl/SelenideElementProxy.java
+++ src/main/java/com/codeborne/selenide/impl/SelenideElementProxy.java
@@ -35,7 +35,6 @@ class SelenideElementProxy implements InvocationHandler {
     "as",
     "getAlias",
     "toWebElement",
-    "getWrappedElement",
     "toString",
     "getSearchCriteria",
     "$",
