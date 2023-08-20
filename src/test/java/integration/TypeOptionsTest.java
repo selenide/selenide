117d9cc159b8565779d5e1092750388d61115ec6
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-08-20 11:37:27 +0300

    #2422 #2420 add test for $.type(Keys.*)

--- src/test/java/integration/TypeOptionsTest.java
+++ src/test/java/integration/TypeOptionsTest.java
@@ -2,6 +2,7 @@ package integration;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.TypeOptions.text;
@@ -14,14 +15,27 @@ class TypeOptionsTest extends ITest {
   }
 
   @Test
-  void typeOptions() {
+  void typeString() {
     $("#username_input").setValue("test");
+
     $("#username_input").type("abc");
     $("#username_input").shouldHave(attribute("value", "abc"));
 
     $("#username_input").type(text("test"));
     $("#username_input").shouldHave(attribute("value", "test"));
+  }
 
+  @Test
+  void typeKeys() {
+    $("#username_input").type(Keys.NUMPAD1);
+    $("#username_input").type(Keys.NUMPAD2);
+    $("#username_input").type(Keys.NUMPAD3);
+    $("#username_input").shouldHave(attribute("value", "3"));
+  }
+
+  @Test
+  void typeWithOptions() {
+    $("#username_input").setValue("test");
     $("#username_input").type(text("abc").withDelay(ofMillis(100)).clearFirst(false));
     $("#username_input").shouldHave(attribute("value", "testabc"));
   }
