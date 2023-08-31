3b25105678676ac86be38d0140fccf12f47a1976
qwez <github@qwez.space>
2023-08-31 10:13:03 +0300

    Appium Scroll: added ability to set top and bottom point coordinates for scroll

--- modules/appium/src/main/java/com/codeborne/selenide/appium/AppiumScrollOptions.java
+++ modules/appium/src/main/java/com/codeborne/selenide/appium/AppiumScrollOptions.java
@@ -5,10 +5,20 @@ public class AppiumScrollOptions {
   private final ScrollDirection scrollDirection;
   private final int maxSwipeCounts;
   private static final int DEFAULT_MAX_SWIPE_COUNTS = 30;
+  private final float topPointHeightPercent;
+  private static final float DEFAULT_TOP_POINT_HEIGHT_PERCENT = 0.25f;
+  private final float bottomPointHeightPercent;
+  private static final float DEFAULT_BOTTOM_POINT_HEIGHT_PERCENT = 0.5f;
 
   private AppiumScrollOptions(ScrollDirection scrollDirection, int maxSwipeCounts) {
+    this(scrollDirection, maxSwipeCounts, DEFAULT_TOP_POINT_HEIGHT_PERCENT, DEFAULT_BOTTOM_POINT_HEIGHT_PERCENT);
+  }
+
+  private AppiumScrollOptions(ScrollDirection scrollDirection, int maxSwipeCounts, float topPointHeightPercent, float bottomPointHeightPercent) {
     this.scrollDirection = scrollDirection;
     this.maxSwipeCounts = maxSwipeCounts;
+    this.topPointHeightPercent = topPointHeightPercent;
+    this.bottomPointHeightPercent = bottomPointHeightPercent;
   }
 
   public static AppiumScrollOptions with(ScrollDirection scrollDirection, int maxSwipeCounts) {
@@ -31,6 +41,14 @@ public class AppiumScrollOptions {
     return new AppiumScrollOptions(ScrollDirection.UP, maxSwipeCounts);
   }
 
+  public static AppiumScrollOptions up(float top, float bottom) {
+    return new AppiumScrollOptions(ScrollDirection.UP, DEFAULT_MAX_SWIPE_COUNTS, top, bottom);
+  }
+
+  public static AppiumScrollOptions down(float top, float bottom) {
+    return new AppiumScrollOptions(ScrollDirection.DOWN, DEFAULT_MAX_SWIPE_COUNTS, top, bottom);
+  }
+
   public int getMaxSwipeCounts() {
     return maxSwipeCounts;
   }
@@ -39,4 +57,11 @@ public class AppiumScrollOptions {
     return scrollDirection;
   }
 
+  public float getTopPointHeightPercent() {
+    return topPointHeightPercent;
+  }
+
+  public float getBottomPointHeightPercent() {
+    return bottomPointHeightPercent;
+  }
 }
