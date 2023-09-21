78756d528b274a46e65673ba1de028760e49d4b8
Dmitry Plodukhin <dmitry.plodukhin@simbirsoft.com>
2023-09-21 11:56:19 +0300

    #2475 remove hub url condition

--- modules/selenoid/src/test/java/org/selenide/selenoid/SelenoidClientTest.java
+++ modules/selenoid/src/test/java/org/selenide/selenoid/SelenoidClientTest.java
@@ -6,20 +6,12 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class SelenoidClientTest {
   @Test
   void extractsSelenoidBaseUrlFromHubUrl() {
     SelenoidClient client = new SelenoidClient("http://localhost:4444/wd/hub", "sid-01");
     assertThat(client.baseUrl).isEqualTo("http://localhost:4444");
-  }
-
-  @Test
-  void hubsUrlShouldEndWithWdHub() {
-    assertThatThrownBy(() -> new SelenoidClient("http://localhost:4444", "sid-01"))
-        .isInstanceOf(IllegalArgumentException.class)
-        .hasMessage("Expect hub url to end with /wd/hub, but received: http://localhost:4444");
   }
 
   @Test
