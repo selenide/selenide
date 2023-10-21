87f85b2705814b05ef7e9a2536c3502b2dc46a58
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-10-21 17:56:11 +0300

    disable Drag'n'Drop via selenium actions in Firefox
    D'n'D via actions doesn't work in Firefox, what can I do...

--- statics/src/test/java/integration/DragAndDropTest.java
+++ statics/src/test/java/integration/DragAndDropTest.java
@@ -8,6 +8,8 @@ import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.DragAndDropOptions.to;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.WebDriverRunner.isFirefox;
+import static org.assertj.core.api.Assumptions.assumeThat;
 
 public class DragAndDropTest extends IntegrationTest {
 
@@ -30,12 +32,14 @@ public class DragAndDropTest extends IntegrationTest {
 
   @Test
   public void dragAndDrop_toWebElement_using_actions() {
+    assumeThat(isFirefox()).isFalse();
     $("#drag1").dragAndDrop(to($("#div2")).usingSeleniumActions());
     $("#div2").$("#drag1").should(appear);
   }
 
   @Test
   public void dragAndDrop_toCssSelector_using_actions() {
+    assumeThat(isFirefox()).isFalse();
     $("#drag1").dragAndDrop(to("#div2").usingSeleniumActions());
     $("#div2").$("#drag1").should(appear);
   }
