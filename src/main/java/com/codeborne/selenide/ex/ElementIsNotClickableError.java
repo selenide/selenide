bef313ed0b101625c7f977c56e74f367a8dd0417
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-10-24 17:16:18 +0300

    #2485 rename "ElementIsNotClickableException" to "ElementIsNotClickableError"
    I don't think it's really important, but some people find it incorrect to name class "*Exception" if it doesn't inherit `java.lang.Exception`.

--- src/main/java/com/codeborne/selenide/ex/ElementIsNotClickableError.java
+++ src/main/java/com/codeborne/selenide/ex/ElementIsNotClickableError.java
@@ -5,8 +5,8 @@ import com.codeborne.selenide.Driver;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 @ParametersAreNonnullByDefault
-public class ElementIsNotClickableException extends UIAssertionError {
-  public ElementIsNotClickableException(Driver driver, String elementDescription, Throwable cause) {
+public class ElementIsNotClickableError extends UIAssertionError {
+  public ElementIsNotClickableError(Driver driver, String elementDescription, Throwable cause) {
     super(driver, "Element is not clickable: " + elementDescription, cause);
   }
 }
