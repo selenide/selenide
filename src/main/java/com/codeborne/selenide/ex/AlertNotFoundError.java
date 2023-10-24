39cefc3e388fa16516c2cd2c6149b2e5d5b77dc5
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-10-24 17:17:16 +0300

    #2485 rename "AlertNotFoundException" to "AlertNotFoundError"
    I don't think it's really important, but some people find it incorrect to name class "*Exception" if it doesn't inherit `java.lang.Exception`.

--- src/main/java/com/codeborne/selenide/ex/AlertNotFoundError.java
+++ src/main/java/com/codeborne/selenide/ex/AlertNotFoundError.java
@@ -5,8 +5,8 @@ import com.codeborne.selenide.Driver;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 @ParametersAreNonnullByDefault
-public class AlertNotFoundException extends UIAssertionError {
-  public AlertNotFoundException(Driver driver, Throwable cause) {
+public class AlertNotFoundError extends UIAssertionError {
+  public AlertNotFoundError(Driver driver, Throwable cause) {
     super(driver, "Alert not found", cause);
   }
 }
