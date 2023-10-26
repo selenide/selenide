57b51ab55fd7306a8a7eae1fb57a7aaaed8f80f7
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-10-26 18:25:50 +0300

    restore support for "self" field in Container
    Now you need to declare your own field of type `SelenideElement` and annotate it with `@Self`.

--- src/main/java/com/codeborne/selenide/Container.java
+++ src/main/java/com/codeborne/selenide/Container.java
@@ -1,9 +1,20 @@
 package com.codeborne.selenide;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Marker interface for declaring page objects fields that contain other elements
  *
  * @since 6.19.0
  */
 public interface Container {
+
+  @Retention(RetentionPolicy.RUNTIME)
+  @Target(ElementType.FIELD)
+  @interface Self {
+  }
+
 }
