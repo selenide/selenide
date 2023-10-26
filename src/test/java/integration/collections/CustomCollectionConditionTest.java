da47a85b6468ba1596fdf94c66ae819f448ec9f2
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-10-26 15:35:27 +0300

    simplify sample collection condition AllTextsStartingWith

--- src/test/java/integration/collections/CustomCollectionConditionTest.java
+++ src/test/java/integration/collections/CustomCollectionConditionTest.java
@@ -1,9 +1,10 @@
 package integration.collections;
 
 import com.codeborne.selenide.CheckResult;
+import com.codeborne.selenide.Driver;
 import com.codeborne.selenide.WebElementsCondition;
 import com.codeborne.selenide.ex.UIAssertionError;
-import com.codeborne.selenide.impl.CollectionSource;
+import com.codeborne.selenide.impl.ElementCommunicator;
 import integration.ITest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,13 +12,11 @@ import org.openqa.selenium.WebElement;
 
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.codeborne.selenide.CollectionCondition.texts;
-import static java.lang.System.lineSeparator;
+import static com.codeborne.selenide.impl.Plugins.inject;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ParametersAreNonnullByDefault
@@ -39,7 +38,7 @@ final class CustomCollectionConditionTest extends ITest {
     assertThatThrownBy(() ->
       $$(".element").shouldHave(allTextsStartingWith("T"))
     )
-      .isInstanceOf(MismatchingPrefixesError.class)
+      .isInstanceOf(UIAssertionError.class)
       .hasMessageStartingWith("Texts not starting with prefix")
       .hasMessageContaining("Actual: [One, Two, Three]")
       .hasMessageContaining("Expected: All texts starting with T")
@@ -51,6 +50,8 @@ final class CustomCollectionConditionTest extends ITest {
   }
 
   private static class AllTextsStartingWith extends WebElementsCondition {
+    private static final ElementCommunicator communicator = inject(ElementCommunicator.class);
+
     private final String prefix;
 
     private AllTextsStartingWith(String prefix) {
@@ -60,39 +61,23 @@ final class CustomCollectionConditionTest extends ITest {
     @Nonnull
     @CheckReturnValue
     @Override
-    public CheckResult check(CollectionSource collection) {
-      List<WebElement> elements = collection.getElements();
-      List<String> actualTexts = elements.stream().map(WebElement::getText).collect(Collectors.toList());
-      return new CheckResult(elements.stream()
+    public CheckResult check(Driver driver, List<WebElement> elements) {
+      List<String> actualTexts = communicator.texts(driver, elements);
+      Boolean passed = elements.stream()
         .map(webElement -> webElement.getText().startsWith(prefix))
-        .reduce(true, (x, y) -> x && y), actualTexts);
+        .reduce(true, (x, y) -> x && y);
+
+      return new CheckResult(passed, actualTexts);
     }
 
     @Override
-    public void fail(CollectionSource collection,
-                     CheckResult lastCheckResult,
-                     @Nullable Exception cause,
-                     long timeoutMs) {
-      throw new MismatchingPrefixesError(toString(), lastCheckResult.getActualValue(), collection, explanation);
+    public String errorMessage() {
+      return "Texts not starting with prefix " + prefix;
     }
 
     @Override
     public String toString() {
       return "All texts starting with " + prefix;
-    }
-  }
-
-  private static class MismatchingPrefixesError extends UIAssertionError {
-    MismatchingPrefixesError(String expected, @Nullable List<String> actualTexts, CollectionSource collection,
-                             @Nullable String explanation) {
-      super(
-        collection.driver(),
-        "Texts not starting with prefix " +
-        lineSeparator() + "Actual: " + actualTexts +
-        lineSeparator() + "Expected: " + expected +
-        (explanation == null ? "" : lineSeparator() + "Because: " + explanation) +
-        lineSeparator() + "Collection: " + collection.description(),
-        expected, actualTexts);
     }
   }
 }
