f704252f25a21a7b7b5e2e8d793636adc3be1ffd
amuthan-sakthivel <amuthan.sakthivel@clipboardhealth.com>
2023-11-04 08:12:34 +0530

    fix: type command issue

--- modules/appium/src/test/java/it/mobile/android/AndroidTypeTest.java
+++ modules/appium/src/test/java/it/mobile/android/AndroidTypeTest.java
@@ -1,22 +1,13 @@
 package it.mobile.android;
 
-import com.codeborne.selenide.appium.SelenideAppium;
 import io.appium.java_client.AppiumBy;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 import static com.codeborne.selenide.appium.SelenideAppium.$;
 
 class AndroidTypeTest extends BaseApiDemosTest {
-
-  @BeforeEach
-  void setUp() {
-    closeWebDriver();
-    SelenideAppium.launchApp();
-  }
 
   @Test
   void androidType() {
