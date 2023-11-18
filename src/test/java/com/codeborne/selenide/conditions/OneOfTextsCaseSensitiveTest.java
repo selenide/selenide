bfbfa51be16affc12b5a9152ec91d92a05d94cde
Yuri <yur_yurch@mail.ru>
2023-11-18 22:49:09 +0300

    #2130 oneOfTexts()-like Conditions tests

---
+++ src/test/java/com/codeborne/selenide/conditions/OneOfTextsCaseSensitiveTest.java
@@ -0,0 +1,70 @@
+package com.codeborne.selenide.conditions;
+
+import com.codeborne.selenide.Driver;
+import com.codeborne.selenide.DriverStub;
+import com.codeborne.selenide.SelenideConfig;
+import com.codeborne.selenide.TextCheck;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static com.codeborne.selenide.CheckResult.Verdict.ACCEPT;
+import static com.codeborne.selenide.CheckResult.Verdict.REJECT;
+import static com.codeborne.selenide.Mocks.mockElement;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class OneOfTextsCaseSensitiveTest {
+  private final Driver driver = new DriverStub(new SelenideConfig().textCheck(TextCheck.FULL_TEXT));
+
+  private final Collection<String> collectionWithNull = new HashSet<>(Arrays.asList("John", "Lucy", null, "Banana"));
+  private final Collection<String> collectionWithEmptyString = List.of("John", "Lucy", "", "Banana");
+  private final Collection<String> collectionWithBlankString = List.of("John", "Lucy", "   ", "Banana");
+  private final Collection<String> regularCollection = List.of("John", "Lucy", "Vod  ka", " Banana   fruit  ");
+  private final OneOfTextsCaseSensitive regularCollectionArgument = new OneOfTextsCaseSensitive(regularCollection);
+
+  @Test
+  void shouldMatchRegularCollection() {
+    assertThat(regularCollectionArgument.check(driver,
+      mockElement("My favorite fruit is BBanana fruitT of course")).verdict()).isEqualTo(ACCEPT);
+    assertThat(regularCollectionArgument.check(driver,
+      mockElement("My favorite fruit is of course bbBanana fruit")).verdict()).isEqualTo(ACCEPT);
+    assertThat(regularCollectionArgument.check(driver,
+      mockElement("Banana fruitTt My favorite fruit is of course")).verdict()).isEqualTo(ACCEPT);
+  }
+
+  @Test
+  void shouldNotMatchRegularCollectionDifferentCases() {
+    assertThat(regularCollectionArgument.check(driver,
+      mockElement("My favorite fruit is bAnAnAaaaaaa of course")).verdict()).isEqualTo(REJECT);
+    assertThat(regularCollectionArgument.check(driver,
+      mockElement("My favorite fruit is of course bAnAnA")).verdict()).isEqualTo(REJECT);
+    assertThat(regularCollectionArgument.check(driver,
+      mockElement("bAnAnA My favorite fruit is of course")).verdict()).isEqualTo(REJECT);
+  }
+
+  @Test
+  void shouldNotMatchRegularCollection() {
+    assertThat(regularCollectionArgument.check(driver,
+      mockElement("My favorite fruit is Banan of course")).verdict()).isEqualTo(REJECT);
+  }
+
+  @Test
+  void shouldThrowExceptionBecauseOfNullCollectionItem() {
+    assertThatIllegalArgumentException().isThrownBy(() -> new OneOfTextsCaseSensitive(collectionWithNull));
+  }
+
+  @Test
+  void shouldNotMatchEmptyCollection() {
+    assertThat(new OneOfTextsCaseSensitive(Collections.emptyList())
+      .check(driver, mockElement("")).verdict()).isEqualTo(REJECT);
+    assertThat(new OneOfTextsCaseSensitive(Collections.emptyList())
+      .check(driver, mockElement("Hi")).verdict()).isEqualTo(REJECT);
+  }
+
+  @Test
+  void shouldThrowExceptionIfCollectionContainsEmptyOrBlankString() {
+    assertThatIllegalArgumentException().isThrownBy(() -> new OneOfTextsCaseSensitive(collectionWithEmptyString));
+    assertThatIllegalArgumentException().isThrownBy(() -> new OneOfTextsCaseSensitive(collectionWithBlankString));
+  }
+}
