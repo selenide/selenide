8a67b84857df1887e849a62a43cd4edb0e7e7d77
Andrei Solntsev <andrei.solntsev@gmail.com>
2024-01-28 09:56:15 +0200

    #2624 mask the password in credentials (just in case)

--- src/main/java/com/codeborne/selenide/BearerTokenCredentials.java
+++ src/main/java/com/codeborne/selenide/BearerTokenCredentials.java
@@ -3,9 +3,11 @@ package com.codeborne.selenide;
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.regex.Pattern;
 
 @ParametersAreNonnullByDefault
 public class BearerTokenCredentials implements Credentials {
+  private static final Pattern REGEX_ANY_CHAR = Pattern.compile(".");
   public final String domain;
   public final String token;
 
@@ -31,6 +33,6 @@ public class BearerTokenCredentials implements Credentials {
   @CheckReturnValue
   @Nonnull
   public String toString() {
-    return String.format("%s:%s", domain, token);
+    return String.format("%s:%s", domain, REGEX_ANY_CHAR.matcher(token).replaceAll("*"));
   }
 }
