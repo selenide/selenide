17cb5f3ffbd3fff44a5dccc6b2f431087dedda35
Andrei Solntsev <andrei.solntsev@gmail.com>
2024-02-18 22:43:55 +0200

    #2654 add javadoc for @As annotation
    (that it doesn't work out of page object)

--- src/main/java/com/codeborne/selenide/As.java
+++ src/main/java/com/codeborne/selenide/As.java
@@ -8,6 +8,10 @@ import java.lang.annotation.Target;
 
 /**
  * Human-readable alias for page object field
+ *
+ * Applicable only for elements initialized by {@link com.codeborne.selenide.impl.SelenidePageFactory}.
+ * Usually it means elements annotated by {@link org.openqa.selenium.support.FindBy}, {@link org.openqa.selenium.support.FindBys} etc.
+ *
  * @since 6.8.0
  */
 @Retention(RetentionPolicy.RUNTIME)
