26e15e77f582261e57040efb286beea6b33cee13
Andrei Solntsev <andrei.solntsev@gmail.com>
2024-02-20 17:46:03 +0200

    #2333 add a comment to ScreenObject saying it's not really needed anymore

--- modules/appium/src/main/java/com/codeborne/selenide/appium/ScreenObject.java
+++ modules/appium/src/main/java/com/codeborne/selenide/appium/ScreenObject.java
@@ -1,10 +1,16 @@
 package com.codeborne.selenide.appium;
 
+import com.codeborne.selenide.Selenide;
+
 import static com.codeborne.selenide.Selenide.page;
 
 public class ScreenObject {
   /**
    * Create a Page Object instance.
+   * <br/>
+   * It's just an alias for {@link Selenide#page(Class)}
+   *
+   * @see Selenide#page(Class)
    */
   public static <PageObjectClass> PageObjectClass screen(Class<PageObjectClass> pageObjectClass) {
     return page(pageObjectClass);
@@ -12,6 +18,10 @@ public class ScreenObject {
 
   /**
    * Initialize a Page Object fields annotated with @FindBy, @AndroidFindBy, @iOSFindBy etc.
+   * <br/>
+   * It's just an alias for {@link Selenide#page(Object)}
+   *
+   * @see Selenide#page(Object)
    */
   public static <PageObjectClass, T extends PageObjectClass> PageObjectClass screen(T pageObject) {
     return page(pageObject);
