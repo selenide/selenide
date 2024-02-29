7de49865e8d59d763fc2093e23cc2bada426a399
petroov <petro.ovcharenko@solidgate.com>
2024-02-29 11:58:04 +0200

    fix aurgement exception

--- src/main/java/com/codeborne/selenide/impl/WebdriverUnwrapper.java
+++ src/main/java/com/codeborne/selenide/impl/WebdriverUnwrapper.java
@@ -1,6 +1,10 @@
 package com.codeborne.selenide.impl;
 
 import com.codeborne.selenide.Driver;
+import java.util.Optional;
+import javax.annotation.CheckReturnValue;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,11 +12,6 @@ import org.openqa.selenium.WrapsDriver;
 import org.openqa.selenium.WrapsElement;
 import org.openqa.selenium.remote.Augmenter;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
-import javax.annotation.CheckReturnValue;
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.util.Optional;
 
 public class WebdriverUnwrapper {
   public static <T> boolean instanceOf(Driver driver, Class<T> klass) {
@@ -31,9 +30,7 @@ public class WebdriverUnwrapper {
     WebDriver webdriver = unwrap(driverOrElement);
 
     //noinspection unchecked
-    return webdriver != null && klass.isAssignableFrom(webdriver.getClass()) ?
-      Optional.of((T) webdriver) :
-      Optional.empty();
+    return webdriver != null && klass.isAssignableFrom(webdriver.getClass()) ? Optional.of((T) webdriver) : Optional.empty();
   }
 
   public static <T> T cast(WebElement probablyWrappedWebElement, Class<T> klass) {
@@ -55,8 +52,12 @@ public class WebdriverUnwrapper {
     if (driverOrElement instanceof WrapsDriver wrapper) {
       return unwrap(wrapper.getWrappedDriver());
     }
-    if (driverOrElement instanceof RemoteWebDriver remoteWebDriver) {
-      return new Augmenter().augment(remoteWebDriver);
+    try {
+      if (driverOrElement instanceof RemoteWebDriver remoteWebDriver) {
+        return new Augmenter().augment(remoteWebDriver);
+      }
+    } catch (IllegalStateException e) {
+      return (WebDriver) driverOrElement;
     }
     return null;
   }
@@ -64,7 +65,6 @@ public class WebdriverUnwrapper {
   @Nonnull
   @CheckReturnValue
   public static RemoteWebDriver unwrapRemoteWebDriver(WebDriver driver) {
-    return driver instanceof WrapsDriver wrapper ?
-      (RemoteWebDriver) wrapper.getWrappedDriver() : (RemoteWebDriver) driver;
+    return driver instanceof WrapsDriver wrapper ? (RemoteWebDriver) wrapper.getWrappedDriver() : (RemoteWebDriver) driver;
   }
 }
