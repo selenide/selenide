a2e0cf81cad1209e803d921c8b71a3f49086676d
Andrei Solntsev <andrei.solntsev@gmail.com>
2024-07-23 09:51:54 +0300

    simplify code for opening deep link URL in Safari
    (after opening a Safari browser) instead of clicking "Address" element, just call old good "driver.navigate().to(deepLinkUrl)". It's much more reliable.
    
    Also, SAFARI_ELEMENTS_TIMEOUT can now be much shorter because the alert "are you sure you want open deeplink?" appears almost immediatelly.

--- modules/appium/src/main/java/com/codeborne/selenide/appium/DeepLinkLauncher.java
+++ modules/appium/src/main/java/com/codeborne/selenide/appium/DeepLinkLauncher.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.ios.IOSDriver;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.HasCapabilities;
 import org.slf4j.Logger;
@@ -15,47 +16,33 @@ import java.time.Duration;
 import java.util.Map;
 
 import static com.codeborne.selenide.Condition.enabled;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.appium.SelenideAppium.$;
 import static com.codeborne.selenide.impl.WebdriverUnwrapper.cast;
 import static io.appium.java_client.AppiumBy.iOSNsPredicateString;
 import static io.appium.java_client.ios.options.wda.SupportsAutoAcceptAlertsOption.AUTO_ACCEPT_ALERTS_OPTION;
 
 @ParametersAreNonnullByDefault
 public class DeepLinkLauncher {
-  public static Duration SAFARI_ELEMENTS_TIMEOUT = Duration.ofSeconds(10);
+  public static Duration SAFARI_ELEMENTS_TIMEOUT = Duration.ofSeconds(1);
   private static final String SAFARI_BUNDLE_ID = "com.apple.mobilesafari";
   private static final Logger log = LoggerFactory.getLogger(DeepLinkLauncher.class);
 
   // adopted from https://bit.ly/3OKVsvq
   // see also https://appiumpro.com/editions/84-reliably-opening-deep-links-across-platforms-and-devices
-  public void openDeepLinkOnIos(AppiumDriver driver, String deepLinkUrl) {
+  public void openDeepLinkOnIos(IOSDriver driver, String deepLinkUrl) {
     SelenideLogger.run("open ios deeplink", deepLinkUrl, () -> {
       openSafari(driver);
-      // Add the deep link url in Safari in the `URL`-field
-      // This can be 2 different elements, or the button, or the text field
-      // Use the predicate string because the accessibility label will return 2 different types
-      // of elements making it flaky to use. With predicate string we can be more precise
-      SelenideElement addressBar = $(iOSNsPredicateString("label == 'Address' OR label == 'Адрес' OR name == 'URL'"));
-      SelenideElement urlField = $(iOSNsPredicateString("type == 'XCUIElementTypeTextField' && name CONTAINS 'URL'"));
-      // Wait for the url button to appear and click on it so the text field will appear
-      // iOS 13 now has the keyboard open by default because the URL field has focus when opening the Safari browser
+      driver.navigate().to(deepLinkUrl);
 
-      addressBar.shouldBe(visible).click();
-
-      // Submit the url and add a break
-      urlField.shouldBe(visible, SAFARI_ELEMENTS_TIMEOUT).setValue(deepLinkUrl + "\uE007");
       // if you started the iOS device with `autoAcceptAlerts:true` in the capabilities
       // then Appium will auto accept the alert that should be shown now.
       if (!canAutoAcceptAlerts(driver)) {
         // Wait for the notification and accept it
         // When using an iOS simulator you will only get the pop-up once, all the other times it won't be shown
-        try {
-          SelenideElement openButton =
-            $(iOSNsPredicateString("type == 'XCUIElementTypeButton' && (name CONTAINS 'Open' OR name CONTAINS 'Открыть')"));
-          openButton.shouldBe(enabled, SAFARI_ELEMENTS_TIMEOUT).click();
-        } catch (AssertionError openButtonNotFound) {
-          log.warn("Open button not found", openButtonNotFound);
+        SelenideElement openButton =
+          $(iOSNsPredicateString("type == 'XCUIElementTypeButton' && (name CONTAINS 'Open' OR name CONTAINS 'Открыть')"));
+        if (openButton.is(enabled, SAFARI_ELEMENTS_TIMEOUT)) {
+          openButton.click();
         }
       }
     });
