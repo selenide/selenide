6ccaeedafe58ba3e5793eaf76f3aa19a953da9e3
Andrei Solntsev <andrei.solntsev@gmail.com>
2024-08-10 16:37:33 +0300

    remove WebDriverListener after a test
    I discovered that we didn't remove the listener in iOS tests (which potentially may cause OOM and similar problems).

--- modules/appium/src/test/java/it/mobile/ios/BaseSwagLabsAppIosTest.java
+++ modules/appium/src/test/java/it/mobile/ios/BaseSwagLabsAppIosTest.java
@@ -1,24 +1,36 @@
 package it.mobile.ios;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.appium.SelenideAppium.launchApp;
-
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
+import it.mobile.ClickListener;
 import it.mobile.ITTest;
 import it.mobile.ios.driverproviders.SwagLabsIosDriverProvider;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.support.events.WebDriverListener;
 
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.appium.SelenideAppium.launchApp;
+
 abstract class BaseSwagLabsAppIosTest extends ITTest {
+  private static final WebDriverListener listener = new ClickListener();
+
   @BeforeEach
   public final void setUp() {
     closeWebDriver();
     Configuration.browser = SwagLabsIosDriverProvider.class.getName();
-    WebDriverRunner.addListener(new WebDriverListener() {
-    });
-    Configuration.timeout = 10_000;
     launchApp();
+  }
+
+  @BeforeAll
+  static void beforeAll() {
+    WebDriverRunner.addListener(listener);
+  }
+
+  @AfterAll
+  static void afterAll() {
+    WebDriverRunner.removeListener(listener);
   }
 }
 
