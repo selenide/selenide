b38854ed860ee039c0f61efaee1f6b7172700c72
Andrei Solntsev <andrei.solntsev@gmail.com>
2024-09-08 15:57:57 +0300

    fix flaky mobile tests
    Properly clean up the app after terminating it.
    
    It seems that "DeepLinkUrlAndroidTest" leaves the app in a terminated state which is not cleared. And the following test restores this app without clearing it.

--- modules/appium/src/test/java/it/mobile/ios/DeepLinkUrlIosTest.java
+++ modules/appium/src/test/java/it/mobile/ios/DeepLinkUrlIosTest.java
@@ -1,19 +1,26 @@
 package it.mobile.ios;
 
 import com.codeborne.selenide.appium.SelenideAppium;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.appium.AppiumSelectors.byAttribute;
 
 class DeepLinkUrlIosTest extends BaseSwagLabsAppIosTest {
   @Test
-  void testDeepLinkInIos() {
+  void deepLinkInIos() {
     SelenideAppium.openIOSDeepLink("mydemoapprn://product-details/1");
     $(byAttribute("label", "Sauce Labs Backpack")).shouldBe(visible, Duration.ofSeconds(20));
+  }
+
+  @AfterEach
+  void tearDown() {
     SelenideAppium.terminateApp("com.saucelabs.mydemoapp.rn");
+    closeWebDriver();
   }
 }
