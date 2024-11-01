e46355dcf4efcda60d740a9fbb9477df1d6d3a85
Andrei Solntsev <andrei.solntsev@gmail.com>
2024-11-01 19:12:44 +0200

    #2858 simplify regex: most of these space characters are already covered by `p{Zs}`

--- src/main/java/com/codeborne/selenide/impl/Html.java
+++ src/main/java/com/codeborne/selenide/impl/Html.java
@@ -8,8 +8,7 @@ import static java.util.regex.Pattern.DOTALL;
 
 @ParametersAreNonnullByDefault
 public class Html {
-  private static final Pattern REGEX_SPACES = Pattern.compile(
-    "[\\s\\p{Zs}\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u200B\u200C\u200D\u202F\u205F\u2060\u3000]+");
+  private static final Pattern REGEX_SPACES = Pattern.compile("[\\s\\p{Zs}\u200B\u200C\u200D\u2060]+");
 
   public static Html text = new Html();
 
