6ea92167978fc1b7d1d6670585c5977423115d03
petroov <petro.ovcharenko@solidgate.com>
2024-11-04 07:30:07 +0200

    fix test failure

--- modules/appium/src/test/java/com/codeborne/selenide/appium/SelenideAppiumPageFactoryTest.java
+++ modules/appium/src/test/java/com/codeborne/selenide/appium/SelenideAppiumPageFactoryTest.java
@@ -1,14 +1,18 @@
 package com.codeborne.selenide.appium;
 
-import static com.codeborne.selenide.Selenide.open;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.WebDriverRunner;
+import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.FindBy;
 
 class SelenideAppiumPageFactoryTest {
@@ -32,7 +36,9 @@ class SelenideAppiumPageFactoryTest {
 
   @Test
   void mobile_platform_element_successfully_init_with_created_webdriver() {
-    open();
+    AndroidDriver androidDriver = mock(AndroidDriver.class);
+    when(androidDriver.getCapabilities()).thenReturn(new DesiredCapabilities());
+    WebDriverRunner.setWebDriver(androidDriver);
     var page = Selenide.page(PageWithPlatformSelectors.class);
     assertThat(page.element).isNotNull()
       .hasToString("{By.id: element}");
