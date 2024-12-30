a2fb284fd6c9d7a4b70b1d95bc7cb45c01ff6acb
Andrei Solntsev <andrei.solntsev@gmail.com>
2024-12-30 13:13:47 +0200

    check that file name doesn't contain ".." or "/"
    I think it's not possible because we get the filename from Selenoid/Moon, not from end user.
    But just in case, let's double-check.

--- modules/moon/src/main/java/org/selenide/moon/MoonClient.java
+++ modules/moon/src/main/java/org/selenide/moon/MoonClient.java
@@ -25,6 +25,8 @@ import java.util.List;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Objects.requireNonNull;
+import static org.apache.commons.io.FilenameUtils.getName;
+import static org.apache.commons.io.FilenameUtils.normalize;
 
 public class MoonClient {
   private static final Logger log = LoggerFactory.getLogger(MoonClient.class);
@@ -150,6 +152,9 @@ public class MoonClient {
   }
 
   URL urlOfDownloadedFile(String fileName) {
+    if (!fileName.equals(normalize(getName(fileName)))) {
+      throw new IllegalArgumentException("Invalid file name: " + fileName);
+    }
     return url(baseUrl, "session", sessionId, "aerokube", "download", fileName);
   }
 
