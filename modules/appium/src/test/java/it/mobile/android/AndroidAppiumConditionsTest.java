ea8a14160cdfaf0e5a91d4a4953c26979566d836
Andrei Solntsev <andrei.solntsev@gmail.com>
2025-03-07 09:43:00 +0200

    #2967 improve how the Android/iOS combined attribute looks in error messages
    Before: `Element should have attribute com.codeborne.selenide.appium.conditions.CombinedAttribute@47b530e0="My Projects"`
    After: `Element should have attribute @text|@ioz="My Projects"`

--- modules/appium/src/test/java/it/mobile/android/AndroidAppiumConditionsTest.java
+++ modules/appium/src/test/java/it/mobile/android/AndroidAppiumConditionsTest.java
@@ -2,8 +2,10 @@ package it.mobile.android;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.appium.SelenideAppium;
+import com.codeborne.selenide.appium.SelenideAppiumElement;
+import com.codeborne.selenide.ex.ElementShould;
 import com.codeborne.selenide.ex.TextsMismatch;
-import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -15,6 +17,7 @@ import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.appium.AppiumCollectionCondition.attributes;
 import static com.codeborne.selenide.appium.AppiumCondition.attribute;
 import static com.codeborne.selenide.appium.conditions.CombinedAttribute.android;
+import static io.appium.java_client.AppiumBy.accessibilityId;
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -35,12 +38,27 @@ class AndroidAppiumConditionsTest extends BaseApiDemosTest {
 
     Configuration.timeout = 1;
     List<String> expectedAttributes = asList("API Demos", "KeyEventText", "Linkify", "LogTextBox");
-    assertThatThrownBy(() -> elements.shouldHave(attributes(android("text"), expectedAttributes)))
+    assertThatThrownBy(() -> elements.shouldHave(attributes(android("text").ios("ioz"), expectedAttributes)))
       .isInstanceOf(TextsMismatch.class)
       .hasMessageStartingWith("List size mismatch (expected: 4, actual: 6)")
       .hasMessageContaining("Actual (6): [API Demos, KeyEventText, Linkify, LogTextBox, Marquee, Unicode]")
       .hasMessageContaining("Expected (4): [API Demos, KeyEventText, Linkify, LogTextBox]")
       .hasMessageContaining("Collection: By.xpath: //android.widget.TextView");
+  }
+
+  @Test
+  void appiumConditionAttribute_textMismatch() {
+    $(By.xpath(".//*[@text='Text']")).click();
+    SelenideAppiumElement element = SelenideAppium.$(By.xpath("//android.widget.TextView"));
+
+    Configuration.timeout = 1;
+    assertThatThrownBy(() -> element.shouldHave(attribute(android("text").ios("ioz"), "WRONG")))
+      .isInstanceOf(ElementShould.class)
+      .hasMessageStartingWith("Element should have attribute @text|@ioz=\"WRONG\" {By.xpath: //android.widget.TextView}")
+      .hasMessageContaining("Element: '<TextView ")
+      .hasMessageContaining("Actual value: @text|@ioz=\"API Demos\"")
+      .hasMessageContaining("Screenshot:")
+      .hasMessageContaining("Page source:");
   }
 
   @Test
@@ -61,7 +79,7 @@ class AndroidAppiumConditionsTest extends BaseApiDemosTest {
 
   @Test
   void appiumConditionAttribute() {
-    $(AppiumBy.accessibilityId("Accessibility"))
+    $(accessibilityId("Accessibility"))
       .shouldHave(attribute(android("content-desc").ios("name"), "Accessibility"));
   }
 }
