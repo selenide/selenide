0ff34f918c2963a5b67e7496f92e8a89bc1ec600
Andrei Solntsev <andrei.solntsev@gmail.com>
2025-03-27 09:18:10 +0200

    remove unused field `WebElementSelector.instance`

--- src/main/java/com/codeborne/selenide/impl/WebElementSelector.java
+++ src/main/java/com/codeborne/selenide/impl/WebElementSelector.java
@@ -22,8 +22,6 @@ import static java.util.Objects.requireNonNull;
  * Thanks to http://selenium.polteq.com/en/injecting-the-sizzle-css-selector-library/
  */
 public class WebElementSelector {
-  public static WebElementSelector instance = new WebElementSelector();
-
   protected final FileContent sizzleSource = new FileContent("sizzle.js");
 
   public WebElement findElement(Driver driver, @Nullable WebElementSource parent, By selector, int index) {
