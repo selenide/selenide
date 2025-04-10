19023536e241821c33fb42beaf6ee638b3129c43
Andrei Solntsev <andrei.solntsev@gmail.com>
2025-04-10 18:21:40 +0300

    test how [[ATTACHMENT|%s]] works

---
+++ statics/src/test/java/integration/AttachmentTest.java
@@ -0,0 +1,33 @@
+package integration;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
+public class AttachmentTest {
+  private static final Logger log = LoggerFactory.getLogger(AttachmentTest.class);
+
+  @Test
+  void one() {
+    System.out.printf("[[ATTACHMENT|%s]]%n", new File("build.gradle").getAbsolutePath());
+    System.out.printf("[[ATTACHMENT|%s]]%n", new File("CHANGELOG.md").getPath());
+  }
+
+  @Test
+  void two() {
+    System.out.printf("SHOT [[ATTACHMENT|%s]] SAVED%n", new File("LICENSE").getAbsolutePath());
+    System.out.printf("SHOT [[ATTACHMENT|%s]] SAVED%n", new File("README.md").getPath());
+  }
+
+  @Test
+  void three() {
+    log.info("[[ATTACHMENT|{}]]", new File("release").getAbsolutePath());
+  }
+
+  @Test
+  void four() {
+    log.info("SHOT [[ATTACHMENT|{}]] SAVED", new File("settings.gradle").getAbsolutePath());
+  }
+}
