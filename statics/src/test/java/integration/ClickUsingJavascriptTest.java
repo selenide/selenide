9bfeb7fb8d1668b4f04ffdad7acf9ed113ae088a
Andrei Solntsev <andrei.solntsev@gmail.com>
2025-04-18 17:37:39 +0300

    #2763 add method for clicking element while holding keys ALT or SHIFT or CONTROL or META

--- statics/src/test/java/integration/ClickUsingJavascriptTest.java
+++ statics/src/test/java/integration/ClickUsingJavascriptTest.java
@@ -1,11 +1,14 @@
 package integration;
 
+import com.codeborne.selenide.ClickMethod;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.events.WebDriverListener;
@@ -13,11 +16,15 @@ import org.openqa.selenium.support.events.WebDriverListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.codeborne.selenide.ClickOptions.using;
 import static com.codeborne.selenide.ClickOptions.usingJavaScript;
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.selected;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.openqa.selenium.Keys.ALT;
+import static org.openqa.selenium.Keys.SHIFT;
 
 final class ClickUsingJavascriptTest extends IntegrationTest {
   private static final List<WebElement> clickedElements = new ArrayList<>();
@@ -65,6 +72,22 @@ final class ClickUsingJavascriptTest extends IntegrationTest {
     $(By.name("rememberMe")).click();
     $(By.name("rememberMe")).shouldBe(selected);
     assertThat(clickedElements).hasSize(1);
+  }
+
+  @ParameterizedTest
+  @EnumSource(ClickMethod.class)
+  void userCanClick_whileHoldingKeys(ClickMethod clickMethod) {
+    openFile("page_with_double_clickable_button.html");
+    $("h2").shouldHave(exactText("Status: not clicked yet"));
+
+    $("#double-clickable-button").click(using(clickMethod).holdingKeys(ALT));
+    $("h2").shouldHave(exactText("Status: clicked+alt"));
+
+    $("#double-clickable-button").click(using(clickMethod).holdingKeys(SHIFT));
+    $("h2").shouldHave(exactText("Status: clicked+shift"));
+
+    $("#double-clickable-button").click(using(clickMethod).holdingKeys(ALT, SHIFT));
+    $("h2").shouldHave(exactText("Status: clicked+shift+alt"));
   }
 
   public static class MyListener implements WebDriverListener {
