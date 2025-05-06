2dbe7c1686f26c5a9cc35e36ee03603bd144ad12
Andrei Solntsev <andrei.solntsev@gmail.com>
2025-05-07 00:35:56 +0300

    #3022 increase video processing timeout from 20s to 5 minutes
    Now we need more time because we start rendering the video AFTER test completion.

--- modules/video-recorder/src/main/java/org/selenide/videorecorder/core/VideoConfiguration.java
+++ modules/video-recorder/src/main/java/org/selenide/videorecorder/core/VideoConfiguration.java
@@ -5,20 +5,38 @@ import com.codeborne.selenide.PropertiesReader;
 /**
  * Inspired by Video Recorder from Sergey Pirogov
  *
+ * These settings can be set only once, and only using these two means:
+ * 1. via file "selenide.properties", or
+ * 2. via system properties (has higher priority)
+ *
  * @see <a href="https://github.com/SergeyPirogov/video-recorder-java/blob/master/core/src/main/java/com/automation/remarks/video/recorder/VideoConfiguration.java">VideoConfiguration</a>
  */
 public class VideoConfiguration {
   private final PropertiesReader properties = new PropertiesReader("selenide.properties");
 
+  /**
+   * Is video recording enabled.
+   * Use it to globally disable video recording in all tests (e.g. in some specific environments, build types etc.)
+   *
+   * @return true by default
+   */
   @SuppressWarnings("BooleanMethodIsAlwaysInverted")
   public boolean videoEnabled() {
     return properties.getBoolean("selenide.video.enabled", true);
   }
 
+  /**
+   * Should the videos be recorded for all tests, or only for annotated with {@link Video}.
+   * @return {@link RecordingMode#ANNOTATED} by default
+   */
   public RecordingMode mode() {
     return RecordingMode.valueOf(properties.getProperty("selenide.video.mode", "ANNOTATED"));
   }
 
+  /**
+   * Should the videos be saved for all tests, or only for failed tests.
+   * @return {@link VideoSaveMode#FAILED_ONLY} by default
+   */
   public VideoSaveMode saveMode() {
     return VideoSaveMode.valueOf(properties.getProperty("selenide.video.save.mode", "FAILED_ONLY"));
   }
@@ -42,8 +60,10 @@ public class VideoConfiguration {
 
   /**
    * Timeout for processing a video (in milliseconds)
+   *
+   * Default value: 5 minutes (300000 ms)
    */
   long videoProcessingTimeout() {
-    return properties.getLong("selenide.video.processing.timeout", 20_000L);
+    return properties.getLong("selenide.video.processing.timeout", 300_000L);
   }
 }
