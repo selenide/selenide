6186b8e69d7c8871d98717c389ca1586db574f9a
Andrei Solntsev <andrei.solntsev@gmail.com>
2025-05-25 12:44:16 +0300

    fix bug in "setValue": don't lose focus if the input is already focused
    Before this change, method "$.setValue(JS)" did:
    1. blur (move focus from previous active element)
    2. focus (put focus on the needed input)
    
    The problem is that on the 1st step, the input (which was already focused by that moment) looses the focus and can disappear.

--- src/main/resources/set-value.js
+++ src/main/resources/set-value.js
@@ -26,8 +26,10 @@
   if (webelement.getAttribute('readonly') !== null) return 'Cannot change value of readonly element';
   if (webelement.getAttribute('disabled') !== null) return 'Cannot change value of disabled element';
 
-  trigger(document.activeElement, ['blur']);
-  webelement.focus();
+  if (document.activeElement !== webelement) {
+    trigger(document.activeElement, ['blur']);
+    webelement.focus();
+  }
   var maxlength = webelement.getAttribute('maxlength') == null ? -1 : parseInt(webelement.getAttribute('maxlength'));
   webelement.value = maxlength === -1 ? text : text.length <= maxlength ? text : text.substring(0, maxlength);
   trigger(webelement, ['focus', 'keydown', 'keypress', 'input', 'keyup', 'change']);
