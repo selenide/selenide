0fb2aa40ac987e5955a678e5051ab79ff84321b5
Andrei Solntsev <andrei.solntsev@gmail.com>
2025-07-22 17:29:42 +0300

    remove "end marker" - the last Selenide screenshot
    it looked ugly when it didn't match the browser size. ffmpeg resized it automatically, and I didn't find an easy configuration option to avoid resizing.
    
    Bye-bye, my precious... :( :( :(

--- modules/video-recorder/src/main/java/org/selenide/videorecorder/core/ImageSource.java
+++ modules/video-recorder/src/main/java/org/selenide/videorecorder/core/ImageSource.java
@@ -1,38 +1,10 @@
 package org.selenide.videorecorder.core;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 
-import static java.lang.Thread.currentThread;
-import static java.util.Objects.requireNonNull;
-import static org.apache.commons.io.FileUtils.openInputStream;
-
-public interface ImageSource {
-  InputStream getImage() throws IOException;
-}
-
-record FileImageSource(File file) implements ImageSource {
-  @Override
-  public InputStream getImage() throws IOException {
-    return openInputStream(file);
-  }
-
+public record ImageSource(File file) {
   @Override
   public String toString() {
     return file.toString();
-  }
-}
-
-record ClasspathResource(String file) implements ImageSource {
-  @Override
-  public InputStream getImage() {
-    return requireNonNull(currentThread().getContextClassLoader().getResourceAsStream(file),
-      () -> "Resource " + file + " not found in classpath");
-  }
-
-  @Override
-  public String toString() {
-    return "cp:" + file;
   }
 }
