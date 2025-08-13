3f04f338abbf8ffe76b58f5ca8f8ff1287d36718
Aliaksandr Rasolka <rosolko@gmail.com>
2025-08-13 10:58:43 +0300

    Update Selenium from 4.34.0 to 4.35.0
    * CDP from 138 to 139

--- src/main/java/com/codeborne/selenide/impl/DownloadFileWithCdp.java
+++ src/main/java/com/codeborne/selenide/impl/DownloadFileWithCdp.java
@@ -13,10 +13,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.devtools.HasDevTools;
-import org.openqa.selenium.devtools.v138.browser.Browser;
-import org.openqa.selenium.devtools.v138.browser.model.DownloadProgress;
-import org.openqa.selenium.devtools.v138.browser.model.DownloadWillBegin;
-import org.openqa.selenium.devtools.v138.page.Page;
+import org.openqa.selenium.devtools.v139.browser.Browser;
+import org.openqa.selenium.devtools.v139.browser.model.DownloadProgress;
+import org.openqa.selenium.devtools.v139.browser.model.DownloadWillBegin;
+import org.openqa.selenium.devtools.v139.page.Page;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,8 +32,8 @@ import static com.codeborne.selenide.impl.WebdriverUnwrapper.cast;
 import static java.lang.System.currentTimeMillis;
 import static java.util.Collections.emptyMap;
 import static java.util.Objects.requireNonNull;
-import static org.openqa.selenium.devtools.v138.browser.Browser.downloadProgress;
-import static org.openqa.selenium.devtools.v138.browser.Browser.downloadWillBegin;
+import static org.openqa.selenium.devtools.v139.browser.Browser.downloadProgress;
+import static org.openqa.selenium.devtools.v139.browser.Browser.downloadWillBegin;
 
 public class DownloadFileWithCdp {
   private static final Logger log = LoggerFactory.getLogger(DownloadFileWithCdp.class);
