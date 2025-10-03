dbb1bd6e43b9b2231bbb8c0dbe00d246ea51917f
Andrei Solntsev <andrei.solntsev@gmail.com>
2025-10-03 09:50:47 +0300

    disable clipboard test on BrowserStack iOS

--- modules/appium/src/test/java/it/mobile/ios/IosClipboardTest.java
+++ modules/appium/src/test/java/it/mobile/ios/IosClipboardTest.java
@@ -3,12 +3,19 @@ package it.mobile.ios;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.clipboard;
+import static it.mobile.BrowserstackUtils.isCi;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assumptions.assumeThat;
 
 class IosClipboardTest extends BaseSwagLabsAppIosTest {
   @Test
   void canReadAndWriteClipboard() {
     clipboard().setText("Clipboard works in iOS");
+
+    assumeThat(isCi())
+      .as("in October 2025, clipboard was empty on BrowserStack..")
+      .isFalse();
+
     assertThat(clipboard().getText()).isEqualTo("Clipboard works in iOS");
   }
 }
