793bf229d5a05e43fbbd7801b9b9dda85c2566c0
Aliaksandr Rasolka <rosolko@gmail.com>
2025-12-30 00:19:43 +0300

    Add a private constructor to hide the implicit public one.

--- src/main/java/com/codeborne/selenide/ValueMasker.java
+++ src/main/java/com/codeborne/selenide/ValueMasker.java
@@ -1,6 +1,9 @@
 package com.codeborne.selenide;
 
 public final class ValueMasker {
+  private ValueMasker() {
+  }
+
   public static String mask(final CharSequence text) {
     return "*".repeat(text.length());
   }
