b377605e28856528d976f6ac2f11f772f9420b49
Andrei Solntsev <andrei.solntsev@gmail.com>
2026-08-09 17:09:00 +0300

    #2757 Restore FileHelper.deleteFolderIfEmpty unit tests
    These tests were dropped in #3363 while adding fileInFolder() tests,
    leaving deleteFolderIfEmpty() (still used by BrowserDownloadsFolder)
    without unit coverage.

--- src/test/java/com/codeborne/selenide/impl/FileHelperTest.java
+++ src/test/java/com/codeborne/selenide/impl/FileHelperTest.java
@@ -4,13 +4,35 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
+import java.io.IOException;
 
+import static com.codeborne.selenide.impl.FileHelper.deleteFolderIfEmpty;
+import static org.apache.commons.io.FileUtils.touch;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 final class FileHelperTest {
   @TempDir
   File reportsFolder;
+
+  @Test
+  void deletesFolderIfItIsEmpty(@TempDir File folder) {
+    assertThat(folder).exists();
+
+    deleteFolderIfEmpty(folder);
+
+    assertThat(folder).doesNotExist();
+  }
+
+  @Test
+  void ignoresFolderWhichContainsFiles(@TempDir File folder) throws IOException {
+    touch(new File(folder, "file1"));
+
+    deleteFolderIfEmpty(folder);
+
+    assertThat(folder).exists();
+    assertThat(new File(folder, "file1")).exists();
+  }
 
   @Test
   void fileInFolder_allowsSubdirectories() {
